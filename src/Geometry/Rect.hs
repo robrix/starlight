cd1e5329e97761f2cf3affe38443b60048338b10
Rob Rix <rob.rix@me.com>
2018-08-12 15:59:07 -0400

    Add min/max lenses for rects.

--- src/Geometry/Rect.hs
+++ src/Geometry/Rect.hs
@@ -1,8 +1,16 @@
 module Geometry.Rect where
 
+import Lens.Micro
+import Lens.Micro.Extras
 import Linear.V2 as Linear
 
 data Rect a = Rect
   { rectMin :: {-# UNPACK #-} !(V2 a)
   , rectMax :: {-# UNPACK #-} !(V2 a)
   }
+
+_rectMin :: Lens' (Rect a) (V2 a)
+_rectMin = lens rectMin (\ r v -> r { rectMin = v })
+
+_rectMax :: Lens' (Rect a) (V2 a)
+_rectMax = lens rectMax (\ r v -> r { rectMax = v })
