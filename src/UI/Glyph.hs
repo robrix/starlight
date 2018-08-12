360932bb375904011c00c8e6fa5852673e41c99f
Rob Rix <rob.rix@me.com>
2018-08-12 16:45:08 -0400

    Move Glyph into its own module.

--- src/UI/Glyph.hs
+++ src/UI/Glyph.hs
@@ -1,1 +1,11 @@
 module UI.Glyph where
+
+import Geometry.Rect
+import Linear.V4
+
+data Glyph = Glyph
+  { glyphCodePoint    :: !Char
+  , glyphAdvanceWidth :: {-# UNPACK #-} !Float
+  , glyphGeometry     :: ![V4 Float]
+  , glyphBounds       :: {-# UNPACK #-} !(Rect Float)
+  }
