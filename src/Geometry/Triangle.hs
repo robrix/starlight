16a139de909c33c5a33e6f54753ded19b89229b5
Rob Rix <rob.rix@me.com>
2018-08-13 18:41:06 -0400

    Unpack the fields of Triangle.

--- src/Geometry/Triangle.hs
+++ src/Geometry/Triangle.hs
@@ -3,5 +3,8 @@ module Geometry.Triangle where
 
 import Linear.V2 as Linear
 
-data Triangle n = Triangle (V2 n) (V2 n) (V2 n)
+data Triangle n = Triangle
+  {-# UNPACK #-} !(V2 n)
+  {-# UNPACK #-} !(V2 n)
+  {-# UNPACK #-} !(V2 n)
   deriving Functor
