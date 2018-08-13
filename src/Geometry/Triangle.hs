2db79ab3846aa76b3989fa39956c48f67db475b0
Rob Rix <rob.rix@me.com>
2018-08-13 18:45:02 -0400

    Move triangleVertices into Geometry.Triangle.

--- src/Geometry/Triangle.hs
+++ src/Geometry/Triangle.hs
@@ -1,10 +1,16 @@
 {-# LANGUAGE DeriveFunctor #-}
 module Geometry.Triangle where
 
-import Linear.V2 as Linear
+import Linear.V2
+import Linear.V4
 
 data Triangle n = Triangle
   {-# UNPACK #-} !(V2 n)
   {-# UNPACK #-} !(V2 n)
   {-# UNPACK #-} !(V2 n)
   deriving Functor
+
+
+triangleVertices :: Fractional a => Triangle a -> Bool -> [V4 a]
+triangleVertices (Triangle (V2 ax ay) (V2 bx by) (V2 cx cy)) True  = [ V4 ax ay 0 1, V4 bx by 0 1, V4 cx cy 0 1 ]
+triangleVertices (Triangle (V2 ax ay) (V2 bx by) (V2 cx cy)) False = [ V4 ax ay 0 0, V4 bx by 0.5 0, V4 cx cy 1 1 ]
--- src/UI/Font.hs
+++ src/UI/Font.hs
@@ -121,10 +121,6 @@ glyphPaths :: Typeface -> O.Glyph Int -> [Path V2 O.FWord]
 glyphPaths typeface glyph = fmap contourToPath (O.getScaledContours (typefaceUnderlying typeface) glyph)
 
 
-triangleVertices :: Fractional a => Triangle a -> Bool -> [V4 a]
-triangleVertices (Triangle (V2 ax ay) (V2 bx by) (V2 cx cy)) True  = [ V4 ax ay 0 1, V4 bx by 0 1, V4 cx cy 0 1 ]
-triangleVertices (Triangle (V2 ax ay) (V2 bx by) (V2 cx cy)) False = [ V4 ax ay 0 0, V4 bx by 0.5 0, V4 cx cy 1 1 ]
-
 glyphVertices :: Typeface -> O.Glyph Int -> [V4 Float]
 glyphVertices typeface = (>>= uncurry triangleVertices . first (fmap fromIntegral)) . (>>= pathTriangles 0 zero zero) . glyphPaths typeface
 
