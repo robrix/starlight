01495961ee74d44886bc0170826360e80b964628
Rob Rix <rob.rix@me.com>
2018-08-13 18:13:18 -0400

    Define a Bytes instance for Word16.

--- src/Data/Bytes.hs
+++ src/Data/Bytes.hs
@@ -1,5 +1,6 @@
 module Data.Bytes where
 
+import Data.Bits
 import Data.Word
 
 class Bytes t where
@@ -7,3 +8,6 @@ class Bytes t where
 
 instance Bytes Word8 where
   toBytes = pure
+
+instance Bytes Word16 where
+  toBytes x = [ fromIntegral $ x .&. 0xFF, fromIntegral $ (x .&. 0xFF00) `shiftR` 8 ]
