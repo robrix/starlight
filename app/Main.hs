8727aa42c054faf6c582976628f7bb6f9fa9d6e9
Rob Rix <rob.rix@me.com>
2018-08-13 18:08:10 -0400

    Move the rest of the typeface handling stuff into UI.Font.

--- app/Main.hs
+++ app/Main.hs
@@ -1,23 +1,17 @@
 {-# LANGUAGE GADTs, FlexibleInstances, RecordWildCards, ScopedTypeVariables #-}
 module Main where
 
-import Control.Applicative
 import qualified Control.Concurrent as CC
 import qualified Control.Exception as E
 import Control.Monad
 import Data.Bits
-import Data.Char
 import Data.Foldable
 import Data.List.NonEmpty (nonEmpty)
-import qualified Data.Map as Map
-import Data.Maybe (fromMaybe)
 import Data.Semigroup.Foldable
-import Data.Vector ((!?))
 import Data.Word
 import qualified Foreign.C.String as C
 import Foreign.Ptr
 import Geometry.Rect
-import Geometry.Triangle
 import GHC.Stack
 import GL.Array
 import GL.Error
@@ -36,19 +30,17 @@ import Linear.V2 as Linear
 import Linear.V3 as Linear
 import Linear.V4 as Linear
 import Linear.Vector as Linear
-import qualified Opentype.Fileformat as O
 import SDL.Event
 import SDL.Init
 import qualified SDL.Raw as SDL
 import System.Exit
-import UI.Font
+import UI.Font as Font
 import UI.Glyph
-import UI.Path
 
 main :: HasCallStack => IO ()
 main = do
   Just tahoma <- readTypeface "/Library/Fonts/Tahoma.ttf"
-  let glyphs = Main.glyphs tahoma "hs"
+  let glyphs = Font.glyphs tahoma "hs"
   [textVertex, textFragment, glyphVertex, glyphFragment] <- traverse readFile ["text-vertex.glsl", "text-fragment.glsl", "glyph-vertex.glsl", "glyph-fragment.glsl"]
   CC.runInBoundThread $ C.withCString "Text" $ \ name -> do
     _ <- SDL.init SDL.SDL_INIT_EVERYTHING >>= checkWhen (< 0)
@@ -203,84 +195,6 @@ combineGeometry ArrayVertices{..} (Geometry mode vertices) =
     (prevIndex + count)
     (arrayRanges <> [ ArrayRange { mode = mode, firstVertexIndex = prevIndex, vertexCount = count } ])
 
-
-glyphs :: Typeface -> [Char] -> [Glyph]
-glyphs typeface chars = concat (zipWith toGlyph chars (glyphsForChars typeface chars))
-  where toGlyph char (Just g) = let vertices = glyphVertices typeface g in
-          [ scaleGlyph (V2 (1 / fromIntegral (unitsPerEm typeface)) (1 / fromIntegral (unitsPerEm typeface))) $ Glyph char (fromIntegral (O.advanceWidth g)) vertices (bounds ((^. _xy) <$> vertices)) ]
-        toGlyph _ Nothing = []
-        bounds vertices = Rect (foldr1 (liftA2 min) vertices) (foldr1 (liftA2 max) vertices)
-
-glyphsForChars :: Typeface -> [Char] -> [Maybe (O.Glyph Int)]
-glyphsForChars (Typeface _ o) chars = map (>>= (glyphs !?) . fromIntegral) glyphIDs
-  where glyphIDs = fromMaybe (Nothing <$ chars) $ do
-          cmap <- find viablePlatform (O.getCmaps (O.cmapTable o))
-          Just $ lookupAll (O.glyphMap cmap) (fmap (fromIntegral . ord :: Char -> Word32) chars)
-        lookupAll = fmap . flip Map.lookup
-        glyphs = case O.outlineTables o of
-          O.QuadTables _ (O.GlyfTable glyphs) -> glyphs
-          _ -> error "wtf"
-        viablePlatform p = O.cmapPlatform p == O.UnicodePlatform || O.cmapPlatform p == O.MicrosoftPlatform && O.cmapEncoding p == 1
-
-
-contourToPath :: [O.CurvePoint] -> Path V2 O.FWord
-contourToPath [] = Z
-contourToPath (p@(O.CurvePoint x y _) : rest) = makePath Z
-  where (makePath, _) = (foldl (\ (makePath, prev) point -> (makePath . pathFor prev point, point)) (M (V2 x y), p) rest)
-        pathFor (O.CurvePoint _ _ True)  (O.CurvePoint _ _ False)   = id
-        pathFor (O.CurvePoint _ _ True)  (O.CurvePoint x y True)    = L (V2 x y)
-        pathFor (O.CurvePoint x y False) (O.CurvePoint x' y' False) = Q (V2 x y) (V2 (x + ((x' - x) `div` 2)) (y + ((y' - y) `div` 2)))
-        pathFor (O.CurvePoint x y False) (O.CurvePoint x' y' True)  = Q (V2 x y) (V2 x' y')
-
-glyphPaths :: Typeface -> O.Glyph Int -> [Path V2 O.FWord]
-glyphPaths typeface glyph = fmap contourToPath (O.getScaledContours (typefaceUnderlying typeface) glyph)
-
-
-pathTriangles :: (Int, V2 O.FWord, V2 O.FWord) -> Path V2 O.FWord -> [(Triangle V2 O.FWord, Bool)]
-pathTriangles (count, first, current) p = case p of
-  M v rest ->                                                                             pathTriangles (0,          v,     v ) rest
-  L v rest
-    | count >= 2 -> (Triangle first current v,  True)                                   : pathTriangles (succ count, first, v ) rest
-    | otherwise  ->                                                                       pathTriangles (succ count, first, v ) rest
-  Q v1 v2 rest
-    | count >= 2 -> (Triangle first current v2, True) : (Triangle current v1 v2, False) : pathTriangles (succ count, first, v2) rest
-    | otherwise  ->                                     (Triangle current v1 v2, False) : pathTriangles (succ count, first, v2) rest
-  Z ->                                                                                    []
-
-triangleVertices :: Triangle V2 O.FWord -> Bool -> [V4 Float]
-triangleVertices (Triangle (V2 ax ay) (V2 bx by) (V2 cx cy)) True  = [ V4 (fromIntegral ax) (fromIntegral ay) 0 1, V4 (fromIntegral bx) (fromIntegral by) 0 1, V4 (fromIntegral cx) (fromIntegral cy) 0 1 ]
-triangleVertices (Triangle (V2 ax ay) (V2 bx by) (V2 cx cy)) False = [ V4 (fromIntegral ax) (fromIntegral ay) 0 0, V4 (fromIntegral bx) (fromIntegral by) 0.5 0, V4 (fromIntegral cx) (fromIntegral cy) 1 1 ]
-
-glyphVertices :: Typeface -> O.Glyph Int -> [V4 Float]
-glyphVertices typeface = (>>= uncurry triangleVertices) . (>>= pathTriangles (0, V2 0 0, V2 0 0)) . glyphPaths typeface
-
-
-encodePath :: Path V2 O.FWord -> [Word8]
-encodePath = go . fmap (word16Bytes . fromIntegral)
-  where go path = case path of
-          M (V2 x y)            rest -> moveTo  : x ++ y             ++ go rest
-          L (V2 x y)            rest -> lineTo  : x ++ y             ++ go rest
-          Q (V2 x y) (V2 x' y') rest -> curveTo : x ++ y ++ x' ++ y' ++ go rest
-          _                          -> close   : []
-        (moveTo, lineTo, curveTo, close) = (0, 1, 2, 3)
-
-word16Bytes :: Word16 -> [Word8]
-word16Bytes x = [ fromIntegral $ x .&. 0xFF, fromIntegral $ (x .&. 0xFF00) `shiftR` 8 ]
-
-word32Bytes :: Word32 -> [Word8]
-word32Bytes x = [ fromIntegral $ x .&. 0xFF, fromIntegral $ (x .&. 0xFF00) `shiftR` 8, fromIntegral $ (x .&. 0xFF0000) `shiftR` 16, fromIntegral $ (x .&. 0xFF000000) `shiftR` 24 ]
-
-
-encodeGlyphPaths :: Typeface -> O.Glyph Int -> [Word8]
-encodeGlyphPaths typeface = (>>= encodePath) . glyphPaths typeface
-
-
-encodeGlyphsForChars :: Typeface -> [Char] -> [Word8]
-encodeGlyphsForChars face chars = header ++ glyphHeaders ++ (charsGlyphsAndPaths >>= \ (_, _, path) -> path)
-  where charsGlyphsAndPaths = zip chars (glyphsForChars face chars) >>= \ (char, glyph) -> (,,) char <$> toList glyph <*> fmap (encodeGlyphPaths face) (toList glyph)
-        header = word16Bytes (unitsPerEm face) ++ word16Bytes (fromIntegral (ascent face)) ++ word16Bytes (fromIntegral (descent face)) ++ word16Bytes (fromIntegral (length charsGlyphsAndPaths))
-        glyphHeaders = snd (foldl encodeGlyphHeader (0, id) charsGlyphsAndPaths) []
-        encodeGlyphHeader (offset, makeList) (char, glyph, path) = (offset + fromIntegral (length path), makeList . (++ (word16Bytes (fromIntegral (ord char)) ++ word16Bytes (O.advanceWidth glyph) ++ word32Bytes offset ++ word16Bytes (fromIntegral (length path)))))
 
 ignoreEventsOfTypes :: [Word32] -> IO ()
 ignoreEventsOfTypes = traverse_ (\ t -> SDL.eventState t 0 >>= checkWhen (/= 0))
--- src/UI/Font.hs
+++ src/UI/Font.hs
@@ -1,14 +1,27 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 module UI.Font where
 
+import Control.Applicative (liftA2)
 import Control.Monad (guard)
 import qualified Control.Exception as E
-import Data.Foldable (find)
+import Data.Bits ((.&.), shiftR)
+import Data.Char (ord)
+import Data.Foldable (find, toList)
 import Data.Int
+import qualified Data.Map as Map
+import Data.Maybe (fromMaybe)
 import qualified Data.Text as T
 import qualified Data.Text.Encoding as T
+import Data.Vector ((!?))
 import Data.Word
+import Geometry.Rect
+import Geometry.Triangle
 import qualified Opentype.Fileformat as O
+import Lens.Micro
+import Linear.V2
+import Linear.V4
+import UI.Glyph
+import UI.Path
 
 data Typeface = Typeface { typefaceName :: String, typefaceUnderlying :: O.OpentypeFont }
 
@@ -72,3 +85,82 @@ ascent = O.ascent . O.hheaTable . typefaceUnderlying
 
 descent :: Typeface -> Int16
 descent = O.descent . O.hheaTable . typefaceUnderlying
+
+
+glyphs :: Typeface -> [Char] -> [Glyph]
+glyphs typeface chars = concat (zipWith toGlyph chars (glyphsForChars typeface chars))
+  where toGlyph char (Just g) = let vertices = glyphVertices typeface g in
+          [ scaleGlyph (V2 (1 / fromIntegral (unitsPerEm typeface)) (1 / fromIntegral (unitsPerEm typeface))) $ Glyph char (fromIntegral (O.advanceWidth g)) vertices (bounds ((^. _xy) <$> vertices)) ]
+        toGlyph _ Nothing = []
+        bounds vertices = Rect (foldr1 (liftA2 min) vertices) (foldr1 (liftA2 max) vertices)
+
+glyphsForChars :: Typeface -> [Char] -> [Maybe (O.Glyph Int)]
+glyphsForChars (Typeface _ o) chars = map (>>= (glyphs !?) . fromIntegral) glyphIDs
+  where glyphIDs = fromMaybe (Nothing <$ chars) $ do
+          cmap <- find viablePlatform (O.getCmaps (O.cmapTable o))
+          Just $ lookupAll (O.glyphMap cmap) (fmap (fromIntegral . ord :: Char -> Word32) chars)
+        lookupAll = fmap . flip Map.lookup
+        glyphs = case O.outlineTables o of
+          O.QuadTables _ (O.GlyfTable glyphs) -> glyphs
+          _ -> error "wtf"
+        viablePlatform p = O.cmapPlatform p == O.UnicodePlatform || O.cmapPlatform p == O.MicrosoftPlatform && O.cmapEncoding p == 1
+
+
+contourToPath :: [O.CurvePoint] -> Path V2 O.FWord
+contourToPath [] = Z
+contourToPath (p@(O.CurvePoint x y _) : rest) = makePath Z
+  where (makePath, _) = (foldl (\ (makePath, prev) point -> (makePath . pathFor prev point, point)) (M (V2 x y), p) rest)
+        pathFor (O.CurvePoint _ _ True)  (O.CurvePoint _ _ False)   = id
+        pathFor (O.CurvePoint _ _ True)  (O.CurvePoint x y True)    = L (V2 x y)
+        pathFor (O.CurvePoint x y False) (O.CurvePoint x' y' False) = Q (V2 x y) (V2 (x + ((x' - x) `div` 2)) (y + ((y' - y) `div` 2)))
+        pathFor (O.CurvePoint x y False) (O.CurvePoint x' y' True)  = Q (V2 x y) (V2 x' y')
+
+glyphPaths :: Typeface -> O.Glyph Int -> [Path V2 O.FWord]
+glyphPaths typeface glyph = fmap contourToPath (O.getScaledContours (typefaceUnderlying typeface) glyph)
+
+
+pathTriangles :: (Int, V2 O.FWord, V2 O.FWord) -> Path V2 O.FWord -> [(Triangle V2 O.FWord, Bool)]
+pathTriangles (count, first, current) p = case p of
+  M v rest ->                                                                             pathTriangles (0,          v,     v ) rest
+  L v rest
+    | count >= 2 -> (Triangle first current v,  True)                                   : pathTriangles (succ count, first, v ) rest
+    | otherwise  ->                                                                       pathTriangles (succ count, first, v ) rest
+  Q v1 v2 rest
+    | count >= 2 -> (Triangle first current v2, True) : (Triangle current v1 v2, False) : pathTriangles (succ count, first, v2) rest
+    | otherwise  ->                                     (Triangle current v1 v2, False) : pathTriangles (succ count, first, v2) rest
+  Z ->                                                                                    []
+
+triangleVertices :: Triangle V2 O.FWord -> Bool -> [V4 Float]
+triangleVertices (Triangle (V2 ax ay) (V2 bx by) (V2 cx cy)) True  = [ V4 (fromIntegral ax) (fromIntegral ay) 0 1, V4 (fromIntegral bx) (fromIntegral by) 0 1, V4 (fromIntegral cx) (fromIntegral cy) 0 1 ]
+triangleVertices (Triangle (V2 ax ay) (V2 bx by) (V2 cx cy)) False = [ V4 (fromIntegral ax) (fromIntegral ay) 0 0, V4 (fromIntegral bx) (fromIntegral by) 0.5 0, V4 (fromIntegral cx) (fromIntegral cy) 1 1 ]
+
+glyphVertices :: Typeface -> O.Glyph Int -> [V4 Float]
+glyphVertices typeface = (>>= uncurry triangleVertices) . (>>= pathTriangles (0, V2 0 0, V2 0 0)) . glyphPaths typeface
+
+
+encodePath :: Path V2 O.FWord -> [Word8]
+encodePath = go . fmap (word16Bytes . fromIntegral)
+  where go path = case path of
+          M (V2 x y)            rest -> moveTo  : x ++ y             ++ go rest
+          L (V2 x y)            rest -> lineTo  : x ++ y             ++ go rest
+          Q (V2 x y) (V2 x' y') rest -> curveTo : x ++ y ++ x' ++ y' ++ go rest
+          _                          -> close   : []
+        (moveTo, lineTo, curveTo, close) = (0, 1, 2, 3)
+
+word16Bytes :: Word16 -> [Word8]
+word16Bytes x = [ fromIntegral $ x .&. 0xFF, fromIntegral $ (x .&. 0xFF00) `shiftR` 8 ]
+
+word32Bytes :: Word32 -> [Word8]
+word32Bytes x = [ fromIntegral $ x .&. 0xFF, fromIntegral $ (x .&. 0xFF00) `shiftR` 8, fromIntegral $ (x .&. 0xFF0000) `shiftR` 16, fromIntegral $ (x .&. 0xFF000000) `shiftR` 24 ]
+
+
+encodeGlyphPaths :: Typeface -> O.Glyph Int -> [Word8]
+encodeGlyphPaths typeface = (>>= encodePath) . glyphPaths typeface
+
+
+encodeGlyphsForChars :: Typeface -> [Char] -> [Word8]
+encodeGlyphsForChars face chars = header ++ glyphHeaders ++ (charsGlyphsAndPaths >>= \ (_, _, path) -> path)
+  where charsGlyphsAndPaths = zip chars (glyphsForChars face chars) >>= \ (char, glyph) -> (,,) char <$> toList glyph <*> fmap (encodeGlyphPaths face) (toList glyph)
+        header = word16Bytes (unitsPerEm face) ++ word16Bytes (fromIntegral (ascent face)) ++ word16Bytes (fromIntegral (descent face)) ++ word16Bytes (fromIntegral (length charsGlyphsAndPaths))
+        glyphHeaders = snd (foldl encodeGlyphHeader (0, id) charsGlyphsAndPaths) []
+        encodeGlyphHeader (offset, makeList) (char, glyph, path) = (offset + fromIntegral (length path), makeList . (++ (word16Bytes (fromIntegral (ord char)) ++ word16Bytes (O.advanceWidth glyph) ++ word32Bytes offset ++ word16Bytes (fromIntegral (length path)))))
