db69ac568d07306f8087d4ae8ffa0c2a7f8b8e1b
Rob Rix <rob.rix@me.com>
2018-08-19 12:06:44 -0400

    Define some default colours.

--- src/UI/Colour.hs
+++ src/UI/Colour.hs
@@ -6,6 +6,16 @@ import Linear.V4
 
 type Colour = V4
 
+black :: Num a => Colour a
+black = V4 0 0 0 1
+
+white :: Num a => Colour a
+white = V4 1 1 1 1
+
+transparent :: Num a => Colour a
+transparent = V4 0 0 0 0
+
+
 opaque :: Num a => Colour a -> Colour a
 opaque = set _w 1
 
