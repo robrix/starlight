a96a7aa61aad980a4b0b3f95fc0c80a297db5b15
Rob Rix <rob.rix@me.com>
2018-08-19 12:02:03 -0400

    Set the colour to transparent.

--- app/Main.hs
+++ app/Main.hs
@@ -135,7 +135,7 @@ main = do
                 (1 - fromIntegral (floor   (minY instanceBounds') :: Int) / fromIntegral height)
 
           setUniformValue textProgram rect rect'
-          setUniformValue textProgram colour (V4 0 0 0 1)
+          setUniformValue textProgram colour (V4 0 0 0 0)
           glActiveTexture GL_TEXTURE0
           glBindTexture GL_TEXTURE_2D (unTexture texture)
           setUniformValue textProgram sampler (TextureUnit 0)
