963a52f160f175a1ad30dd44e5b1c3e75baa8693
Rob Rix <rob.rix@me.com>
2018-08-25 10:14:56 -0400

    Rename glUniform to uniform.

--- src/GL/Uniform.hs
+++ src/GL/Uniform.hs
@@ -17,24 +17,24 @@ import Linear.V4 as Linear
 newtype Var t = Var { varName :: String }
 
 class Uniform t where
-  glUniform :: HasCallStack => GLint -> t -> IO ()
+  uniform :: HasCallStack => GLint -> t -> IO ()
 
 setUniformValue :: (Uniform t, HasCallStack) => Program -> Var t -> t -> IO ()
 setUniformValue program var v = do
   location <- checkingGLError $ C.withCString (varName var) (glGetUniformLocation (unProgram program))
-  glUniform location v
+  uniform location v
 
 instance Uniform (Linear.V4 Float) where
-  glUniform location (Linear.V4 x y z w) = checkingGLError $ glUniform4f location x y z w
+  uniform location (Linear.V4 x y z w) = checkingGLError $ glUniform4f location x y z w
 
 instance Uniform (Linear.V4 Double) where
-  glUniform location (Linear.V4 x y z w) = checkingGLError $ glUniform4d location x y z w
+  uniform location (Linear.V4 x y z w) = checkingGLError $ glUniform4d location x y z w
 
 instance Uniform (Linear.M44 Float) where
-  glUniform location matrix = checkingGLError $ A.withArray (toList (Linear.transpose matrix) >>= toList) (glUniformMatrix4fv location 1 GL_FALSE . castPtr)
+  uniform location matrix = checkingGLError $ A.withArray (toList (Linear.transpose matrix) >>= toList) (glUniformMatrix4fv location 1 GL_FALSE . castPtr)
 
 instance Uniform (Linear.M33 Float) where
-  glUniform location matrix = checkingGLError $ A.withArray (toList (Linear.transpose matrix) >>= toList) (glUniformMatrix3fv location 1 GL_FALSE . castPtr)
+  uniform location matrix = checkingGLError $ A.withArray (toList (Linear.transpose matrix) >>= toList) (glUniformMatrix3fv location 1 GL_FALSE . castPtr)
 
 instance Uniform TextureUnit where
-  glUniform location textureUnit = checkingGLError $ glUniform1i location (unTextureUnit textureUnit)
+  uniform location textureUnit = checkingGLError $ glUniform1i location (unTextureUnit textureUnit)
