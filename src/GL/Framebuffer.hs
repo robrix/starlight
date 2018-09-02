652ca64cebf55fcb8b20e21f90ea7b9ec23fb471
Rob Rix <rob.rix@me.com>
2018-09-02 11:53:00 -0400

    Define a helper to bind a framebuffrer.

--- src/GL/Framebuffer.hs
+++ src/GL/Framebuffer.hs
@@ -1,5 +1,6 @@
 module GL.Framebuffer where
 
+import GL.Error
 import GL.Object
 import Graphics.GL.Core41
 import Graphics.GL.Types
@@ -7,3 +8,7 @@ import Graphics.GL.Types
 newtype Framebuffer = Framebuffer { unFramebuffer :: GLuint }
 
 instance Object Framebuffer where characterize = (Framebuffer, glGenFramebuffers, glDeleteFramebuffers)
+
+
+bindFramebuffer :: Framebuffer -> IO ()
+bindFramebuffer = checkingGLError . glBindFramebuffer GL_FRAMEBUFFER . unFramebuffer
