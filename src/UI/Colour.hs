a244e0d7b974484ea9115928e975c88fc0c7ef70
Rob Rix <rob.rix@me.com>
2018-09-02 16:00:33 -0400

    Define red/green/blue bindings.

--- src/UI/Colour.hs
+++ src/UI/Colour.hs
@@ -16,6 +16,16 @@ transparent :: Num a => Colour a
 transparent = V4 0 0 0 0
 
 
+red :: Num a => Colour a
+red = V4 1 0 0 1
+
+green :: Num a => Colour a
+green = V4 0 1 0 1
+
+blue :: Num a => Colour a
+blue = V4 0 0 1 1
+
+
 opaque :: Num a => Colour a -> Colour a
 opaque = set _w 1
 
