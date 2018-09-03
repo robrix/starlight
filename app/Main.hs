f1fb275741884b32cdf939ad62c0df3b9e4bbb2b
Rob Rix <rob.rix@me.com>
2018-09-03 11:29:25 -0400

    Be more precise about the format of the texture.

--- app/Main.hs
+++ app/Main.hs
@@ -69,7 +69,7 @@ main = do
       setMinFilter Texture2D Nearest
       checkingGLError $ glTexParameteri GL_TEXTURE_2D GL_TEXTURE_WRAP_S GL_CLAMP_TO_EDGE
       checkingGLError $ glTexParameteri GL_TEXTURE_2D GL_TEXTURE_WRAP_T GL_CLAMP_TO_EDGE
-      checkingGLError $ glTexImage2D GL_TEXTURE_2D 0 GL_RGBA width height 0 GL_RGBA GL_UNSIGNED_BYTE nullPtr
+      checkingGLError $ glTexImage2D GL_TEXTURE_2D 0 GL_RGBA8 width height 0 GL_RGBA GL_UNSIGNED_BYTE nullPtr
       bindTexture Texture2D Nothing
 
       bindFramebuffer (Just framebuffer)
