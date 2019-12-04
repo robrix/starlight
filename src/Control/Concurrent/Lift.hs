011cafb499a0c1edb0a7cef6e57940a567fb3f5c
Rob Rix <rob.rix@me.com>
2019-12-04 18:35:10 -0500

    Generalize runInBoundThread to Lift IO.

--- src/Control/Concurrent/Lift.hs
+++ src/Control/Concurrent/Lift.hs
@@ -1,2 +1,9 @@
 module Control.Concurrent.Exts
-() where
+( runInBoundThread
+) where
+
+import qualified Control.Concurrent as CC
+import Control.Effect.Lift
+
+runInBoundThread :: Has (Lift IO) sig m => m a -> m a
+runInBoundThread m = liftWith $ \ ctx hdl -> CC.runInBoundThread (hdl (m <$ ctx))
