cab608bbfe0f3e51fc578fb2f5613488bd021dff
Rob Rix <rob.rix@me.com>
2019-12-04 17:49:04 -0500

    Copy in some exception abstraction stuff.

--- src/Control/Exception/Exts.hs
+++ src/Control/Exception/Exts.hs
@@ -1,5 +1,50 @@
+{-# LANGUAGE RankNTypes #-}
 module Control.Exception.Exts
 ( E.Exception(..)
+, throwIO
+, catch
+, mask
+, bracket
+, finally
+, onException
 ) where
 
-import Control.Exception as E
+import Control.Effect.Lift
+import qualified Control.Exception as E
+
+-- | See @"Control.Exception".'E.throwIO'@.
+throwIO :: (E.Exception e, Has (Lift IO) sig m) => e -> m a
+throwIO = sendM . E.throwIO
+
+-- | See @"Control.Exception".'E.catch'@.
+catch :: (E.Exception e, Has (Lift IO) sig m) => m a -> (e -> m a) -> m a
+catch m h = liftWith $ \ ctx run -> run (m <$ ctx) `E.catch` (run . (<$ ctx) . h)
+
+-- | See @"Control.Exception".'E.mask'@.
+mask :: Has (Lift IO) sig m => ((forall a . m a -> m a) -> m b) -> m b
+mask with = liftWith $ \ ctx run -> E.mask $ \ restore ->
+  run (with (\ m -> liftWith $ \ ctx' run' -> restore (run' (m <$ ctx'))) <$ ctx)
+
+-- | See @"Control.Exception".'E.bracket'@.
+bracket
+  :: Has (Lift IO) sig m
+  => m a
+  -> (a -> m b)
+  -> (a -> m c)
+  -> m c
+bracket acquire release m = mask $ \ restore -> do
+  a <- acquire
+  r <- restore (m a) `onException` release a
+  r <$ release a
+
+-- | See @"Control.Exception".'E.finally'@.
+finally
+  :: Has (Lift IO) sig m
+  => m a
+  -> m b
+  -> m a
+finally m sequel = mask $ \ restore -> (restore m `onException` sequel) <* sequel
+
+-- | See @"Control.Exception".'E.onException'@.
+onException :: Has (Lift IO) sig m => m a -> m b -> m a
+onException io what = io `catch` \e -> what >> throwIO (e :: E.SomeException)
