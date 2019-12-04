5188ce22651ab10e6cf7ffbb276856958aca448c
Rob Rix <rob.rix@me.com>
2019-12-04 18:36:14 -0500

    :memo: runInBoundThread.

--- src/Control/Concurrent/Lift.hs
+++ src/Control/Concurrent/Lift.hs
@@ -5,5 +5,6 @@ module Control.Concurrent.Exts
 import qualified Control.Concurrent as CC
 import Control.Effect.Lift
 
+-- | See @"Control.Concurrent".'CC.runInBoundThread'@.
 runInBoundThread :: Has (Lift IO) sig m => m a -> m a
 runInBoundThread m = liftWith $ \ ctx hdl -> CC.runInBoundThread (hdl (m <$ ctx))
