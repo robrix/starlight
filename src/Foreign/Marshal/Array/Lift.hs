7378b8275b9ed56b4c2af121f888d7d3e55cd6f1
Rob Rix <rob.rix@me.com>
2019-12-06 18:43:22 -0500

    Generalize peekArray to Lift IO.

--- src/Foreign/Marshal/Array/Lift.hs
+++ src/Foreign/Marshal/Array/Lift.hs
@@ -1,5 +1,6 @@
 module Foreign.Marshal.Array.Lift
 ( allocaArray
+, peekArray
 ) where
 
 import Control.Carrier.Lift
@@ -9,3 +10,6 @@ import Foreign.Storable
 
 allocaArray :: (Has (Lift IO) sig m, Storable a) => Int -> (Ptr a -> m b) -> m b
 allocaArray n with = liftWith $ \ ctx hdl -> A.allocaArray n (hdl . (<$ ctx) . with)
+
+peekArray :: (Has (Lift IO) sig m, Storable a) => Int -> Ptr a -> m [a]
+peekArray n = sendM . A.peekArray n
