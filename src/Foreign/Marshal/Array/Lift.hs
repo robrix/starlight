fb7f87db9635cf1ee7cfd9907227053f044712e7
Rob Rix <rob.rix@me.com>
2019-12-06 18:39:47 -0500

    Generalize allocaArray to Lift IO.

--- src/Foreign/Marshal/Array/Lift.hs
+++ src/Foreign/Marshal/Array/Lift.hs
@@ -1,2 +1,11 @@
 module Foreign.Marshal.Array.Lift
-() where
+( allocaArray
+) where
+
+import Control.Carrier.Lift
+import qualified Foreign.Marshal.Array as A
+import Foreign.Ptr
+import Foreign.Storable
+
+allocaArray :: (Has (Lift IO) sig m, Storable a) => Int -> (Ptr a -> m b) -> m b
+allocaArray n with = liftWith $ \ ctx hdl -> A.allocaArray n (hdl . (<$ ctx) . with)
