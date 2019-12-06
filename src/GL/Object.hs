b937be7236c14ebd14ed7ad280a37fe303593b44
Rob Rix <rob.rix@me.com>
2019-12-06 18:55:41 -0500

    Generalize withN to Lift IO/MonadIO.

--- src/GL/Object.hs
+++ src/GL/Object.hs
@@ -1,12 +1,14 @@
+{-# LANGUAGE FlexibleContexts #-}
 module GL.Object
 ( Object(..)
 , withN
 , with
 ) where
 
-import qualified Control.Exception as E
+import Control.Carrier.Lift
+import qualified Control.Exception.Lift as E
 import Control.Monad.IO.Class
-import qualified Foreign.Marshal.Array as A
+import qualified Foreign.Marshal.Array.Lift as A
 import Foreign.Ptr
 import Foreign.Storable
 import Graphics.GL.Types
@@ -15,7 +17,7 @@ class Storable t => Object t where
   gen :: MonadIO m => GLsizei -> Ptr t -> m ()
   delete :: MonadIO m => GLsizei -> Ptr t -> m ()
 
-withN :: Object t => Int -> ([t] -> IO a) -> IO a
+withN :: (MonadIO m, Has (Lift IO) sig m, Object t) => Int -> ([t] -> m a) -> m a
 withN n = E.bracket acquire release where
   acquire = A.allocaArray n $ \ p -> do
     gen (fromIntegral n) p
