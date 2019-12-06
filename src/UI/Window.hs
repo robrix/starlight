ba9554796e431b6ead821ac5b459db86ac10574d
Rob Rix <rob.rix@me.com>
2019-12-06 00:02:49 -0500

    Exit the loop without throwing.

--- src/UI/Window.hs
+++ src/UI/Window.hs
@@ -6,26 +6,25 @@ module UI.Window
 import Control.Carrier.Lift
 import qualified Control.Concurrent.Lift as CC
 import qualified Control.Exception.Lift as E
-import Control.Monad
 import Control.Monad.IO.Class
+import Data.Function (fix)
 import Data.Text (Text)
 import Linear.V2 as Linear
 import Linear.V4 as Linear
 import SDL.Event
 import SDL.Init
 import SDL.Video
-import System.Exit
 
 withWindow :: (Has (Lift IO) sig m, MonadIO m) => Text -> Linear.V2 Int -> ((m () -> m ()) -> m a) -> m a
 withWindow name size action = withSDL $
   withSDLWindow name size $ \ window ->
     withGLContext window $ \ _ ->
-      action (\ draw -> forever $ do
+      action $ \ draw -> fix $ \ loop -> do
         draw
         Event _ payload <- waitEvent
         case payload of
-          QuitEvent -> liftIO exitSuccess
-          _ -> glSwapWindow window) where
+          QuitEvent -> pure ()
+          _ -> glSwapWindow window *> loop
 
 withSDL :: (Has (Lift IO) sig m, MonadIO m) => m a -> m a
 withSDL = CC.runInBoundThread . E.bracket_ initializeAll quit
