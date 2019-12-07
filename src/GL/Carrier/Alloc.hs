b99122245d29d7bda3b29c37522f6c6505ef66c5
Rob Rix <rob.rix@me.com>
2019-12-07 15:42:32 -0500

    Define AllocC w/o bracketing.

--- src/GL/Carrier/Alloc.hs
+++ src/GL/Carrier/Alloc.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE FlexibleInstances, GeneralizedNewtypeDeriving, LambdaCase, MultiParamTypeClasses, TypeOperators, UndecidableInstances #-}
+{-# LANGUAGE FlexibleContexts, FlexibleInstances, GeneralizedNewtypeDeriving, LambdaCase, MultiParamTypeClasses, TypeOperators, UndecidableInstances #-}
 module GL.Carrier.Alloc
 ( -- * Alloc carrier
   runAlloc
@@ -8,18 +8,29 @@ module GL.Carrier.Alloc
 ) where
 
 import Control.Algebra
-import Control.Carrier.Lift
-import Control.Monad.IO.Class
+import Control.Carrier.State.IORef
+import qualified Control.Exception.Lift as E
+import Control.Monad.IO.Class.Lift
+import Data.Foldable (sequenceA_)
+import Data.IORef
+import qualified Foreign.Marshal.Array.Lift as A
 import GL.Effect.Alloc
 import qualified GL.Object as GL
 
-runAlloc :: AllocC m a -> m a
-runAlloc (AllocC m) = m
+runAlloc :: Has (Lift IO) sig m => AllocC m a -> m a
+runAlloc (AllocC m) = do
+  ref <- sendM (newIORef [])
+  runStateRef ref m `E.finally` (sendM (readIORef ref) >>= sequenceA_)
 
-newtype AllocC m a = AllocC (m a)
+newtype AllocC m a = AllocC (StateC [m ()] m a)
   deriving (Applicative, Functor, Monad, MonadIO)
 
 instance Has (Lift IO) sig m => Algebra (Alloc :+: sig) (AllocC m) where
   alg = \case
-    L (Gen n k) -> GL.withN n k
+    L (Gen n k) -> do
+      bs <- acquire
+      k bs where
+      acquire = A.allocaArray n $ \ p -> runLiftIO $ do
+        GL.gen (fromIntegral n) p
+        A.peekArray n p
     R other     -> AllocC (send (handleCoercible other))
