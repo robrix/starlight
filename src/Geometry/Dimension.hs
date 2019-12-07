f984315fcba7407fd40e8eb1601a58c4e00f2f2a
Rob Rix <rob.rix@me.com>
2019-12-07 18:29:30 -0500

    Define a Dimension instance for V4.

--- src/Geometry/Dimension.hs
+++ src/Geometry/Dimension.hs
@@ -7,6 +7,7 @@ import Linear.V0
 import Linear.V1
 import Linear.V2
 import Linear.V3
+import Linear.V4
 
 -- | Finite, fixed-length containers.
 class Dimension (v :: * -> *) where
@@ -23,3 +24,6 @@ instance Dimension V2 where
 
 instance Dimension V3 where
   dimension _ = 3
+
+instance Dimension V4 where
+  dimension _ = 4
