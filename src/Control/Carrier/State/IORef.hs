7f3e43659ab6715ff3f537f5409279431358d18d
Rob Rix <rob.rix@me.com>
2019-12-07 15:10:58 -0500

    Define a handler for StateC.

--- src/Control/Carrier/State/IORef.hs
+++ src/Control/Carrier/State/IORef.hs
@@ -1,12 +1,20 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module Control.Carrier.State.IORef
 ( -- * State carrier
-  StateC(..)
+  runState
+, StateC(..)
 ) where
 
 import Control.Carrier.Reader
-import Control.Monad.IO.Class
+import Control.Monad.IO.Class.Lift
 import Data.IORef
+
+runState :: Has (Lift IO) sig m => s -> StateC s m a -> m (s, a)
+runState s (StateC m) = do
+  ref <- sendM (newIORef s)
+  a <- runReader ref m
+  s' <- sendM (readIORef ref)
+  pure (s', a)
 
 newtype StateC s m a = StateC (ReaderC (IORef s) m a)
   deriving (Applicative, Functor, Monad, MonadIO)
