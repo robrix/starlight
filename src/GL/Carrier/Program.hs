8167d39b3679435322f8ba2eb14185a9861ec109
Rob Rix <rob.rix@me.com>
2019-12-07 04:01:15 -0500

    Define a carrier type for Program.

--- src/GL/Carrier/Program.hs
+++ src/GL/Carrier/Program.hs
@@ -1,2 +1,24 @@
+{-# LANGUAGE ExplicitForAll, GeneralizedNewtypeDeriving, PolyKinds #-}
 module GL.Carrier.Program
-() where
+( -- * Program carrier
+  runProgram
+, ProgramC(..)
+  -- * Program effect
+, module GL.Effect.Program
+) where
+
+import Control.Carrier.Lift
+import Control.Carrier.Reader
+import Control.Monad.IO.Class
+import GL.Effect.Program
+import GL.Shader
+import qualified GL.Program as GL
+
+runProgram :: forall name sig m a . Has (Lift IO) sig m => [(ShaderType, FilePath)] -> ProgramC name m a -> m a
+runProgram shaders (ProgramC m) = do
+  shaders <- traverse (traverse (sendM . readFile)) shaders
+  GL.withBuiltProgram shaders $ \ program ->
+    runReader program m
+
+newtype ProgramC name m a = ProgramC (ReaderC GL.Program m a)
+  deriving (Applicative, Functor, Monad, MonadIO)
