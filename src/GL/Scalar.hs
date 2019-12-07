d31b0737f54ffd21d773a7f33ce24e999d2a1554
Rob Rix <rob.rix@me.com>
2019-12-07 18:43:50 -0500

    Generalize glType to other proxies.

--- src/GL/Scalar.hs
+++ src/GL/Scalar.hs
@@ -2,13 +2,12 @@ module GL.Scalar
 ( Scalar(..)
 ) where
 
-import Data.Proxy
 import qualified Foreign.Storable as S
 import Graphics.GL.Core41
 import Graphics.GL.Types
 
 class (Num n, S.Storable n) => Scalar n where
-  glType :: Proxy n -> GLenum
+  glType :: proxy n -> GLenum
 
 instance Scalar Float where
   glType _ = GL_FLOAT
