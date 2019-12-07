970a8e1a0749ff58841c65d1fa17de97ec69c57b
Rob Rix <rob.rix@me.com>
2019-12-06 19:18:02 -0500

    Generalize alloca to Lift IO.

--- src/Foreign/Marshal/Alloc/Lift.hs
+++ src/Foreign/Marshal/Alloc/Lift.hs
@@ -1,2 +1,11 @@
 module Foreign.Marshal.Alloc.Lift
-() where
+( alloca
+) where
+
+import Control.Carrier.Lift
+import qualified Foreign.Marshal.Alloc as A
+import Foreign.Ptr
+import Foreign.Storable
+
+alloca :: (Has (Lift IO) sig m, Storable a) => (Ptr a -> m b) -> m b
+alloca with = liftWith $ \ ctx hdl -> A.alloca (hdl . (<$ ctx) . with)
