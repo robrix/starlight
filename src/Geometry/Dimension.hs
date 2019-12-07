9ce29681f3c417496b8abd6276256231557677d3
Rob Rix <rob.rix@me.com>
2019-12-07 18:31:40 -0500

    Revert "Define a Dimension instance for V3."
    This reverts commit 11b41df97451a0c91eb869b9cbc433b77706e2c2.

--- src/Geometry/Dimension.hs
+++ src/Geometry/Dimension.hs
@@ -6,7 +6,6 @@ module Geometry.Dimension
 import Linear.V0
 import Linear.V1
 import Linear.V2
-import Linear.V3
 
 -- | Finite, fixed-length containers.
 class Dimension (v :: * -> *) where
@@ -20,6 +19,3 @@ instance Dimension V1 where
 
 instance Dimension V2 where
   dimension _ = 2
-
-instance Dimension V3 where
-  dimension _ = 3
