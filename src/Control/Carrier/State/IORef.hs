492eed92cbe662a8114b0294cd7a8d530b4a8aa7
Rob Rix <rob.rix@me.com>
2019-12-07 15:08:09 -0500

    Derive some instances.

--- src/Control/Carrier/State/IORef.hs
+++ src/Control/Carrier/State/IORef.hs
@@ -1,9 +1,12 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module Control.Carrier.State.IORef
 ( -- * State carrier
   StateC(..)
 ) where
 
 import Control.Carrier.Reader
+import Control.Monad.IO.Class
 import Data.IORef
 
 newtype StateC s m a = StateC (ReaderC (IORef s) m a)
+  deriving (Applicative, Functor, Monad, MonadIO)
