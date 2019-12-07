9111c54b28e230111da4e92f2a6c356eb63cc959
Rob Rix <rob.rix@me.com>
2019-12-06 19:18:54 -0500

    Generalize allocaBytes to Lift IO.

--- src/Foreign/Marshal/Alloc/Lift.hs
+++ src/Foreign/Marshal/Alloc/Lift.hs
@@ -1,5 +1,6 @@
 module Foreign.Marshal.Alloc.Lift
 ( alloca
+, allocaBytes
 ) where
 
 import Control.Carrier.Lift
@@ -9,3 +10,6 @@ import Foreign.Storable
 
 alloca :: (Has (Lift IO) sig m, Storable a) => (Ptr a -> m b) -> m b
 alloca with = liftWith $ \ ctx hdl -> A.alloca (hdl . (<$ ctx) . with)
+
+allocaBytes :: Has (Lift IO) sig m => Int -> (Ptr a -> m b) -> m b
+allocaBytes n with = liftWith $ \ ctx hdl -> A.allocaBytes n (hdl . (<$ ctx) . with)
