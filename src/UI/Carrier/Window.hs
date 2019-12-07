22ae1caa8637200e949776bd66286648dbcc0ac2
Rob Rix <rob.rix@me.com>
2019-12-07 04:54:19 -0500

    Define a handler for WindowC.

--- src/UI/Carrier/Window.hs
+++ src/UI/Carrier/Window.hs
@@ -1,13 +1,26 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module UI.Carrier.Window
 ( -- * Window carrier
-  WindowC(..)
+  runWindow
+, WindowC(..)
   -- * Window effect
 , module UI.Effect.Window
 ) where
 
+import Control.Carrier.Lift
+import Control.Carrier.Reader
 import Control.Monad.IO.Class
+import Data.Text (Text)
+import Linear.V2
 import UI.Effect.Window
+import qualified UI.Window as UI
 
-newtype WindowC m a = WindowC (m a)
+
+runWindow :: Has (Lift IO) sig m => Text -> V2 Int -> WindowC m a -> m a
+runWindow name size (WindowC m) = UI.withSDL $
+  UI.withSDLWindow name size $ \ window ->
+    UI.withGLContext window $ \ _ ->
+      runReader window m
+
+newtype WindowC m a = WindowC (ReaderC UI.Window m a)
   deriving (Applicative, Functor, Monad, MonadIO)
