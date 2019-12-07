11068c598ecd32b9ba6520cae26af258b0b4eadd
Rob Rix <rob.rix@me.com>
2019-12-07 05:50:58 -0500

    Carry per-file timestamps around.

--- src/GL/Carrier/Program/Live.hs
+++ src/GL/Carrier/Program/Live.hs
@@ -10,8 +10,10 @@ module GL.Carrier.Program.Live
 import Control.Algebra
 import Control.Carrier.Lift
 import Control.Carrier.Reader
+import Control.Carrier.State.Strict
 import Control.Monad.IO.Class
 import Control.Monad.Trans.Class
+import Data.Time.Clock (UTCTime)
 import GL.Effect.Program
 import GL.Shader
 import qualified GL.Program as GL
@@ -19,14 +21,17 @@ import GL.Uniform
 
 runProgram :: forall name sig m a . Has (Lift IO) sig m => [(ShaderType, FilePath)] -> ProgramC name m a -> m a
 runProgram shaders (ProgramC m) = do
-  shaders <- traverse (traverse (sendM . readFile)) shaders
-  GL.withBuiltProgram shaders $ \ program ->
-    runReader program m
+  shaders' <- traverse (traverse (sendM . readFile)) shaders
+  GL.withBuiltProgram shaders' $ \ program ->
+    runReader program (evalState (map (\ (_, p) -> (p, Nothing)) shaders) m)
 
-newtype ProgramC name m a = ProgramC (ReaderC GL.Program m a)
-  deriving (Applicative, Functor, Monad, MonadIO, MonadTrans)
+newtype ProgramC name m a = ProgramC (StateC [(FilePath, Maybe UTCTime)] (ReaderC GL.Program m) a)
+  deriving (Applicative, Functor, Monad, MonadIO)
 
-instance Has (Lift IO) sig m => Algebra (Program name :+: sig) (ProgramC name m) where
+instance MonadTrans (ProgramC name) where
+  lift = ProgramC . lift . lift
+
+instance (Has (Lift IO) sig m, Effect sig) => Algebra (Program name :+: sig) (ProgramC name m) where
   alg = \case
     L (Use k)     -> ProgramC ask >>= GL.useProgram >> k
     L (Set v a k) -> ProgramC ask >>= \ p -> setUniformValue p v a >> k
