e16eadcc19ff2b451a5c37e37b94f9dc85a9fd67
Rob Rix <rob.rix@me.com>
2019-12-07 14:30:30 -0500

    Stub in an Alloc carrier.

--- src/GL/Carrier/Alloc.hs
+++ src/GL/Carrier/Alloc.hs
@@ -1,6 +1,10 @@
 module GL.Carrier.Alloc
-( -- * Alloc effect
-  module GL.Effect.Alloc
+( -- * Alloc carrier
+  AllocC(..)
+  -- * Alloc effect
+, module GL.Effect.Alloc
 ) where
 
 import GL.Effect.Alloc
+
+newtype AllocC m a = AllocC (m a)
