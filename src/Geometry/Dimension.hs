788c98b414bbaec64c88b9f8344ee7f0d128d4ff
Rob Rix <rob.rix@me.com>
2019-12-07 18:28:16 -0500

    Define a Dimension instance for V0.

--- src/Geometry/Dimension.hs
+++ src/Geometry/Dimension.hs
@@ -3,6 +3,11 @@ module Geometry.Dimension
 ( Dimension(..)
 ) where
 
+import Linear.V0
+
 -- | Finite, fixed-length containers.
 class Dimension (v :: * -> *) where
   dimension :: proxy v -> Int
+
+instance Dimension V0 where
+  dimension _ = 0
