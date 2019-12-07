4852a949353d4da8e94c04d6cb34176ce1608cbb
Rob Rix <rob.rix@me.com>
2019-12-06 19:40:28 -0500

    Generalize withArray to Lift IO.

--- src/GL/Array.hs
+++ src/GL/Array.hs
@@ -12,7 +12,7 @@ import Control.Monad.IO.Class.Lift
 import Data.Coerce
 import Data.Foldable (toList)
 import Data.Proxy
-import qualified Foreign.Marshal.Array as A
+import qualified Foreign.Marshal.Array.Lift as A
 import Foreign.Ptr
 import qualified Foreign.Storable as S
 import GL.Buffer
@@ -25,8 +25,8 @@ import Graphics.GL.Types
 newtype Array n = Array { unArray :: GLuint }
   deriving (S.Storable)
 
-withArray :: forall v n a. (Foldable v, Scalar n) => [v n] -> (Array n -> IO a) -> IO a
-withArray vertices body = with $ \ buffer -> do
+withArray :: forall v n m a sig . (Foldable v, Scalar n, Has (Lift IO) sig m) => [v n] -> (Array n -> m a) -> m a
+withArray vertices body = with $ \ buffer -> runLifting $ do
   glBindBuffer GL_ARRAY_BUFFER (unBuffer buffer)
   A.withArrayLen (vertices >>= toList) $ \ n p ->
     glBufferData GL_ARRAY_BUFFER (fromIntegral (n * S.sizeOf @n 0)) (castPtr p) GL_STATIC_DRAW
@@ -34,7 +34,7 @@ withArray vertices body = with $ \ buffer -> do
     bindArray array
     glEnableVertexAttribArray 0
     glVertexAttribPointer 0 (fromIntegral (length (head vertices))) (glType (Proxy @n)) GL_FALSE 0 nullPtr
-    body array
+    Lifting (body array)
 
 bindArray :: Has (Lift IO) sig m => Array n -> m ()
 bindArray = checkingGLError . runLifting . glBindVertexArray . unArray
