a4e46827e997eb508cfd14489bd4c9806438302e
Rob Rix <rob.rix@me.com>
2019-12-07 14:38:58 -0500

    Define an Algebra instance for AllocC.

--- src/GL/Carrier/Alloc.hs
+++ src/GL/Carrier/Alloc.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE FlexibleInstances, GeneralizedNewtypeDeriving, LambdaCase, MultiParamTypeClasses, TypeOperators, UndecidableInstances #-}
 module GL.Carrier.Alloc
 ( -- * Alloc carrier
   runAlloc
@@ -7,11 +7,19 @@ module GL.Carrier.Alloc
 , module GL.Effect.Alloc
 ) where
 
+import Control.Algebra
+import Control.Carrier.Lift
 import Control.Monad.IO.Class
 import GL.Effect.Alloc
+import qualified GL.Object as GL
 
 runAlloc :: AllocC m a -> m a
 runAlloc (AllocC m) = m
 
 newtype AllocC m a = AllocC (m a)
   deriving (Applicative, Functor, Monad, MonadIO)
+
+instance Has (Lift IO) sig m => Algebra (Alloc :+: sig) (AllocC m) where
+  alg = \case
+    L (Gen n k) -> GL.withN n k
+    R other     -> AllocC (send (handleCoercible other))
