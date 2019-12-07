629ad04735ca04e0a9f30bec8f97be128d131a08
Rob Rix <rob.rix@me.com>
2019-12-07 17:36:07 -0500

    Define a Bind instance for Buffer.

--- src/GL/Buffer.hs
+++ src/GL/Buffer.hs
@@ -3,8 +3,10 @@ module GL.Buffer
 ( Buffer(..)
 ) where
 
+import Control.Monad.IO.Class.Lift
 import Data.Coerce
 import Foreign.Storable
+import GL.Error
 import GL.Object
 import Graphics.GL.Core41
 import Graphics.GL.Types
@@ -15,3 +17,7 @@ newtype Buffer n = Buffer { unBuffer :: GLuint }
 instance Object (Buffer n) where
   gen n = glGenBuffers n . coerce
   delete n = glDeleteBuffers n . coerce
+
+instance Bind (Buffer n) where
+  nullObject = Buffer 0
+  bindObject = checkingGLError . runLiftIO . glBindBuffer GL_ARRAY_BUFFER . unBuffer
