3d29e54c0916fc5b926b6ad1046743196e735a9c
Rob Rix <rob.rix@me.com>
2019-12-07 18:31:47 -0500

    Revert "Define a Dimension instance for V0."
    This reverts commit 788c98b414bbaec64c88b9f8344ee7f0d128d4ff.

--- src/Geometry/Dimension.hs
+++ src/Geometry/Dimension.hs
@@ -3,11 +3,6 @@ module Geometry.Dimension
 ( Dimension(..)
 ) where
 
-import Linear.V0
-
 -- | Finite, fixed-length containers.
 class Dimension (v :: * -> *) where
   dimension :: proxy v -> Int
-
-instance Dimension V0 where
-  dimension _ = 0
