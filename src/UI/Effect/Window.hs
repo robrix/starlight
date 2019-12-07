d47ea0771ef147f2d8950702799ddf99c33787bd
Rob Rix <rob.rix@me.com>
2019-12-07 04:40:37 -0500

    Define an Effect instance for Window.

--- src/UI/Effect/Window.hs
+++ src/UI/Effect/Window.hs
@@ -17,3 +17,6 @@ deriving instance Functor m => Functor (Window m)
 
 instance HFunctor Window where
   hmap f (Draw m k) = Draw (f m) (f . k)
+
+instance Effect Window where
+  thread ctx hdl (Draw m k) = Draw (hdl (m <$ ctx)) (hdl . fmap k)
