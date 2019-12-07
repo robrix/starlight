417875d24bd5154a12bb37343646658e9b7d21c7
Rob Rix <rob.rix@me.com>
2019-12-07 03:16:36 -0500

    Generalize with to Lift IO.

--- src/Foreign/Marshal/Utils/Lift.hs
+++ src/Foreign/Marshal/Utils/Lift.hs
@@ -1,2 +1,11 @@
 module Foreign.Marshal.Utils.Lift
-() where
+( with
+) where
+
+import Control.Carrier.Lift
+import Foreign.Ptr
+import Foreign.Storable
+import qualified Foreign.Marshal.Utils as U
+
+with :: (Has (Lift IO) sig m, Storable a) => a -> (Ptr a -> m b) -> m b
+with a action = liftWith $ \ ctx hdl -> U.with a (hdl . (<$ ctx) . action)
