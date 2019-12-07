fe153c3dda5c4891223ac3f3234140ee973c60a5
Rob Rix <rob.rix@me.com>
2019-12-06 19:10:17 -0500

    Define a Lifting adapter for MonadIO.

--- src/Control/Monad/IO/Class/Lift.hs
+++ src/Control/Monad/IO/Class/Lift.hs
@@ -1,5 +1,18 @@
+{-# LANGUAGE FlexibleInstances, GeneralizedNewtypeDeriving, MultiParamTypeClasses, UndecidableInstances #-}
 module Control.Monad.IO.Class.Lift
-( module Control.Carrier.Lift
+( Lifting(..)
+, module Control.Carrier.Lift
 ) where
 
+import Control.Algebra
 import Control.Carrier.Lift
+import Control.Monad.IO.Class
+
+newtype Lifting m a = Lifting { runLifting :: m a }
+  deriving (Applicative, Functor, Monad)
+
+instance Has (Lift IO) sig m => MonadIO (Lifting m) where
+  liftIO = sendM
+
+instance Algebra sig m => Algebra sig (Lifting m) where
+  alg = Lifting . alg . handleCoercible
