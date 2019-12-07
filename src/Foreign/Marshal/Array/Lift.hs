aa83a8cfae50f0bde95a2e77ac1be9fc681c032d
Rob Rix <rob.rix@me.com>
2019-12-06 19:14:47 -0500

    Revert "Change the generalizations to use MonadIO where possible."
    This reverts commit 7e2a4b9ea51dcd0951a65909b8df527e03a9637f.

--- src/Foreign/Marshal/Array/Lift.hs
+++ src/Foreign/Marshal/Array/Lift.hs
@@ -5,7 +5,6 @@ module Foreign.Marshal.Array.Lift
 ) where
 
 import Control.Carrier.Lift
-import Control.Monad.IO.Class
 import qualified Foreign.Marshal.Array as A
 import Foreign.Ptr
 import Foreign.Storable
@@ -13,8 +12,8 @@ import Foreign.Storable
 allocaArray :: (Has (Lift IO) sig m, Storable a) => Int -> (Ptr a -> m b) -> m b
 allocaArray n with = liftWith $ \ ctx hdl -> A.allocaArray n (hdl . (<$ ctx) . with)
 
-peekArray :: (MonadIO m, Storable a) => Int -> Ptr a -> m [a]
-peekArray n = liftIO . A.peekArray n
+peekArray :: (Has (Lift IO) sig m, Storable a) => Int -> Ptr a -> m [a]
+peekArray n = sendM . A.peekArray n
 
-pokeArray :: (MonadIO m, Storable a) => Ptr a -> [a] -> m ()
-pokeArray p = liftIO . A.pokeArray p
+pokeArray :: (Has (Lift IO) sig m, Storable a) => Ptr a -> [a] -> m ()
+pokeArray p = sendM . A.pokeArray p
