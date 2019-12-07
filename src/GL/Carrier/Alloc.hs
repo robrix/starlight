29c8df27a3f4f71f555660e58b611d52be9dbb82
Rob Rix <rob.rix@me.com>
2019-12-07 14:31:32 -0500

    Define a handler for AllocC.

--- src/GL/Carrier/Alloc.hs
+++ src/GL/Carrier/Alloc.hs
@@ -1,13 +1,17 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module GL.Carrier.Alloc
 ( -- * Alloc carrier
-  AllocC(..)
+  runAlloc
+, AllocC(..)
   -- * Alloc effect
 , module GL.Effect.Alloc
 ) where
 
 import Control.Monad.IO.Class
 import GL.Effect.Alloc
+
+runAlloc :: AllocC m a -> m a
+runAlloc (AllocC m) = m
 
 newtype AllocC m a = AllocC (m a)
   deriving (Applicative, Functor, Monad, MonadIO)
