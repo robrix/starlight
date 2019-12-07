7f0d6ac3e9fce81259b13c9a3ac69251c1c6924b
Rob Rix <rob.rix@me.com>
2019-12-06 19:33:30 -0500

    Generalize withCompiledShader to Lift IO.

--- src/GL/Shader.hs
+++ src/GL/Shader.hs
@@ -7,7 +7,7 @@ module GL.Shader
 
 import qualified Control.Exception.Lift as E
 import Control.Monad.IO.Class.Lift
-import qualified Foreign.C.String as C
+import qualified Foreign.C.String.Lift as C
 import qualified Foreign.Marshal.Alloc.Lift as A
 import Foreign.Ptr
 import qualified Foreign.Storable as S
@@ -30,15 +30,15 @@ withShader shaderType = E.bracket
   (runLifting (Shader <$> glCreateShader (toGLEnum shaderType)))
   (runLifting . glDeleteShader . unShader)
 
-withCompiledShader :: HasCallStack => ShaderType -> String -> (Shader -> IO a) -> IO a
-withCompiledShader shaderType source body = withShader shaderType $ \ (Shader shader) -> do
+withCompiledShader :: (Has (Lift IO) sig m, HasCallStack) => ShaderType -> String -> (Shader -> m a) -> m a
+withCompiledShader shaderType source body = withShader shaderType $ \ (Shader shader) -> runLifting $ do
   C.withCString source $ \ source ->
     A.alloca $ \ p -> do
-      S.poke p source
+      sendM (S.poke p source)
       glShaderSource shader 1 p nullPtr
   glCompileShader shader
   s <- checkShader source (Shader shader)
-  body s
+  Lifting (body s)
 
 withCompiledShaders :: HasCallStack => [(ShaderType, String)] -> ([Shader] -> IO a) -> IO a
 withCompiledShaders sources body = go sources []
