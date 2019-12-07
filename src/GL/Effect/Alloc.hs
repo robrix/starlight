3d71ff45a213153469778cfcdf4665be0d5967d0
Rob Rix <rob.rix@me.com>
2019-12-07 14:20:53 -0500

    Define an HFunctor instance for Alloc.

--- src/GL/Effect/Alloc.hs
+++ src/GL/Effect/Alloc.hs
@@ -3,9 +3,13 @@ module GL.Effect.Alloc
 ( Alloc(..)
 ) where
 
+import Control.Algebra
 import GL.Object
 
 data Alloc m k
   = forall t . Object t => Gen (t -> m k)
 
 deriving instance Functor m => Functor (Alloc m)
+
+instance HFunctor Alloc where
+  hmap f (Gen k) = Gen (f . k)
