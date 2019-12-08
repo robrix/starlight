c7f48f330eb069eb05c0a4a6e903ef1ca400d3d1
Rob Rix <rob.rix@me.com>
2019-12-08 12:38:47 -0500

    Define a handler for Finally.

--- src/Control/Carrier/Finally.hs
+++ src/Control/Carrier/Finally.hs
@@ -1,14 +1,23 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module Control.Carrier.Finally
 ( -- * Finally carrier
-  FinallyC(..)
+  runFinally
+, FinallyC(..)
   -- * Finally effect
 , module Control.Effect.Finally
 ) where
 
 import Control.Carrier.State.IORef
 import Control.Effect.Finally
-import Control.Monad.IO.Class
+import qualified Control.Exception.Lift as E
+import Control.Monad.IO.Class.Lift
+import Data.Foldable (sequenceA_)
+import Data.IORef
+
+runFinally :: Has (Lift IO) sig m => FinallyC m a -> m a
+runFinally (FinallyC m) = do
+  ref <- sendM (newIORef [])
+  runStateRef ref m `E.finally` (sendM (readIORef ref) >>= sequenceA_)
 
 newtype FinallyC m a = FinallyC (StateC [m ()] m a)
   deriving (Applicative, Functor, Monad, MonadIO)
