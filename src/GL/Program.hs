6834e220f2dd98798e85dfb306e103167a999c35
Rob Rix <rob.rix@me.com>
2019-12-08 14:31:08 -0500

    :fire: with*Program.

--- src/GL/Program.hs
+++ src/GL/Program.hs
@@ -4,10 +4,7 @@ module GL.Program
 , (:::)(..)
 , createProgram
 , useProgram
-, withProgram
-, withLinkedProgram
 , link
-, withBuiltProgram
 , checkProgram
   -- * Uniforms
 , Var(..)
@@ -16,7 +13,6 @@ module GL.Program
 ) where
 
 import Control.Effect.Finally
-import qualified Control.Exception.Lift as E
 import Control.Monad.IO.Class.Lift
 import Data.Foldable (for_)
 import Data.Proxy
@@ -45,25 +41,12 @@ createProgram = do
 useProgram :: Has (Lift IO) sig m => Program ty -> m ()
 useProgram = runLiftIO . glUseProgram . unProgram
 
-withProgram :: Has (Lift IO) sig m => (Program ty -> m a) -> m a
-withProgram = E.bracket
-  (runLiftIO (Program <$> glCreateProgram))
-  (runLiftIO . glDeleteProgram . unProgram)
-
-withLinkedProgram :: (Has (Lift IO) sig m, HasCallStack) => [Shader] -> (Program ty -> m a) -> m a
-withLinkedProgram shaders body = withProgram $ \ program -> do
-  link shaders program
-  body program
-
 link :: (Has (Lift IO) sig m, HasCallStack) => [Shader] -> Program ty -> m ()
 link shaders (Program program) = runLiftIO $ do
   for_ shaders (glAttachShader program . unShader)
   glLinkProgram program
   for_ shaders (glDetachShader program . unShader)
   checkProgram (Program program)
-
-withBuiltProgram :: (Has (Lift IO) sig m, HasCallStack) => [(ShaderType, String)] -> (Program ty -> m a) -> m a
-withBuiltProgram sources body = withCompiledShaders sources (`withLinkedProgram` body)
 
 
 checkProgram :: (Has (Lift IO) sig m, HasCallStack) => Program ty -> m ()
