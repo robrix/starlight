f576b51f36d51c3909d451e84cdb244c3d750c79
Rob Rix <rob.rix@me.com>
2019-12-08 12:06:54 -0500

    Stub in a Finalize effect.

--- src/Control/Effect/Finalize.hs
+++ src/Control/Effect/Finalize.hs
@@ -1,8 +1,13 @@
 module Control.Effect.Finalize
-( -- * Re-exports
-  Algebra
+( -- * Finalize effect
+  Finalize(..)
+  -- * Re-exports
+, Algebra
 , Has
 , run
 ) where
 
 import Control.Algebra
+
+data Finalize m k
+  = Finalize (IO ()) (m k)
