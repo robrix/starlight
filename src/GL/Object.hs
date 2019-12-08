ae9a1cf48cc58f9f7a20ec4173a03de22a4b9ca3
Rob Rix <rob.rix@me.com>
2019-12-07 20:05:22 -0500

    :fire: withN.

--- src/GL/Object.hs
+++ src/GL/Object.hs
@@ -1,15 +1,12 @@
 {-# LANGUAGE FlexibleContexts, ScopedTypeVariables #-}
 module GL.Object
 ( Object(..)
-, withN
 , Bind(..)
 , bind
 ) where
 
-import qualified Control.Exception.Lift as E
 import Control.Monad.IO.Class.Lift
 import GHC.Stack
-import qualified Foreign.Marshal.Array.Lift as A
 import Foreign.Ptr
 import Foreign.Storable
 import Graphics.GL.Types
@@ -17,14 +14,6 @@ import Graphics.GL.Types
 class Storable t => Object t where
   gen :: MonadIO m => GLsizei -> Ptr t -> m ()
   delete :: MonadIO m => GLsizei -> Ptr t -> m ()
-
-withN :: (Has (Lift IO) sig m, Object t) => Int -> ([t] -> m a) -> m a
-withN n = E.bracket acquire release where
-  acquire = A.allocaArray n $ \ p -> runLiftIO $ do
-    gen (fromIntegral n) p
-    A.peekArray n p
-  release buffers = A.withArray buffers $ runLiftIO . delete (fromIntegral n)
-
 
 class Bind t where
   nullObject :: t
