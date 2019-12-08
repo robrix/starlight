eaf29b47bb1d189b63cb90898774a9e8d2318c66
Rob Rix <rob.rix@me.com>
2019-12-08 12:47:29 -0500

    Define an Algebra instance for FinallyC.

--- src/Control/Carrier/Finally.hs
+++ src/Control/Carrier/Finally.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE FlexibleInstances, GeneralizedNewtypeDeriving, LambdaCase, MultiParamTypeClasses, TypeOperators, UndecidableInstances #-}
 module Control.Carrier.Finally
 ( -- * Finally carrier
   runFinally
@@ -7,17 +7,24 @@ module Control.Carrier.Finally
 , module Control.Effect.Finally
 ) where
 
+import Control.Algebra
 import Control.Carrier.State.IORef
 import Control.Effect.Finally
 import qualified Control.Exception.Lift as E
 import Control.Monad.IO.Class.Lift
-import Data.Foldable (sequenceA_)
+import Data.Foldable (traverse_)
+import Data.Functor (void)
 import Data.IORef
 
 runFinally :: Has (Lift IO) sig m => FinallyC m a -> m a
 runFinally (FinallyC m) = do
   ref <- sendM (newIORef [])
-  runStateRef ref m `E.finally` (sendM (readIORef ref) >>= sequenceA_)
+  runStateRef ref m `E.finally` (sendM (readIORef ref) >>= traverse_ runFinally)
 
-newtype FinallyC m a = FinallyC (StateC [m ()] m a)
+newtype FinallyC m a = FinallyC (StateC [FinallyC m ()] m a)
   deriving (Applicative, Functor, Monad, MonadIO)
+
+instance Has (Lift IO) sig m => Algebra (Finally :+: sig) (FinallyC m) where
+  alg = \case
+    L (OnExit m k) -> FinallyC (modify (void m :)) >> k
+    R other        -> FinallyC (send (handleCoercible other))
