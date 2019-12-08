e430916713fcf705096e98148e724dcb669dca5d
Rob Rix <rob.rix@me.com>
2019-12-08 12:30:12 -0500

    Rename finally to onExit.

--- src/Control/Effect/Finally.hs
+++ src/Control/Effect/Finally.hs
@@ -2,7 +2,7 @@
 module Control.Effect.Finally
 ( -- * Finally effect
   Finally(..)
-, finally
+, onExit
   -- * Re-exports
 , Algebra
 , Has
@@ -12,16 +12,16 @@ module Control.Effect.Finally
 import Control.Algebra
 
 data Finally m k
-  = forall a . Finally (m a) (m k)
+  = forall a . OnExit (m a) (m k)
 
 deriving instance Functor m => Functor (Finally m)
 
 instance HFunctor Finally where
-  hmap f (Finally m k) = Finally (f m) (f k)
+  hmap f (OnExit m k) = OnExit (f m) (f k)
 
 instance Effect Finally where
-  thread ctx hdl (Finally m k) = Finally (hdl (m <$ ctx)) (hdl (k <$ ctx))
+  thread ctx hdl (OnExit m k) = OnExit (hdl (m <$ ctx)) (hdl (k <$ ctx))
 
 
-finally :: Has Finally sig m => m () -> m ()
-finally m = send (Finally m (pure ()))
+onExit :: Has Finally sig m => m () -> m ()
+onExit m = send (OnExit m (pure ()))
