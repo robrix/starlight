6bad060c43cd533b002d3e06f3a32413c027cbcc
Rob Rix <rob.rix@me.com>
2019-12-11 00:02:51 -0500

    Derive an HFunctor instance for Time.

--- src/Control/Effect/Time.hs
+++ src/Control/Effect/Time.hs
@@ -3,9 +3,12 @@ module Control.Effect.Time
 ( Time(..)
 ) where
 
+import Control.Algebra
 import Data.Time.Clock
 import GHC.Generics (Generic1)
 
 data Time m k
   = Now (UTCTime -> m k)
   deriving (Functor, Generic1)
+
+instance HFunctor Time
