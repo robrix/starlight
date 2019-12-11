53b07be3b267b553ba264ee2212992c062e71bb2
Rob Rix <rob.rix@me.com>
2019-12-11 00:02:17 -0500

    Derive a Generic1 instance for Time.

--- src/Control/Effect/Time.hs
+++ src/Control/Effect/Time.hs
@@ -1,10 +1,11 @@
-{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE DeriveFunctor, DeriveGeneric #-}
 module Control.Effect.Time
 ( Time(..)
 ) where
 
 import Data.Time.Clock
+import GHC.Generics (Generic1)
 
 data Time m k
   = Now (UTCTime -> m k)
-  deriving (Functor)
+  deriving (Functor, Generic1)
