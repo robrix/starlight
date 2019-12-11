186963067549206bcfa3c6348716508eb5d24336
Rob Rix <rob.rix@me.com>
2019-12-11 00:11:04 -0500

    Define an Algebra instance for TimeC.

--- src/Control/Carrier/Time.hs
+++ src/Control/Carrier/Time.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE FlexibleInstances, GeneralizedNewtypeDeriving, LambdaCase, MultiParamTypeClasses, TypeOperators, UndecidableInstances #-}
 module Control.Carrier.Time
 ( -- * Time carrier
   runTime
@@ -7,8 +7,16 @@ module Control.Carrier.Time
 , module Control.Effect.Time
 ) where
 
+import Control.Algebra
+import Control.Carrier.Lift
 import Control.Effect.Time
 import Control.Monad.IO.Class
+import Data.Time.Clock
 
 newtype TimeC m a = TimeC { runTime :: m a }
   deriving (Applicative, Functor, Monad, MonadIO)
+
+instance Has (Lift IO) sig m => Algebra (Time :+: sig) (TimeC m) where
+  alg = \case
+    L (Now k) -> sendM getCurrentTime >>= k
+    R other   -> TimeC (send (handleCoercible other))
