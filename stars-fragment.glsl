2c9000c704a77e2b5f6bd12963b427a16d011a76
Rob Rix <rob.rix@me.com>
2019-12-12 22:25:37 -0500

    :fire: redundant stuff.

--- stars-fragment.glsl
+++ stars-fragment.glsl
@@ -2,22 +2,9 @@
 
 // from https://www.shadertoy.com/view/XlfGRj
 
-uniform vec3      resolution;           // viewport resolution (in pixels)
-uniform float     time;                 // shader playback time (in seconds)
-// uniform float     iTimeDelta;            // render time (in seconds)
-// uniform int       iFrame;                // shader playback frame
-// uniform float     iChannelTime[4];       // channel playback time (in seconds)
-// uniform vec3      iChannelResolution[4]; // channel resolution (in pixels)
-uniform vec4      mouse;                // mouse pixel coords. xy: current (if MLB down), zw: click
-// uniform samplerXX iChannel0..3;          // input channel. XX = 2D/Cube
-// uniform vec4      iDate;                 // (year, month, day, time in seconds)
-// uniform float     iSampleRate;           // sound sample rate (i.e., 44100)
-
-// precision highp float;
-// out vec4 fragColor;
-// void main() {
-//   fragColor = vec4(0, 0, 0, 1);
-// }
+uniform vec3 resolution; // viewport resolution (in pixels)
+uniform float time;      // shader playback time (in seconds)
+uniform vec4 mouse;      // mouse pixel coords. xy: current (if MLB down), zw: click
 
 
 // Star Nest by Pablo Roman Andrioli
