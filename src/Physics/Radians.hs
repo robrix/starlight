7c99380e217ecd0a667c4641c4eacda9fb01caee
Rob Rix <rob.rix@me.com>
2019-12-14 12:34:07 -0500

    Add a helper to construct V2 from r/phi.

--- src/Physics/Radians.hs
+++ src/Physics/Radians.hs
@@ -1,9 +1,14 @@
 {-# LANGUAGE DeriveTraversable, GeneralizedNewtypeDeriving #-}
 module Physics.Radians
 ( Radians(..)
+, cartesian2
 ) where
 
 import GL.Uniform
+import Linear.V2
 
 newtype Radians a = Radians { getRadians :: a }
   deriving (Eq, Foldable, Floating, Fractional, Functor, Num, Ord, Real, RealFloat, RealFrac, Show, Traversable, Uniform)
+
+cartesian2 :: Floating a => a -> Radians a -> V2 a
+cartesian2 r (Radians phi) = V2 (r * cos phi) (r * sin phi)
