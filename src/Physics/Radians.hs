b6173d460f5cad61a5c042f58af1fd8b74c46909
Rob Rix <rob.rix@me.com>
2019-12-14 12:37:06 -0500

    Swap the order of the parameters to cartesian2.

--- src/Physics/Radians.hs
+++ src/Physics/Radians.hs
@@ -10,5 +10,5 @@ import Linear.V2
 newtype Radians a = Radians { getRadians :: a }
   deriving (Eq, Foldable, Floating, Fractional, Functor, Num, Ord, Real, RealFloat, RealFrac, Show, Traversable, Uniform)
 
-cartesian2 :: Floating a => a -> Radians a -> V2 a
-cartesian2 r (Radians phi) = V2 (r * cos phi) (r * sin phi)
+cartesian2 :: Floating a => Radians a -> a -> V2 a
+cartesian2 (Radians phi) r = V2 (r * cos phi) (r * sin phi)
