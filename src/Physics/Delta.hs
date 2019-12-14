783eec15cdd41705843b7fce32c0ca9605d14918
Rob Rix <rob.rix@me.com>
2019-12-14 04:28:32 -0500

    Derive instances for Delta.

--- src/Physics/Delta.hs
+++ src/Physics/Delta.hs
@@ -1,5 +1,9 @@
+{-# LANGUAGE DeriveTraversable, GeneralizedNewtypeDeriving #-}
 module Physics.Delta
 ( Delta(..)
 ) where
 
+import GL.Uniform
+
 newtype Delta a = Delta { getDelta :: a }
+  deriving (Eq, Foldable, Floating, Fractional, Functor, Num, Ord, Real, RealFloat, RealFrac, Show, Traversable, Uniform)
