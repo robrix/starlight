560bad08f01831d47db86da59dfda967c97ecdb1
Rob Rix <rob.rix@me.com>
2019-12-13 23:46:37 -0500

    Reformat translated.

--- src/Linear/Exts.hs
+++ src/Linear/Exts.hs
@@ -8,9 +8,10 @@ import Linear.V2
 import Linear.V3
 
 translated :: V2 Float -> M33 Float
-translated (V2 tx ty) = V3 (V3 1 0 tx)
-                           (V3 0 1 ty)
-                           (V3 0 0 1)
+translated (V2 tx ty) = V3
+  (V3 1 0 tx)
+  (V3 0 1 ty)
+  (V3 0 0 1)
 
 rotated :: Float -> M33 Float
 rotated theta = V3
