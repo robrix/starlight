6470c43d198ee311fd639226657a004b40e347d1
Rob Rix <rob.rix@me.com>
2019-12-14 04:46:28 -0500

    Derive some instances for Seconds.

--- src/Physics/Seconds.hs
+++ src/Physics/Seconds.hs
@@ -1,5 +1,9 @@
+{-# LANGUAGE DeriveTraversable, GeneralizedNewtypeDeriving #-}
 module Physics.Seconds
 ( Seconds(..)
 ) where
 
+import GL.Uniform
+
 newtype Seconds a = Seconds { getSeconds :: a }
+  deriving (Eq, Foldable, Floating, Fractional, Functor, Num, Ord, Real, RealFloat, RealFrac, Show, Traversable, Uniform)
