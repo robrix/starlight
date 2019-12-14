82e41acd869b2fa462b0b5fbf3cf54d6c490fff6
Rob Rix <rob.rix@me.com>
2019-12-14 04:11:44 -0500

    Derive a bunch of instances for Duration.

--- src/Physics/Duration.hs
+++ src/Physics/Duration.hs
@@ -1,5 +1,9 @@
+{-# LANGUAGE DeriveTraversable, GeneralizedNewtypeDeriving #-}
 module Physics.Duration
 ( Duration(..)
 ) where
 
+import GL.Uniform
+
 newtype Duration a = Duration { getDuration :: a }
+  deriving (Eq, Foldable, Floating, Fractional, Functor, Num, Ord, Real, RealFloat, RealFrac, Show, Traversable, Uniform)
