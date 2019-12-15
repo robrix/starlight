266cd2c5d2be026fe4be85a6715266b8c1179519
Rob Rix <rob.rix@me.com>
2019-12-14 22:44:43 -0500

    Revert "Derive instances for Distance."
    This reverts commit ac44fc7035b667175f1cddf98b8da2211a791f02.

--- src/Physics/Distance.hs
+++ src/Physics/Distance.hs
@@ -1,9 +1,5 @@
-{-# LANGUAGE DeriveTraversable, GeneralizedNewtypeDeriving #-}
 module Physics.Distance
 ( Distance(..)
 ) where
 
-import GL.Uniform
-
 newtype Distance a = Distance { getDistance :: a }
-  deriving (Eq, Foldable, Floating, Fractional, Functor, Num, Ord, Real, RealFloat, RealFrac, Show, Traversable, Uniform)
