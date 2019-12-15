396dc2b902f9932b956ae15243d7e0dd2ccf6055
Rob Rix <rob.rix@me.com>
2019-12-15 13:09:47 -0500

    Cache the glyph table on the Typeface.

--- src/UI/Font.hs
+++ src/UI/Font.hs
@@ -20,24 +20,38 @@ import qualified Data.Map as Map
 import Data.Maybe (catMaybes)
 import qualified Data.Text as T
 import qualified Data.Text.Encoding as T
-import Data.Vector (Vector, (!?))
+import Data.Vector ((!?))
 import Data.Word
 import Geometry.Triangle
 import qualified Opentype.Fileformat as O
 import Lens.Micro
 import Linear.V2
-import Linear.V4
 import Linear.Vector ((^/))
 import UI.Glyph
 import UI.Path
 
-data Typeface = Typeface { name :: Maybe String, _font :: O.OpentypeFont }
+data Typeface = Typeface { name :: Maybe String, allGlyphs :: Map.Map Char (Maybe Glyph), _font :: O.OpentypeFont }
 
 data Font = Font { face :: Typeface, size :: Float }
 
 
 readTypeface :: Has (Lift IO) sig m => FilePath -> m Typeface
-readTypeface path = sendM ((Typeface . opentypeFontName <*> id) <$> O.readOTFile path)
+readTypeface = fmap toTypeface . sendM . O.readOTFile where
+  toTypeface o = Typeface (opentypeFontName o) allGlyphs o where
+    allGlyphs = Map.fromList (map ((,) <*> lookupGlyph) [minBound..maxBound])
+    lookupGlyph char = do
+      table <- O.glyphMap <$> cmap
+      glyphID <- table Map.!? fromIntegral (ord char)
+      g <- glyphTable !? fromIntegral glyphID
+      let vertices = glyphVertices g
+      pure $! scaleGlyph scale $ Glyph char (fromIntegral (O.advanceWidth g)) vertices (bounds (map (^. _xy) vertices))
+    cmap = find supportedPlatform (O.getCmaps (O.cmapTable o))
+    scale = 1 ^/ fromIntegral (O.unitsPerEm (O.headTable o))
+    glyphTable = case O.outlineTables o of
+      O.QuadTables _ (O.GlyfTable glyphs) -> glyphs
+      _ -> error "wtf"
+    supportedPlatform p = O.cmapPlatform p == O.UnicodePlatform || O.cmapPlatform p == O.MicrosoftPlatform && O.cmapEncoding p == 1
+    glyphVertices = uncurry triangleVertices . first (fmap fromIntegral) <=< pathTriangles <=< map contourToPath . O.getScaledContours o
 
 readFontOfSize :: Has (Lift IO) sig m => FilePath -> Float -> m Font
 readFontOfSize path size = (`Font` size) <$> readTypeface path
@@ -91,26 +105,6 @@ ascent = O.ascent . O.hheaTable . _font
 descent :: Typeface -> Int16
 descent = O.descent . O.hheaTable . _font
 
-glyphTable :: Typeface -> Vector (O.Glyph Int)
-glyphTable (Typeface _ o) = case O.outlineTables o of
-  O.QuadTables _ (O.GlyfTable glyphs) -> glyphs
-  _ -> error "wtf"
-
-supportedCMap :: Typeface -> Maybe O.CMap
-supportedCMap = find supportedPlatform . O.getCmaps . O.cmapTable . _font
-  where supportedPlatform p = O.cmapPlatform p == O.UnicodePlatform || O.cmapPlatform p == O.MicrosoftPlatform && O.cmapEncoding p == 1
-
-
-allGlyphs :: Typeface -> Map.Map Char (Maybe Glyph)
-allGlyphs face = Map.fromList (map ((,) <*> lookupGlyph) [minBound..maxBound])
-  where lookupGlyph char = do
-          table <- O.glyphMap <$> cmap
-          glyphID <- table Map.!? fromIntegral (ord char)
-          g <- glyphTable face !? fromIntegral glyphID
-          let vertices = glyphVertices face g
-          pure $! scaleGlyph scale $ Glyph char (fromIntegral (O.advanceWidth g)) vertices (bounds (map (^. _xy) vertices))
-        cmap = supportedCMap face
-        scale = 1 ^/ fromIntegral (unitsPerEm face)
 
 glyphs :: Font -> [Char] -> [Glyph]
 glyphs (Font face size) = map (scaleGlyph (pure size)) . catMaybes . map (join . (allGlyphs face Map.!?))
@@ -125,10 +119,3 @@ contourToPath (p@(O.CurvePoint x y _) : ps) = M (V2 x y) : go p ps
         go (O.CurvePoint x1 y1 False) (p@(O.CurvePoint x2 y2 True)  : ps) = Q (V2 x1 y1) (V2 x2 y2) : go p ps
         go (O.CurvePoint x1 y1 False) []                                  = Q (V2 x1 y1) (V2 x y) : []
         go _                          []                                  = []
-
-glyphPaths :: Typeface -> O.Glyph Int -> [Path V2 O.FWord]
-glyphPaths typeface glyph = fmap contourToPath (O.getScaledContours (_font typeface) glyph)
-
-
-glyphVertices :: Typeface -> O.Glyph Int -> [V4 Float]
-glyphVertices typeface = uncurry triangleVertices . first (fmap fromIntegral) <=< pathTriangles <=< glyphPaths typeface
