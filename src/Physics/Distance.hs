ac44fc7035b667175f1cddf98b8da2211a791f02
Rob Rix <rob.rix@me.com>
2019-12-14 22:43:21 -0500

    Derive instances for Distance.

--- src/Physics/Distance.hs
+++ src/Physics/Distance.hs
@@ -1,5 +1,9 @@
+{-# LANGUAGE DeriveTraversable, GeneralizedNewtypeDeriving #-}
 module Physics.Distance
 ( Distance(..)
 ) where
 
+import GL.Uniform
+
 newtype Distance a = Distance { getDistance :: a }
+  deriving (Eq, Foldable, Floating, Fractional, Functor, Num, Ord, Real, RealFloat, RealFrac, Show, Traversable, Uniform)
