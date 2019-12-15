129ec9cab36955074e42afa6aa3ead4e257f299e
Rob Rix <rob.rix@me.com>
2019-12-15 16:34:24 -0500

    Define lenses for Label.

--- src/UI/Label.hs
+++ src/UI/Label.hs
@@ -2,6 +2,7 @@ module UI.Label
 ( Label(..)
 ) where
 
+import Lens.Micro
 import UI.Colour
 import UI.Font
 
@@ -10,3 +11,12 @@ data Label = Label
   , text   :: !String
   , colour :: !(Colour Float)
   }
+
+_font :: Lens' Label Font
+_font = lens font (\ l f -> l { font = f })
+
+_text :: Lens' Label String
+_text = lens text (\ l t -> l { text = t })
+
+_colour :: Lens' Label (Colour Float)
+_colour = lens colour (\ l c -> l { colour = c })
