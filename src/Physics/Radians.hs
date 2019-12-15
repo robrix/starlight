15165184914740dfba840fe7deb42eb80f9ae077
Rob Rix <rob.rix@me.com>
2019-12-15 10:07:58 -0500

    Convert cartesian 2-vectors back to polar coordinates.

--- src/Physics/Radians.hs
+++ src/Physics/Radians.hs
@@ -2,6 +2,7 @@
 module Physics.Radians
 ( Radians(..)
 , cartesian2
+, polar2
 ) where
 
 import GL.Uniform
@@ -12,3 +13,8 @@ newtype Radians a = Radians { getRadians :: a }
 
 cartesian2 :: Floating a => Radians a -> a -> V2 a
 cartesian2 (Radians phi) r = V2 (r * cos phi) (r * sin phi)
+
+polar2 :: RealFloat a => V2 a -> (Radians a, a)
+polar2 (V2 x y) = (Radians phi, r) where
+  phi = atan2 y x
+  r = sqrt (x * x + y * y)
