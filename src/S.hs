518d6836c2718f5e5e6842397a92aa4d7d89f0fc
Rob Rix <rob.rix@me.com>
2019-12-16 17:55:09 -0500

    Define a smart constructor for Lam.

--- src/S.hs
+++ src/S.hs
@@ -3,6 +3,7 @@ module S
 ( S(..)
 , Scope
 , Var(..)
+, lam
 ) where
 
 import Control.Monad (ap)
@@ -32,3 +33,14 @@ newtype Scope a = Scope { unScope :: S (Var a) }
 
 data Var a = B | F a
   deriving (Eq, Foldable, Functor, Ord, Show, Traversable)
+
+
+lam :: Eq a => a -> S a -> S a
+lam n = Lam . abstract1 n
+
+
+abstract :: (a -> Var b) -> S a -> Scope b
+abstract f = Scope . fmap f
+
+abstract1 :: Eq a => a -> S a -> Scope a
+abstract1 a = abstract (\b -> if a == b then B else F b)
