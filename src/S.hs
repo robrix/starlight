fe95fe8515fb7aae38ea82dfdf93c1f7f309f392
Rob Rix <rob.rix@me.com>
2019-12-16 17:51:19 -0500

    Define Applicative & Monad instances for S.

--- src/S.hs
+++ src/S.hs
@@ -1,8 +1,10 @@
-{-# LANGUAGE DeriveTraversable #-}
+{-# LANGUAGE DeriveTraversable, LambdaCase #-}
 module S
 ( S(..)
 , Var(..)
 ) where
+
+import Control.Monad (ap)
 
 data S a
   = Var a
@@ -11,6 +13,18 @@ data S a
   deriving (Eq, Foldable, Functor, Ord, Show, Traversable)
 
 infixr 9 :$
+
+instance Applicative S where
+  pure = Var
+  (<*>) = ap
+
+instance Monad S where
+  Var a  >>= f = f a
+  g :$ a >>= f = (f =<< g) :$ (f =<< a)
+  Lam b  >>= f = Lam . Scope $ unScope b >>= \case
+    B   -> pure B
+    F a -> F <$> f a
+
 
 newtype Scope a = Scope { unScope :: S (Var a) }
   deriving (Eq, Foldable, Functor, Ord, Show, Traversable)
