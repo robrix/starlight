54188cd2c57ec0ce413cc2887c426d20e7fdec4f
Rob Rix <rob.rix@me.com>
2019-12-16 17:56:16 -0500

    :fire: Var.

--- src/S.hs
+++ src/S.hs
@@ -1,8 +1,7 @@
-{-# LANGUAGE DeriveTraversable, LambdaCase #-}
+{-# LANGUAGE DeriveTraversable #-}
 module S
 ( S(..)
 , Scope
-, Var(..)
 , lam
 ) where
 
@@ -23,15 +22,10 @@ instance Applicative S where
 instance Monad S where
   Var a  >>= f = f a
   g :$ a >>= f = (f =<< g) :$ (f =<< a)
-  Lam b  >>= f = Lam . Scope $ unScope b >>= \case
-    B   -> pure B
-    F a -> F <$> f a
+  Lam b  >>= f = Lam . Scope $ unScope b >>= maybe (pure Nothing) (fmap Just . f)
 
 
-newtype Scope a = Scope { unScope :: S (Var a) }
-  deriving (Eq, Foldable, Functor, Ord, Show, Traversable)
-
-data Var a = B | F a
+newtype Scope a = Scope { unScope :: S (Maybe a) }
   deriving (Eq, Foldable, Functor, Ord, Show, Traversable)
 
 
@@ -39,8 +33,8 @@ lam :: Eq a => a -> S a -> S a
 lam n = Lam . abstract1 n
 
 
-abstract :: (a -> Var b) -> S a -> Scope b
+abstract :: (a -> Maybe b) -> S a -> Scope b
 abstract f = Scope . fmap f
 
 abstract1 :: Eq a => a -> S a -> Scope a
-abstract1 a = abstract (\b -> if a == b then B else F b)
+abstract1 a = abstract (\b -> if a == b then Nothing else Just b)
