26f11626bb4715f0378d44fe9aec8b278cc13940
Rob Rix <rob.rix@me.com>
2019-12-16 17:45:18 -0500

    Add a Scope newtype.

--- src/S.hs
+++ src/S.hs
@@ -7,11 +7,13 @@ module S
 data S a
   = Var a
   | S a :$ S a
-  | Lam (S (Var a))
+  | Lam (Scope a)
   deriving (Eq, Foldable, Functor, Ord, Show, Traversable)
 
 infixr 9 :$
 
+newtype Scope a = Scope { unScope :: S (Var a) }
+  deriving (Eq, Foldable, Functor, Ord, Show, Traversable)
 
 data Var a = B | F a
   deriving (Eq, Foldable, Functor, Ord, Show, Traversable)
