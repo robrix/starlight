8e9bb05311aa4daec78a75a544faec4a2d852ed3
Rob Rix <rob.rix@me.com>
2019-12-16 10:48:56 -0500

    Define a viewport function.

--- src/Geometry/Rect.hs
+++ src/Geometry/Rect.hs
@@ -5,9 +5,12 @@ module Geometry.Rect
 , _max
 , clamp
 , transformRect
+, viewport
 , Union(..)
 ) where
 
+import Control.Monad.IO.Class.Lift
+import Graphics.GL.Core41
 import Lens.Micro
 import Linear.Matrix
 import Linear.V2
@@ -33,6 +36,11 @@ clamp (Rect min max) = Rect (floor <$> min) (ceiling <$> max)
 transformRect :: Num a => M33 a -> Rect a -> Rect a
 transformRect m (Rect v1 v2) = Rect ((m !* ext v1) ^. _xy) ((m !* ext v2) ^. _xy)
   where ext (V2 x y) = V3 x y 1
+
+
+viewport :: Has (Lift IO) sig m => Rect Int -> m ()
+viewport r = runLiftIO (glViewport x1 y1 x2 y2) where
+  Rect (V2 x1 y1) (V2 x2 y2) = fromIntegral <$> r
 
 
 newtype Union a = Union { getUnion :: Rect a }
