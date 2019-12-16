41cd9d7efb43493fcae4f30e57a29b1b514f305b
Rob Rix <rob.rix@me.com>
2019-12-16 17:29:56 -0500

    Define a datatype of variables.

--- src/S.hs
+++ src/S.hs
@@ -1,12 +1,17 @@
 {-# LANGUAGE DeriveTraversable #-}
 module S
 ( S(..)
+, Var(..)
 ) where
 
 data S a
   = Var a
   | S a :$ S a
-  | Lam (S (Maybe a))
+  | Lam (S (Var a))
   deriving (Eq, Foldable, Functor, Ord, Show, Traversable)
 
 infixr 9 :$
+
+
+data Var a = B | F a
+  deriving (Eq, Foldable, Functor, Ord, Show, Traversable)
