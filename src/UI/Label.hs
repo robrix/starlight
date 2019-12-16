7000a58dbf3fd4df8745d0b18d1dc124e149d1cd
Rob Rix <rob.rix@me.com>
2019-12-15 23:40:35 -0500

    Export the colour & bcolour fields.

--- src/UI/Label.hs
+++ src/UI/Label.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE DataKinds, DisambiguateRecordFields, FlexibleContexts, NamedFieldPuns, TypeApplications, TypeOperators #-}
 module UI.Label
-( Label
+( Label(colour, bcolour)
 , label
 , setLabel
 , drawLabel
