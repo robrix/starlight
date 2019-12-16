c05fbb760d1a256d2395c6467ea44073d447e679
Rob Rix <rob.rix@me.com>
2019-12-16 17:28:39 -0500

    Derive a bunch of instances.

--- src/S.hs
+++ src/S.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE DeriveTraversable #-}
 module S
 ( S(..)
 ) where
@@ -6,5 +7,6 @@ data S a
   = Var a
   | S a :$ S a
   | Lam (S (Maybe a))
+  deriving (Eq, Foldable, Functor, Ord, Show, Traversable)
 
 infixr 9 :$
