a6cb9a2e19783e62c87a5c373ba2928c3d3ba7f5
Rob Rix <rob.rix@me.com>
2019-12-18 21:55:33 -0500

    Set the z coordinate on the multiplied vector.

--- ship-vertex.glsl
+++ ship-vertex.glsl
@@ -3,5 +3,5 @@ precision highp float;
 uniform mat3 matrix3;
 in vec2 position2;
 void main() {
-  gl_Position = vec4(matrix3 * vec3(position2, 0.0), 1.0);
+  gl_Position = vec4(matrix3 * vec3(position2, 1.0), 1.0);
 }
