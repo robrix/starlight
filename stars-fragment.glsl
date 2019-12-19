3290fced4c7314213cf34014224e08b36397b35a
Rob Rix <rob.rix@me.com>
2019-12-19 18:40:18 -0500

    Reformat a bunch of stuff.

--- stars-fragment.glsl
+++ stars-fragment.glsl
@@ -27,33 +27,34 @@ uniform vec2 origin;
 out vec4 fragColor;
 
 void main() {
-	vec2 uv = gl_FragCoord.xy / resolution.xy - .5;
+	vec2 uv = gl_FragCoord.xy / resolution.xy - 0.5;
 	uv.y *= resolution.y / resolution.x;
-	vec3 dir = vec3(uv * zoom, 1.);
+	vec3 dir = vec3(uv * zoom, 1.0);
 
-	vec3 origin = vec3(origin, 1);
+	// vec3 origin = vec3(origin, 1);
+	vec3 origin = vec3(2 * origin / resolution, 1);
 
-	//volumetric rendering
-	float s = 0.1, fade = 1.;
-	vec3 v = vec3(0.);
+	// volumetric rendering
+	float s = 0.1, fade = 1.0;
+	vec3 v = vec3(0.0);
 	for (int r = 0; r < volsteps; r++) {
 		vec3 p = origin + dir * s * 0.5;
-		p = abs(vec3(tile) - mod(p, vec3(tile * 2.))); // tiling fold
-		float pa, a = pa = 0.;
+		p = abs(vec3(tile) - mod(p, vec3(tile * 2.0))); // tiling fold
+		float pa, a = pa = 0.0;
 		for (int i = 0; i < iterations; i++) {
 			p = abs(p) / dot(p, p) - formuparam; // the magic formula
 			a += abs(length(p) - pa); // absolute sum of average change
 			pa = length(p);
 		}
-		float dm = max(0., darkmatter - a * a *.001); //dark matter
+		float dm = max(0.0, darkmatter - a * a * 0.001); //dark matter
 		a *= a * a; // add contrast
-		if (r > 6) fade *= 1. - dm; // dark matter, don't render near
-		//v+=vec3(dm,dm*.5,0.);
+		if (r > 6) fade *= 1.0 - dm; // dark matter, don't render near
+		//v+=vec3(dm,dm*.5,0.0);
 		v += fade;
 		v += vec3(s, s * s, s * s * s * s) * a * brightness * fade; // coloring based on distance
 		fade *= distfading; // distance fading
 		s += stepsize;
 	}
 	v = mix(vec3(length(v)), v, saturation); //color adjust
-	fragColor = vec4(v * .01, 1.);
+	fragColor = vec4(v * 0.01, 1.0);
 }
