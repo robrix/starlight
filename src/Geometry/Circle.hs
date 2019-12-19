85c6efc0ff150d445bf8ae2fab5692b2e2c76f51
Rob Rix <rob.rix@me.com>
2019-12-18 21:21:48 -0500

    Calculate circles.

--- src/Geometry/Circle.hs
+++ src/Geometry/Circle.hs
@@ -1,2 +1,19 @@
 module Geometry.Circle
-() where
+( circle
+) where
+
+import Linear.V2
+import Physics.Radians
+
+circle
+  -- ^ The radius.
+  :: Float
+  -- ^ The number of vertices to produce.
+  -> Int
+  -- ^ The vertices for the circle.
+  -> [V2 Float]
+circle radius n =
+  [ cartesian2 theta radius
+  | i <- [0..pred n]
+  , let theta = 2 * pi * fromIntegral i / fromIntegral n
+  ]
