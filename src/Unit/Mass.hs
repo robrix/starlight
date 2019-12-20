47d8ffd81b68813f9206c621ce64034f1f65c4aa
Rob Rix <rob.rix@me.com>
2019-12-20 18:26:48 -0500

    Derive a bunch of instances for kilograms.

--- src/Unit/Mass.hs
+++ src/Unit/Mass.hs
@@ -1,5 +1,9 @@
+{-# LANGUAGE DeriveTraversable, GeneralizedNewtypeDeriving #-}
 module Unit.Mass
 ( Kilograms(..)
 ) where
 
+import GL.Uniform
+
 newtype Kilograms a = Kilograms { getKilograms :: a }
+  deriving (Eq, Foldable, Floating, Fractional, Functor, Num, Ord, Real, RealFloat, RealFrac, Show, Traversable, Uniform)
