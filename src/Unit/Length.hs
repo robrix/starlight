799e31391d294d04ba7ebef9aef3e244de83e050
Rob Rix <rob.rix@me.com>
2019-12-20 18:21:42 -0500

    Derive instances for Kilometres.

--- src/Unit/Length.hs
+++ src/Unit/Length.hs
@@ -1,5 +1,9 @@
+{-# LANGUAGE DeriveTraversable, GeneralizedNewtypeDeriving #-}
 module Unit.Length
 ( Kilometres(..)
 ) where
 
+import GL.Uniform
+
 newtype Kilometres a = Kilometres { getKilometres :: a }
+  deriving (Eq, Foldable, Floating, Fractional, Functor, Num, Ord, Real, RealFloat, RealFrac, Show, Traversable, Uniform)
