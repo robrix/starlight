b20fca6411b4a007883adfd9adc3b33b6ff5e3d9
Rob Rix <rob.rix@me.com>
2019-12-20 18:07:23 -0500

    Convert Days to Seconds.

--- src/Unit/Time.hs
+++ src/Unit/Time.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE DeriveTraversable, GeneralizedNewtypeDeriving #-}
 module Unit.Time
 ( Seconds(..)
+, fromDays
 , Days(..)
 ) where
 
@@ -8,6 +9,11 @@ import GL.Uniform
 
 newtype Seconds a = Seconds { getSeconds :: a }
   deriving (Eq, Foldable, Floating, Fractional, Functor, Num, Ord, Real, RealFloat, RealFrac, Show, Traversable, Uniform)
+
+-- | Convert 'Days' to 'Seconds'. Note that this does not take e.g. leap seconds into account.
+fromDays :: Num a => Days a -> Seconds a
+fromDays (Days d) = Seconds (d * 86400)
+
 
 newtype Days a = Days { getDays :: a }
   deriving (Eq, Foldable, Floating, Fractional, Functor, Num, Ord, Real, RealFloat, RealFrac, Show, Traversable, Uniform)
