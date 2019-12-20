e1ff8c0b21fad75ba9aa951df5218e02bf64c80b
Rob Rix <rob.rix@me.com>
2019-12-20 18:05:45 -0500

    fromDegrees takes Degrees.

--- src/Unit/Angle.hs
+++ src/Unit/Angle.hs
@@ -4,6 +4,7 @@ module Unit.Angle
 , fromDegrees
 , cartesian2
 , polar2
+, Degrees(..)
 ) where
 
 import GL.Uniform
@@ -12,8 +13,8 @@ import Linear.V2
 newtype Radians a = Radians { getRadians :: a }
   deriving (Eq, Foldable, Floating, Fractional, Functor, Num, Ord, Real, RealFloat, RealFrac, Show, Traversable, Uniform)
 
-fromDegrees :: Float -> Radians Float
-fromDegrees d = Radians (d * pi / 180)
+fromDegrees :: Floating a => Degrees a -> Radians a
+fromDegrees (Degrees d) = Radians (d * pi / 180)
 
 
 cartesian2 :: Floating a => Radians a -> a -> V2 a
@@ -23,3 +24,7 @@ polar2 :: RealFloat a => V2 a -> (Radians a, a)
 polar2 (V2 x y) = (Radians phi, r) where
   phi = atan2 y x
   r = sqrt (x * x + y * y)
+
+
+newtype Degrees a = Degrees { getDegrees :: a }
+  deriving (Eq, Foldable, Floating, Fractional, Functor, Num, Ord, Real, RealFloat, RealFrac, Show, Traversable, Uniform)
