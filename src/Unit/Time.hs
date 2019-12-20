005e097d97c6d6c605ac2f7d61786fbe149db700
Rob Rix <rob.rix@me.com>
2019-12-20 18:06:14 -0500

    Define a newtype for days.

--- src/Unit/Time.hs
+++ src/Unit/Time.hs
@@ -1,9 +1,13 @@
 {-# LANGUAGE DeriveTraversable, GeneralizedNewtypeDeriving #-}
 module Unit.Time
 ( Seconds(..)
+, Days(..)
 ) where
 
 import GL.Uniform
 
 newtype Seconds a = Seconds { getSeconds :: a }
+  deriving (Eq, Foldable, Floating, Fractional, Functor, Num, Ord, Real, RealFloat, RealFrac, Show, Traversable, Uniform)
+
+newtype Days a = Days { getDays :: a }
   deriving (Eq, Foldable, Floating, Fractional, Functor, Num, Ord, Real, RealFloat, RealFrac, Show, Traversable, Uniform)
