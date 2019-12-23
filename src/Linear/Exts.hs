6513d6933a59c127f2f393e2b902bb70234220dd
Rob Rix <rob.rix@me.com>
2019-12-23 09:16:11 -0500

    Define polar2 using angleOf & norm.

--- src/Linear/Exts.hs
+++ src/Linear/Exts.hs
@@ -48,6 +48,4 @@ cartesian2 :: Floating a => Radians a -> a -> V2 a
 cartesian2 (Radians phi) r = V2 (r * cos phi) (r * sin phi)
 
 polar2 :: RealFloat a => V2 a -> (Radians a, a)
-polar2 (V2 x y) = (Radians phi, r) where
-  phi = atan2 y x
-  r = sqrt (x * x + y * y)
+polar2 v = (angleOf v, norm v) where
