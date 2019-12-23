f9c03532e0b0afde2dd6269f448d11beabe5ab6b
Rob Rix <rob.rix@me.com>
2019-12-22 19:11:45 -0500

    Define a Num instance for Interval.

--- src/Data/Interval.hs
+++ src/Data/Interval.hs
@@ -3,6 +3,8 @@ module Data.Interval
 ( Interval(..)
 ) where
 
+import Control.Applicative (liftA2)
+
 data Interval a = Interval
   { from :: !a
   , to   :: !a
@@ -12,3 +14,12 @@ data Interval a = Interval
 instance Applicative Interval where
   pure a = Interval a a
   Interval f1 f2 <*> Interval a1 a2 = Interval (f1 a1) (f2 a2)
+
+instance Num a => Num (Interval a) where
+  (+) = liftA2 (+)
+  (*) = liftA2 (*)
+  (-) = liftA2 (-)
+  abs = fmap abs
+  signum = fmap signum
+  negate = fmap negate
+  fromInteger = pure . fromInteger
