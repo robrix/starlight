7d2f0a1c9bbdca4a750d46bb7ba13b4d05d6a86a
Rob Rix <rob.rix@me.com>
2019-12-22 19:09:59 -0500

    Define an Applicative instance for Interval.

--- src/Data/Interval.hs
+++ src/Data/Interval.hs
@@ -8,3 +8,7 @@ data Interval a = Interval
   , to   :: !a
   }
   deriving (Eq, Foldable, Functor, Show, Traversable)
+
+instance Applicative Interval where
+  pure a = Interval a a
+  Interval f1 f2 <*> Interval a1 a2 = Interval (f1 a1) (f2 a2)
