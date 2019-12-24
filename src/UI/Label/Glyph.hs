45d9c97720b147565cb6fd50902fbd6374b8c9ae
Rob Rix <rob.rix@me.com>
2019-12-24 02:42:10 -0500

    Define a datatype for the glyph shader’s uniforms.

--- src/UI/Label/Glyph.hs
+++ src/UI/Label/Glyph.hs
@@ -1,2 +1,13 @@
+{-# LANGUAGE DeriveAnyClass, DeriveGeneric #-}
 module UI.Label.Glyph
-() where
+( U(..)
+) where
+
+import GHC.Generics (Generic)
+import GL.Shader.DSL
+
+data U v = U
+  { matrix3 :: v (M33 Float)
+  , colour  :: v (Colour Float)
+  }
+  deriving (Generic, Vars)
