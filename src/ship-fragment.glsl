2dff1cda440f1bcfcce6f1c8beaeba3fc70180c6
Rob Rix <rob.rix@me.com>
2019-12-24 03:11:01 -0500

    :fire: the unembedded ship shaders.

--- src/ship-fragment.glsl
+++
@@ -1,9 +0,0 @@
-#version 410
-
-uniform vec4 colour;
-
-out vec4 fragColour;
-
-void main() {
-  fragColour = colour;
-}
--- src/ship-vertex.glsl
+++
@@ -1,9 +0,0 @@
-#version 410
-
-uniform mat3 matrix;
-
-in vec2 position2;
-
-void main() {
-  gl_Position = vec4(matrix * vec3(position2, 1.0), 1.0);
-}
