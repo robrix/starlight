fda904967983d0ea95672c5e81db9366e13b9673
Rob Rix <rob.rix@me.com>
2019-12-24 18:54:37 -0500

    Define a Type instance for V3.

--- src/GL/Type.hs
+++ src/GL/Type.hs
@@ -9,6 +9,7 @@ import qualified Foreign.Storable as S
 import           Graphics.GL.Core41
 import           Graphics.GL.Types
 import           Linear.V2
+import           Linear.V3
 
 class S.Storable n => Type n where
   glType :: proxy n -> GLenum
@@ -23,4 +24,7 @@ instance Type Int where
   glType _ = GL_INT
 
 instance Type a => Type (V2 a) where
+  glType _ = glType (Proxy @a)
+
+instance Type a => Type (V3 a) where
   glType _ = glType (Proxy @a)
