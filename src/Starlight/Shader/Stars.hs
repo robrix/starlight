f81f6ea2803e20c619750b17048d64d13a08d773
Rob Rix <rob.rix@me.com>
2019-12-23 19:54:08 -0500

    Define a vertex shader for the star field.

--- src/Starlight/Shader/Stars.hs
+++ src/Starlight/Shader/Stars.hs
@@ -1,2 +1,13 @@
+{-# LANGUAGE DataKinds, TypeOperators #-}
 module Starlight.Shader.Stars
-() where
+( vertex
+) where
+
+import GL.Shader.DSL
+
+vertex :: Shader 'Vertex
+  '[]
+  '[ "position2" '::: V2 Float ]
+  '[]
+vertex = inputs $ \ position2 -> main $
+  gl_Position .= vec4 (vec3 position2 0) 1
