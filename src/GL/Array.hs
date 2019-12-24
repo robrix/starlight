8b3e77ed4142eb142d6e3342af6594d71a4b6d05
Rob Rix <rob.rix@me.com>
2019-12-24 14:52:27 -0500

    Replace modeToGLEnum with a GL.Enum instance.

--- src/GL/Array.hs
+++ src/GL/Array.hs
@@ -22,6 +22,7 @@ import qualified Foreign.Storable as S
 import           GHC.Stack
 import           GHC.TypeLits
 import qualified GL.Buffer as GL
+import           GL.Enum as GL
 import           GL.Error
 import           GL.Object
 import           GL.Scalar
@@ -55,18 +56,18 @@ data Mode
   | Triangles
   deriving (Eq, Show)
 
-modeToGLEnum :: Mode -> GLenum
-modeToGLEnum = \case
-  Points        -> GL_POINTS
-  Lines         -> GL_LINES
-  LineStrip     -> GL_LINE_STRIP
-  LineLoop      -> GL_LINE_LOOP
-  TriangleStrip -> GL_TRIANGLE_STRIP
-  Triangles     -> GL_TRIANGLES
+instance GL.Enum Mode where
+  glEnum = \case
+    Points        -> GL_POINTS
+    Lines         -> GL_LINES
+    LineStrip     -> GL_LINE_STRIP
+    LineLoop      -> GL_LINE_LOOP
+    TriangleStrip -> GL_TRIANGLE_STRIP
+    Triangles     -> GL_TRIANGLES
 
 
 drawArrays :: (Has (Lift IO) sig m, HasCallStack) => Mode -> Interval Int -> m ()
-drawArrays mode i = checkingGLError . runLiftIO $ glDrawArrays (modeToGLEnum mode) (fromIntegral (min_ i)) (fromIntegral (size i))
+drawArrays mode i = checkingGLError . runLiftIO $ glDrawArrays (glEnum mode) (fromIntegral (min_ i)) (fromIntegral (size i))
 
 
 loadVertices :: (KnownNat (Size v), S.Storable (v n), Scalar n, Has Finally sig m, Has (Lift IO) sig m) => [v n] -> m (Array (v n))
