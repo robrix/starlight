1e1c9fabdb5a97ce4c43f5f9cb9bd9ae1b4a523c
Rob Rix <rob.rix@me.com>
2019-12-25 17:00:16 -0500

    Define an Algebra instance for BindC.

--- src/GL/Carrier/Bind.hs
+++ src/GL/Carrier/Bind.hs
@@ -1,18 +1,36 @@
+{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE UndecidableInstances #-}
 module GL.Carrier.Bind
 ( -- * Bind carrier
   runBind
-, BindC(..)
+, BindC(BindC)
   -- * Bind effect
 , module GL.Effect.Bind
 ) where
 
+import Control.Algebra
+import Control.Effect.Lift
 import Control.Carrier.Reader
 import Control.Monad.IO.Class
 import GL.Effect.Bind
+import qualified GL.Object as GL
 
 runBind :: BindC t m a -> m a
-runBind (BindC m) = runReader Nothing m
+runBind = runReader Nothing . runBindC
 
-newtype BindC t m a = BindC (ReaderC (Maybe t) m a)
+newtype BindC t m a = BindC { runBindC :: ReaderC (Maybe t) m a }
   deriving (Applicative, Functor, Monad, MonadIO)
+
+instance (Has (Lift IO) sig m, GL.Bind t) => Algebra (Bind t :+: sig) (BindC t m) where
+  alg = \case
+    L (Bind t m k) -> do
+      prev <- BindC ask
+      GL.bind (Just t)
+      a <- BindC (local (const (Just t)) (runBindC m))
+      GL.bind (prev `asTypeOf` Just t)
+      k a
+    R other -> BindC (send (handleCoercible other))
