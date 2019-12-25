b84e9ea9a07bd61d13ba3c4d7e675115a9589c08
Rob Rix <rob.rix@me.com>
2019-12-25 16:56:07 -0500

    Define a handler for BindC.

--- src/GL/Carrier/Bind.hs
+++ src/GL/Carrier/Bind.hs
@@ -1,7 +1,8 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module GL.Carrier.Bind
 ( -- * Bind carrier
-  BindC(..)
+  runBind
+, BindC(..)
   -- * Bind effect
 , module GL.Effect.Bind
 ) where
@@ -10,5 +11,8 @@ import Control.Carrier.Reader
 import Control.Monad.IO.Class
 import GL.Effect.Bind
 
-newtype BindC t m a = BindC (ReaderC t m a)
+runBind :: BindC t m a -> m a
+runBind (BindC m) = runReader Nothing m
+
+newtype BindC t m a = BindC (ReaderC (Maybe t) m a)
   deriving (Applicative, Functor, Monad, MonadIO)
