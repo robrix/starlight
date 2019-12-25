c22e09ea89731f2677484395e9ef1907890f9983
Rob Rix <rob.rix@me.com>
2019-12-24 19:02:04 -0500

    Derive a Storable instance for TextureUnit.

--- src/GL/TextureUnit.hs
+++ src/GL/TextureUnit.hs
@@ -1,14 +1,17 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module GL.TextureUnit
 ( TextureUnit(..)
 , setActiveTexture
 ) where
 
 import Control.Monad.IO.Class.Lift
+import Foreign.Storable
 import GL.Uniform
 import Graphics.GL.Core41
 import Graphics.GL.Types
 
 newtype TextureUnit = TextureUnit { unTextureUnit :: GLint }
+  deriving (Storable)
 
 instance Uniform TextureUnit where
   uniform location = runLiftIO . glUniform1i location . unTextureUnit
