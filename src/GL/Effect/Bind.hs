4f300cbb0dba9a3b17f969d85d2c180b398c40bc
Rob Rix <rob.rix@me.com>
2019-12-25 16:47:49 -0500

    Stub in a Bind effect.

--- src/GL/Effect/Bind.hs
+++ src/GL/Effect/Bind.hs
@@ -1,9 +1,14 @@
 module GL.Effect.Bind
-( -- * Re-exports
-  Algebra
+( -- * Bind effect
+  Bind(..)
+  -- * Re-exports
+, Algebra
 , Effect
 , Has
 , run
 ) where
 
 import Control.Algebra
+
+data Bind a m k
+  = Bind a (m k)
