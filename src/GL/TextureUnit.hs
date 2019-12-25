23325b0bae499e0921c5329d8377f890c4f9d652
Rob Rix <rob.rix@me.com>
2019-12-24 19:03:01 -0500

    Define a GL.Type instance for TextureUnit.

--- src/GL/TextureUnit.hs
+++ src/GL/TextureUnit.hs
@@ -6,12 +6,16 @@ module GL.TextureUnit
 
 import Control.Monad.IO.Class.Lift
 import Foreign.Storable
+import GL.Type as GL
 import GL.Uniform
 import Graphics.GL.Core41
 import Graphics.GL.Types
 
 newtype TextureUnit = TextureUnit { unTextureUnit :: GLint }
   deriving (Storable)
+
+instance GL.Type TextureUnit where
+  glType _ = GL_INT
 
 instance Uniform TextureUnit where
   uniform location = runLiftIO . glUniform1i location . unTextureUnit
