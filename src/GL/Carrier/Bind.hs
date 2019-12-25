25b65647048633bae3d9a5f707f015188f0bd953
Rob Rix <rob.rix@me.com>
2019-12-25 16:53:25 -0500

    Define a Bind carrier.

--- src/GL/Carrier/Bind.hs
+++ src/GL/Carrier/Bind.hs
@@ -1,6 +1,14 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module GL.Carrier.Bind
-( -- * Bind effect
-  module GL.Effect.Bind
+( -- * Bind carrier
+  BindC(..)
+  -- * Bind effect
+, module GL.Effect.Bind
 ) where
 
+import Control.Carrier.Reader
+import Control.Monad.IO.Class
 import GL.Effect.Bind
+
+newtype BindC t m a = BindC (ReaderC t m a)
+  deriving (Applicative, Functor, Monad, MonadIO)
