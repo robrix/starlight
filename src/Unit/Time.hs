8e2f0af7ffbf32ff98a8ed5db32e0c95309f8d2b
Rob Rix <rob.rix@me.com>
2019-12-25 20:56:24 -0500

    :fire: Days.

--- src/Unit/Time.hs
+++ src/Unit/Time.hs
@@ -7,7 +7,6 @@ module Unit.Time
 , fromDays
 , fromHours
 , fromMinutes
-, Days(..)
 ) where
 
 import Data.Proxy
@@ -23,21 +22,12 @@ instance GL.Type a => GL.Type (Seconds a) where
 
   glDims _ = glDims (Proxy @a)
 
--- | Convert 'Days' to 'Seconds'. Note that this does not take e.g. leap seconds into account.
-fromDays :: Num a => Days a -> Seconds a
-fromDays (Days d) = Seconds (d * 86400)
+-- | Convert days to 'Seconds'. Note that this does not take e.g. leap seconds into account.
+fromDays :: Num a => a -> Seconds a
+fromDays d = fromHours (d * 24)
 
 fromHours :: Num a => a -> Seconds a
-fromHours h = Seconds (h * 3600)
+fromHours h = fromMinutes (h * 60)
 
 fromMinutes :: Num a => a -> Seconds a
-fromMinutes h = Seconds (h * 60)
-
-
-newtype Days a = Days { getDays :: a }
-  deriving (Eq, Foldable, Floating, Fractional, Functor, Num, Ord, Real, RealFloat, RealFrac, Show, Storable, Traversable, Uniform)
-
-instance GL.Type a => GL.Type (Days a) where
-  glType _ = glType (Proxy @a)
-
-  glDims _ = glDims (Proxy @a)
+fromMinutes m = Seconds (m * 60)
