6c53548deb389946eb3481d6984a894e1f01fcd4
Rob Rix <rob.rix@me.com>
2019-12-27 13:21:56 -0500

    Define a template to construct an Orbit from a string representing an Ephemeris.

--- src/Starlight/Body/TH.hs
+++ src/Starlight/Body/TH.hs
@@ -1,7 +1,51 @@
+{-# LANGUAGE DisambiguateRecordFields #-}
+{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE TemplateHaskell #-}
 module Starlight.Body.TH
 ( System(..)
-, Body(..)
+, S.Body(..)
 , Orbit(..)
+, mkOrbit
 ) where
 
-import Starlight.Body
+import Language.Haskell.TH
+import Starlight.Body as S
+import Unit.Angle
+import Unit.Length
+import Unit.Time
+
+mkOrbit :: String -> Q Exp
+mkOrbit s = do
+  Ephemeris
+    { julianDayNumberBarycentricDynamicalTime
+    , calendarDate
+    , eccentricity
+    , periapsisDistance              = Kilometres periapsisDistance
+    , inclination                    = Degrees inclination
+    , longitudeOfAscendingNode       = Degrees longitudeOfAscendingNode
+    , argumentOfPerifocus            = Degrees argumentOfPerifocus
+    , timeOfPeriapsisRelativeToEpoch = Seconds timeOfPeriapsisRelativeToEpoch
+    , meanMotion                     = Per meanMotion
+    , meanAnomaly                    = Degrees meanAnomaly
+    , trueAnomaly                    = Degrees trueAnomaly
+    , semimajor                      = Kilometres semimajor
+    , apoapsisDistance               = Kilometres apoapsisDistance
+    , siderealOrbitPeriod            = Seconds siderealOrbitPeriod
+    }
+    <- either fail pure (fromCSV s)
+  [e| fromEphemeris Ephemeris
+    { julianDayNumberBarycentricDynamicalTime
+    , calendarDate
+    , eccentricity
+    , periapsisDistance              = Kilometres periapsisDistance
+    , inclination                    = Degrees inclination
+    , longitudeOfAscendingNode       = Degrees longitudeOfAscendingNode
+    , argumentOfPerifocus            = Degrees argumentOfPerifocus
+    , timeOfPeriapsisRelativeToEpoch = Seconds timeOfPeriapsisRelativeToEpoch
+    , meanMotion                     = Per meanMotion
+    , meanAnomaly                    = Degrees meanAnomaly
+    , trueAnomaly                    = Degrees trueAnomaly
+    , semimajor                      = Kilometres semimajor
+    , apoapsisDistance               = Kilometres apoapsisDistance
+    , siderealOrbitPeriod            = Seconds siderealOrbitPeriod
+    } |]
