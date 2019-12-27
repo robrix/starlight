be50b252a970f9245b1f09ba51d899e2c1b7c716
Rob Rix <rob.rix@me.com>
2019-12-26 23:22:41 -0500

    Factor the radius out.

--- app/Main.hs
+++ app/Main.hs
@@ -324,8 +324,9 @@ draw DrawState { quadA, circleA, shipA, radarA, shipP, starsP, radarP, bodyP } t
 
           drawAtRadius 150 minSweep (colour & _a .~ 0.5)
 
-          when (name == S.name target) $ for_ [1..n] $ \ i ->
-            drawAtRadius (step * fromIntegral i) (minSweep * Radians (step * fromIntegral i / (step * 5))) ((colour + 0.5 * fromIntegral i / fromIntegral n) ** 2 & _a .~ (fromIntegral i / fromIntegral n))
+          when (name == S.name target) $ for_ [1..n] $ \ i -> do
+            let radius = step * fromIntegral i
+            drawAtRadius radius (minSweep * Radians (radius / (step * 5))) ((colour + 0.5 * fromIntegral i / fromIntegral n) ** 2 & _a .~ (fromIntegral i / fromIntegral n))
 
           for_ satellites (drawBlip trans)
 
