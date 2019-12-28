114592ea7ed94ef3b59acccc33f3aee9393c004f
Rob Rix <rob.rix@me.com>
2019-12-28 12:48:23 -0500

    Define an Algebra instance for ProfileC.

--- src/Control/Carrier/Profile/Time.hs
+++ src/Control/Carrier/Profile/Time.hs
@@ -1,4 +1,9 @@
+{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE UndecidableInstances #-}
 module Control.Carrier.Profile.Time
 ( -- * Profile carrier
   runProfile
@@ -9,6 +14,8 @@ module Control.Carrier.Profile.Time
 , module Control.Effect.Profile
 ) where
 
+import           Control.Algebra
+import           Control.Carrier.Lift
 import           Control.Carrier.Writer.Strict
 import           Control.Effect.Profile
 import           Control.Monad.IO.Class
@@ -20,6 +27,16 @@ runProfile (ProfileC m) = runWriter m
 
 newtype ProfileC m a = ProfileC (WriterC Timings m a)
   deriving (Applicative, Functor, Monad, MonadIO)
+
+instance (Has (Lift IO) sig m, Effect sig) => Algebra (Profile :+: sig) (ProfileC m) where
+  alg = \case
+    L (Measure l m k) -> do
+      start <- sendM getCurrentTime
+      a <- m
+      end <- sendM getCurrentTime
+      ProfileC (tell (Timings (Map.singleton l (Timing (end `diffUTCTime` start) 1))))
+      k a
+    R other -> ProfileC (send (handleCoercible other))
 
 
 data Timing = Timing
