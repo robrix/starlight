03cd779f68f9849e3777f75be87f5729fe8c91a8
Rob Rix <rob.rix@me.com>
2019-12-28 10:42:33 -0500

    Compute the bounds as we go.

--- src/UI/Glyph.hs
+++ src/UI/Glyph.hs
@@ -33,15 +33,17 @@ data Instance = Instance
 
 
 layoutGlyphs :: [Glyph] -> Run
-layoutGlyphs = (Run <*> bounds) . ($ []) . result . foldl' go (LayoutState 0 id) where
-  go (LayoutState offset is) g@Glyph{ char, bounds_ } = LayoutState
-    { offset = offset + advanceWidth g
-    , result = is . (Instance char offset bounds_ :)
+layoutGlyphs = (Run . ($ []) . result <*> bounds) . foldl' go (LayoutState 0 id Nothing) where
+  go (LayoutState offset is prev) g@Glyph{ char, bounds_ } = LayoutState
+    { offset  = offset + advanceWidth g
+    , result  = is . (Instance char offset bounds_ :)
+    , bounds_ = prev <> Just (Union bounds_)
     }
 
 data LayoutState = LayoutState
-  { offset :: {-# UNPACK #-} !Float
-  , result :: !([Instance] -> [Instance])
+  { offset  :: {-# UNPACK #-} !Float
+  , result  :: !([Instance] -> [Instance])
+  , bounds_ :: !(Maybe (Union Float))
   }
 
 data Run = Run
@@ -58,6 +60,9 @@ instance HasBounds Glyph where
 
 instance HasBounds Instance where
   bounds Instance{ offset, bounds_ } = transformRect (translated (V2 offset 0)) bounds_
+
+instance HasBounds LayoutState where
+  bounds LayoutState{ bounds_ } = maybe (Rect 0 0) getUnion bounds_
 
 instance HasBounds t => HasBounds [t] where
   bounds = maybe (Rect 0 0) getUnion . foldMap (Just . Union . bounds)
