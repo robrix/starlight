bce897d961e7e4fd461c1941a5a656128a69b007
Rob Rix <rob.rix@me.com>
2019-12-28 11:18:34 -0500

    Define getSeconds as a function rather than a selector.

--- src/Unit/Time.hs
+++ src/Unit/Time.hs
@@ -4,6 +4,7 @@
 {-# LANGUAGE TypeApplications #-}
 module Unit.Time
 ( Seconds(..)
+, getSeconds
 , fromDays
 , fromHours
 , fromMinutes
@@ -14,13 +15,16 @@ import Foreign.Storable
 import GL.Type as GL
 import GL.Uniform
 
-newtype Seconds a = Seconds { getSeconds :: a }
+newtype Seconds a = Seconds a
   deriving (Eq, Foldable, Floating, Fractional, Functor, Num, Ord, Read, Real, RealFloat, RealFrac, Show, Storable, Traversable, Uniform)
 
 instance GL.Type a => GL.Type (Seconds a) where
   glType _ = glType (Proxy @a)
 
   glDims _ = glDims (Proxy @a)
+
+getSeconds :: Seconds a -> a
+getSeconds (Seconds a) = a
 
 -- | Convert days to 'Seconds'. Note that this does not take e.g. leap seconds into account.
 fromDays :: Num a => a -> Seconds a
