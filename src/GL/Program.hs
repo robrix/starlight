71a5c23668e2e9987aa7ae68dde89c9d3ffc2970
Rob Rix <rob.rix@me.com>
2019-12-28 16:48:12 -0500

    :fire: link & checkProgram.

--- src/GL/Program.hs
+++ src/GL/Program.hs
@@ -46,31 +46,26 @@ import           Graphics.GL.Types
 newtype Program (u :: (* -> *) -> *) (i :: (* -> *) -> *) (o :: (* -> *) -> *) = Program { unProgram :: GLuint }
   deriving (Eq, Ord, Show)
 
-link :: (Has (Lift IO) sig m, HasCallStack) => [Shader] -> Program u i o -> m ()
-link shaders (Program program) = runLiftIO $ do
-  for_ shaders (glAttachShader program . unShader)
-  glLinkProgram program
-  for_ shaders (glDetachShader program . unShader)
-  checkProgram (Program program)
-
-
-checkProgram :: (Has (Lift IO) sig m, HasCallStack) => Program u i o -> m ()
-checkProgram = runLiftIO . checkStatus glGetProgramiv glGetProgramInfoLog Other GL_LINK_STATUS . unProgram
-
-
 type HasUniform sym t u = (KnownSymbol sym, Uniform t, HasField sym (u Identity) (Identity t))
 
 
-build :: forall u i o m sig . (Has Finally sig m, Has (Lift IO) sig m, DSL.Vars i) => DSL.Shader u i o -> m (Program u i o)
-build p = do
-  program <- runLiftIO glCreateProgram
-  onExit (runLiftIO (glDeleteProgram program))
-  DSL.foldVarsM @i (\ DSL.Field { DSL.name, DSL.location } _ -> runLiftIO . checkingGLError $
+build :: forall u i o m sig . (HasCallStack, Has Finally sig m, Has (Lift IO) sig m, DSL.Vars i) => DSL.Shader u i o -> m (Program u i o)
+build p = runLiftIO $ do
+  program <- glCreateProgram
+  onExit (glDeleteProgram program)
+  DSL.foldVarsM @i (\ DSL.Field { DSL.name, DSL.location } _ -> checkingGLError $
     C.withCString name (glBindAttribLocation program (fromIntegral location))) (DSL.makeVars id)
   shaders <- for (DSL.shaderSources p) $ \ (type', source) -> do
     shader <- createShader type'
     shader <$ compile source shader
-  Program program <$ link shaders (Program program)
+
+  for_ shaders (glAttachShader program . unShader)
+  glLinkProgram program
+  for_ shaders (glDetachShader program . unShader)
+
+  checkStatus glGetProgramiv glGetProgramInfoLog Other GL_LINK_STATUS program
+
+  pure (Program program)
 
 use :: (Has (Lift IO) sig m) => Program u i o -> ProgramT u i o m a -> m a
 use (Program p) (ProgramT m) = do
