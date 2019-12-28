dd00baeb2373a5dca04b0737dd3ef80b4d925769
Rob Rix <rob.rix@me.com>
2019-12-28 17:20:28 -0500

    Re-export Control.Effect.Profile.

--- src/Control/Carrier/Profile/Identity.hs
+++ src/Control/Carrier/Profile/Identity.hs
@@ -3,8 +3,11 @@ module Control.Carrier.Profile.Identity
 ( -- * Profiling carrier
   runProfile
 , ProfileC(ProfileC)
+  -- * Profile effect
+, module Control.Effect.Profile
 ) where
 
+import Control.Effect.Profile
 import Control.Monad.IO.Class
 
 newtype ProfileC m a = ProfileC { runProfile :: m a }
