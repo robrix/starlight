8b0d29b0e32140c2b7cc5f7d35da31a742d5372d
Rob Rix <rob.rix@me.com>
2019-12-28 17:19:54 -0500

    Derive some instances for ProfileC.

--- src/Control/Carrier/Profile/Identity.hs
+++ src/Control/Carrier/Profile/Identity.hs
@@ -1,7 +1,11 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module Control.Carrier.Profile.Identity
 ( -- * Profiling carrier
   runProfile
 , ProfileC(ProfileC)
 ) where
 
+import Control.Monad.IO.Class
+
 newtype ProfileC m a = ProfileC { runProfile :: m a }
+  deriving (Applicative, Functor, Monad, MonadIO)
