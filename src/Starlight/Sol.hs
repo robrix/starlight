e297c31fd385131f82353c24350afbd21b278dff
Rob Rix <rob.rix@me.com>
2019-12-27 23:03:55 -0500

    Look up orbits in the map.

--- src/Starlight/Sol.hs
+++ src/Starlight/Sol.hs
@@ -61,13 +61,7 @@ sol = Body
   , orientation = axisAngle (unit _x) (getRadians (fromDegrees 7.25))
   , period      = fromDays 25.05
   , colour      = V4 1 1 0 1
-  , orbit       = Orbit
-    { semimajor       = 0
-    , eccentricity    = 0
-    , orientation     = 0
-    , period          = 1
-    , timeOfPeriapsis = 0
-    }
+  , orbit       = snd (orbits IntMap.! code sol)
   , parent      = Nothing
   }
 
@@ -80,7 +74,7 @@ mercury = Body
   , orientation = axisAngle (unit _x) (getRadians (fromDegrees 0.034))
   , period      = fromDays 58.646
   , colour      = V4 0.5 0.5 0.5 1
-  , orbit       = $(mkOrbitFromFile ("ephemerides" </> "199 mercury.txt"))
+  , orbit       = snd (orbits IntMap.! code mercury)
   , parent      = Just sol
   }
 
@@ -93,7 +87,7 @@ venus = Body
   , orientation = axisAngle (unit _x) (getRadians (fromDegrees 177.3))
   , period      = fromDays 243.025
   , colour      = V4 1 1 0.5 1
-  , orbit       = $(mkOrbitFromFile ("ephemerides" </> "299 venus.txt"))
+  , orbit       = snd (orbits IntMap.! code venus)
   , parent      = Just sol
   }
 
@@ -106,7 +100,7 @@ earth = Body
   , orientation = axisAngle (unit _x) (getRadians (fromDegrees 23.4392911))
   , period      = fromDays 0.99726968
   , colour      = V4 0 0 1 1
-  , orbit       = $(mkOrbitFromFile ("ephemerides" </> "399 earth.txt"))
+  , orbit       = snd (orbits IntMap.! code earth)
   , parent      = Just sol
   }
 
@@ -119,7 +113,7 @@ luna = Body
   , orientation = axisAngle (unit _x) (getRadians (fromDegrees 6.687))
   , period      = fromDays 27.321661
   , colour      = V4 0.5 0.5 0.5 1
-  , orbit       = $(mkOrbitFromFile ("ephemerides" </> "301 luna.txt"))
+  , orbit       = snd (orbits IntMap.! code luna)
   , parent      = Just earth
   }
 
@@ -132,7 +126,7 @@ mars = Body
   , orientation = axisAngle (unit _x) (getRadians (fromDegrees 25.19))
   , period      = fromDays 1.025957
   , colour      = V4 1 0 0 1
-  , orbit       = $(mkOrbitFromFile ("ephemerides" </> "499 mars.txt"))
+  , orbit       = snd (orbits IntMap.! code mars)
   , parent      = Just sol
   }
 
@@ -145,7 +139,7 @@ jupiter = Body
   , orientation = axisAngle (unit _x) (getRadians (fromDegrees 3.13))
   , period      = fromHours 9.925
   , colour      = V4 0.5 0.5 0 1
-  , orbit       = $(mkOrbitFromFile ("ephemerides" </> "599 jupiter.txt"))
+  , orbit       = snd (orbits IntMap.! code jupiter)
   , parent      = Just sol
   }
 
@@ -158,7 +152,7 @@ saturn = Body
   , orientation = axisAngle (unit _x) (getRadians (fromDegrees 26.73))
   , period      = fromHours 10 + fromMinutes 33 + Seconds 38
   , colour      = V4 (229/255) (216/255) (167/255) 1
-  , orbit       = $(mkOrbitFromFile ("ephemerides" </> "699 saturn.txt"))
+  , orbit       = snd (orbits IntMap.! code saturn)
   , parent      = Just sol
   }
 
@@ -171,7 +165,7 @@ uranus = Body
   , orientation = axisAngle (unit _x) (getRadians (fromDegrees 97.77))
   , period      = fromDays 0.71833
   , colour      = V4 (196/255) (221/255) (240/255) 1
-  , orbit       = $(mkOrbitFromFile ("ephemerides" </> "799 uranus.txt"))
+  , orbit       = snd (orbits IntMap.! code uranus)
   , parent      = Just sol
   }
 
@@ -184,6 +178,6 @@ neptune = Body
   , orientation = axisAngle (unit _x) (getRadians (fromDegrees 28.32))
   , period      = fromDays 0.6713
   , colour      = V4 (138/255) (163/255) (217/255) 1
-  , orbit       = $(mkOrbitFromFile ("ephemerides" </> "899 neptune.txt"))
+  , orbit       = snd (orbits IntMap.! code neptune)
   , parent      = Just sol
   }
