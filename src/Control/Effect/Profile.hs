8274ab3fb8bdff4dff00ba1108d01ef75aa94501
Rob Rix <rob.rix@me.com>
2019-12-28 12:25:06 -0500

    Derive a Functor instance for Profile.

--- src/Control/Effect/Profile.hs
+++ src/Control/Effect/Profile.hs
@@ -1,7 +1,9 @@
-{-# LANGUAGE ExistentialQuantification #-}
+{-# LANGUAGE DeriveFunctor, ExistentialQuantification, StandaloneDeriving #-}
 module Control.Effect.Profile
 ( Profile(..)
 ) where
 
 data Profile m k
   = forall a . Measure String (m a) (a -> m k)
+
+deriving instance Functor m => Functor (Profile m)
