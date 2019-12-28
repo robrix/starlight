c7af9732c8826c58964332870ec1e61254c17a00
Rob Rix <rob.rix@me.com>
2019-12-28 12:33:59 -0500

    Define a Tally counter for ProfileC.

--- src/Control/Carrier/Profile/Time.hs
+++ src/Control/Carrier/Profile/Time.hs
@@ -2,6 +2,7 @@
 module Control.Carrier.Profile.Time
 ( -- * Profile carrier
   ProfileC(..)
+, Tally(..)
   -- * Profile effect
 , module Control.Effect.Profile
 ) where
@@ -11,3 +12,9 @@ import Control.Monad.IO.Class
 
 newtype ProfileC m a = ProfileC (m a)
   deriving (Applicative, Functor, Monad, MonadIO)
+
+
+data Tally a = Tally
+  { tally :: !a
+  , count :: {-# UNPACK #-} !Int
+  }
