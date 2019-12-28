a80fffc713a3eee1c492d7039a58dfa9b17ce93b
Rob Rix <rob.rix@me.com>
2019-12-28 11:09:16 -0500

    Derive a bunch of instances for Milli.

--- src/Unit.hs
+++ src/Unit.hs
@@ -1,5 +1,20 @@
+{-# LANGUAGE DeriveTraversable #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
 module Unit
 ( Milli(..)
 ) where
 
+import Data.Proxy
+import Foreign.Storable
+import GL.Type as GL
+import GL.Uniform
+
 newtype Milli f a = Milli { getMilli :: f a }
+  deriving (Eq, Foldable, Floating, Fractional, Functor, Num, Ord, Read, Real, RealFloat, RealFrac, Show, Storable, Traversable, Uniform)
+
+instance GL.Type (f a) => GL.Type (Milli f a) where
+  glType _ = glType (Proxy @(f a))
+
+  glDims _ = glDims (Proxy @(f a))
