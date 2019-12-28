17c616f7590a03d305a672ee4124192229e3f1eb
Rob Rix <rob.rix@me.com>
2019-12-28 11:10:30 -0500

    Milli does not have a selector.

--- src/Unit.hs
+++ src/Unit.hs
@@ -4,6 +4,7 @@
 {-# LANGUAGE TypeApplications #-}
 module Unit
 ( Milli(..)
+, getMilli
 ) where
 
 import Data.Proxy
@@ -11,10 +12,13 @@ import Foreign.Storable
 import GL.Type as GL
 import GL.Uniform
 
-newtype Milli f a = Milli { getMilli :: f a }
+newtype Milli f a = Milli (f a)
   deriving (Eq, Foldable, Floating, Fractional, Functor, Num, Ord, Read, Real, RealFloat, RealFrac, Show, Storable, Traversable, Uniform)
 
 instance GL.Type (f a) => GL.Type (Milli f a) where
   glType _ = glType (Proxy @(f a))
 
   glDims _ = glDims (Proxy @(f a))
+
+getMilli :: Milli f a -> f a
+getMilli (Milli fa) = fa
