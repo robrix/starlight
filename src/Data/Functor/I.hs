a7c91302ff0af406caf416dcc4a432c9d448943d
Rob Rix <rob.rix@me.com>
2019-12-29 12:02:56 -0500

    Derive instances for I.

--- src/Data/Functor/I.hs
+++ src/Data/Functor/I.hs
@@ -1,6 +1,8 @@
+{-# LANGUAGE DeriveTraversable #-}
 -- | An abbreviation of "Data.Functor.Identity".
 module Data.Functor.I
 ( I(..)
 ) where
 
 newtype I a = I a
+  deriving (Eq, Foldable, Functor, Ord, Show, Traversable)
