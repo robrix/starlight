67b1322a0c4ffd524ff0f3a2bbba0f419f9450eb
Rob Rix <rob.rix@me.com>
2019-12-29 11:56:08 -0500

    Define an Applicative instance for Interval.

--- src/Data/Functor/Interval.hs
+++ src/Data/Functor/Interval.hs
@@ -8,3 +8,7 @@ data Interval f a = Interval
   , max_ :: !(f a)
   }
   deriving (Eq, Foldable, Functor, Show, Traversable)
+
+instance Applicative f => Applicative (Interval f) where
+  pure a = Interval (pure a) (pure a)
+  Interval f1 f2 <*> Interval a1 a2 = Interval (f1 <*> a1) (f2 <*> a2)
