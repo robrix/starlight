15aaf8dc06351554bbceb0eefa43ee73fcbf455d
Rob Rix <rob.rix@me.com>
2019-12-29 19:31:31 -0500

    Log the framebuffer errors.

--- src/GL/Error.hs
+++ src/GL/Error.hs
@@ -24,18 +24,26 @@ data GLError
   | InvalidOperation
   | InvalidFramebufferOperation
   | OutOfMemory
+  | FramebufferIncompleteAttachment
+  -- | FramebufferIncompleteDimensions
+  | FramebufferIncompleteMissingAttachment
+  | FramebufferUnsupported
   | Source String String
   | Other String
 
 instance Show GLError where
   showsPrec _ = \case
-    InvalidEnum                 -> showString "GL_INVALID_ENUM"
-    InvalidValue                -> showString "GL_INVALID_VALUE"
-    InvalidOperation            -> showString "GL_INVALID_OPERATION"
-    InvalidFramebufferOperation -> showString "GL_INVALID_FRAMEBUFFER_OPERATION"
-    OutOfMemory                 -> showString "GL_OUT_OF_MEMORY"
-    Source s t                  -> showString s . showChar '\n' . showString t
-    Other s                     -> showString s
+    InvalidEnum                            -> showString "GL_INVALID_ENUM"
+    InvalidValue                           -> showString "GL_INVALID_VALUE"
+    InvalidOperation                       -> showString "GL_INVALID_OPERATION"
+    InvalidFramebufferOperation            -> showString "GL_INVALID_FRAMEBUFFER_OPERATION"
+    OutOfMemory                            -> showString "GL_OUT_OF_MEMORY"
+    FramebufferIncompleteAttachment        -> showString "GL_FRAMEBUFFER_INCOMPLETE_ATTACHMENT"
+    -- FramebufferIncompleteDimensions        -> showString "GL_FRAMEBUFFER_INCOMPLETE_DIMENSIONS"
+    FramebufferIncompleteMissingAttachment -> showString "GL_FRAMEBUFFER_INCOMPLETE_MISSING_ATTACHMENT"
+    FramebufferUnsupported                 -> showString "GL_FRAMEBUFFER_UNSUPPORTED"
+    Source s t                             -> showString s . showChar '\n' . showString t
+    Other s                                -> showString s
 
 
 data GLException = GLException GLError CallStack
@@ -77,10 +85,14 @@ checkingGLError action = withFrozenCallStack $ do
 
 throwGLError :: (Has (Lift IO) sig m, HasCallStack) => GLenum -> m ()
 throwGLError = \case
-  GL_NO_ERROR                      -> pure ()
-  GL_INVALID_ENUM                  -> E.throwIO $ GLException InvalidEnum                 callStack
-  GL_INVALID_VALUE                 -> E.throwIO $ GLException InvalidValue                callStack
-  GL_INVALID_OPERATION             -> E.throwIO $ GLException InvalidOperation            callStack
-  GL_INVALID_FRAMEBUFFER_OPERATION -> E.throwIO $ GLException InvalidFramebufferOperation callStack
-  GL_OUT_OF_MEMORY                 -> E.throwIO $ GLException OutOfMemory                 callStack
-  _                                -> E.throwIO $ GLException (Other "Unknown")           callStack
+  GL_NO_ERROR                                  -> pure ()
+  GL_INVALID_ENUM                              -> E.throwIO $ GLException InvalidEnum                            callStack
+  GL_INVALID_VALUE                             -> E.throwIO $ GLException InvalidValue                           callStack
+  GL_INVALID_OPERATION                         -> E.throwIO $ GLException InvalidOperation                       callStack
+  GL_INVALID_FRAMEBUFFER_OPERATION             -> E.throwIO $ GLException InvalidFramebufferOperation            callStack
+  GL_OUT_OF_MEMORY                             -> E.throwIO $ GLException OutOfMemory                            callStack
+  GL_FRAMEBUFFER_INCOMPLETE_ATTACHMENT         -> E.throwIO $ GLException FramebufferIncompleteAttachment        callStack
+  -- GL_FRAMEBUFFER_INCOMPLETE_DIMENSIONS         -> E.throwIO $ GLException FramebufferIncompleteDimensions        callStack
+  GL_FRAMEBUFFER_INCOMPLETE_MISSING_ATTACHMENT -> E.throwIO $ GLException FramebufferIncompleteMissingAttachment callStack
+  GL_FRAMEBUFFER_UNSUPPORTED                   -> E.throwIO $ GLException FramebufferUnsupported                 callStack
+  _                                            -> E.throwIO $ GLException (Other "Unknown")                      callStack
