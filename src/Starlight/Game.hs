95e8c8490f8b5ed9a245cbec2e885332b27d52b1
Rob Rix <rob.rix@me.com>
2019-12-31 15:48:19 -0500

    Add an NPC targetting Mercury.

--- src/Starlight/Game.hs
+++ src/Starlight/Game.hs
@@ -125,6 +125,12 @@ runGame = do
           , rotation = axisAngle (unit _z) (pi/2)
           , target   = Just $ Star (10, "Sol")
           }
+        , Actor
+          { position = P (V2 250000 0)
+          , velocity = V2 0 150
+          , rotation = axisAngle (unit _z) (pi/2)
+          , target   = Just $ Star (10, "Sol") :/ (199, "Mercury")
+          }
         ]
       , system = system
       }
