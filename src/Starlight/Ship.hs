e1cdee56735a11250afbac28712830505605e7e1
Rob Rix <rob.rix@me.com>
2019-12-31 16:58:07 -0500

    Define a smart constructor for DrawShip.

--- src/Starlight/Ship.hs
+++ src/Starlight/Ship.hs
@@ -1,6 +1,10 @@
+{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE TypeApplications #-}
 module Starlight.Ship
-( DrawShip
+( makeDrawShip
+, DrawShip
 , drawShip
 ) where
 
@@ -8,9 +12,42 @@ import Control.Effect.Finally
 import Control.Effect.Lift
 import Control.Effect.Profile
 import Control.Effect.Reader
+import Data.Coerce (coerce)
+import Data.Functor.I
+import Data.Functor.Interval
+import GL.Array
+import GL.Program
+import Linear.Affine
+import Linear.Exts
+import Linear.Matrix
+import Linear.V2
+import Linear.V4
 import Starlight.Actor
+import Starlight.Ship.Shader as Ship
 import Starlight.View
 import UI.Colour
+
+makeDrawShip
+  :: ( Has Finally sig m
+     , Has (Lift IO) sig m
+     )
+  => m DrawShip
+makeDrawShip = do
+  program <- build Ship.shader
+  array <- load vertices
+  pure DrawShip
+    { drawShip = \ colour Actor{ position, rotation } -> measure "ship" . use program . bindArray array $ do
+      matrix <- asks scaleToViewZoomed
+      set Ship.U
+        { matrix = Just
+            $   matrix
+            !*! translated3 (ext (unP position) 0)
+            !*! scaled (V4 15 15 15 1)
+            !*! mkTransformation rotation 0
+        , colour = Just colour
+        }
+      drawArrays LineStrip range
+    }
 
 newtype DrawShip = DrawShip
   { drawShip
@@ -23,3 +60,15 @@ newtype DrawShip = DrawShip
     -> Actor
     -> m ()
   }
+
+
+vertices :: [Ship.V I]
+vertices = coerce @[V2 Float]
+  [ V2 1      0
+  , V2 0      (-0.5)
+  , V2 (-0.5) 0
+  , V2 0      0.5
+  ]
+
+range :: Interval I Int
+range = Interval 0 4
