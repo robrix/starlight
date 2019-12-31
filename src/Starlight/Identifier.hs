e64ca78c201bc3737c4f03ecada9fd615052b1e9
Rob Rix <rob.rix@me.com>
2019-12-31 13:15:17 -0500

    Derive some instances for Name & Identifier.

--- src/Starlight/Identifier.hs
+++ src/Starlight/Identifier.hs
@@ -9,7 +9,9 @@ import Data.Text
 type Code = Int
 
 newtype Name = Name { getName :: Text }
+  deriving (Eq, Ord, Read, Show)
 
 data Identifier
   = Star Code Name
   | Identifier :/ (Code, Name)
+  deriving (Eq, Ord, Read, Show)
