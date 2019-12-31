49c476e2a769504e98bc72db2ffbfc8af2cef790
Rob Rix <rob.rix@me.com>
2019-12-31 16:51:23 -0500

    Define a DrawShip type.

--- src/Starlight/Ship.hs
+++ src/Starlight/Ship.hs
@@ -1,2 +1,25 @@
+{-# LANGUAGE RankNTypes #-}
 module Starlight.Ship
-() where
+( DrawShip
+, drawShip
+) where
+
+import Control.Effect.Finally
+import Control.Effect.Lift
+import Control.Effect.Profile
+import Control.Effect.Reader
+import Starlight.Actor
+import Starlight.View
+import UI.Colour
+
+newtype DrawShip = DrawShip
+  { drawShip
+    :: forall sig m
+    .  ( Has (Lift IO) sig m
+       , Has Profile sig m
+       , Has (Reader ViewScale) sig m
+       )
+    => Colour Float
+    -> Actor
+    -> m ()
+  }
