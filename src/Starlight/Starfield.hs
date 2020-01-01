43f3697cb12092c29786d434fa1877eb343872d5
Rob Rix <rob.rix@me.com>
2020-01-01 00:09:23 -0500

    :fire: unnecessary qualifiers.

--- src/Starlight/Starfield.hs
+++ src/Starlight/Starfield.hs
@@ -9,20 +9,20 @@ module Starlight.Starfield
 , DrawStarfield
 ) where
 
-import           Control.Effect.Finally
-import           Control.Effect.Lift
-import           Control.Effect.Profile
-import           Control.Carrier.Reader
-import           Data.Coerce (coerce)
-import           Data.Functor.I
-import           Data.Functor.Interval
-import           GL.Array
-import           GL.Program
-import           Linear.Affine
-import           Linear.V2
-import           Linear.Vector
-import qualified Starlight.Starfield.Shader as Starfield
-import           Starlight.View
+import Control.Carrier.Reader
+import Control.Effect.Finally
+import Control.Effect.Lift
+import Control.Effect.Profile
+import Data.Coerce (coerce)
+import Data.Functor.I
+import Data.Functor.Interval
+import GL.Array
+import GL.Program
+import Linear.Affine
+import Linear.V2
+import Linear.Vector
+import Starlight.Starfield.Shader
+import Starlight.View
 
 starfield
   :: ( Has (Lift IO) sig m
@@ -36,7 +36,7 @@ starfield = do
   measure "starfield" . use program . bindArray array $ do
     View{ scale, size, zoom, focus } <- ask
 
-    set Starfield.U
+    set U
       { resolution = Just (fromIntegral <$> size ^* scale)
       , origin     = Just (focus / P (fromIntegral <$> size))
       , zoom       = Just zoom
@@ -52,18 +52,18 @@ runStarfield
   => ReaderC DrawStarfield m a
   -> m a
 runStarfield m = do
-  program <- build Starfield.shader
+  program <- build shader
   array   <- load vertices
   runReader DrawStarfield{ program, array } m
 
 
 data DrawStarfield = DrawStarfield
-  { program :: Program Starfield.U Starfield.V Starfield.O
-  , array   :: Array (Starfield.V I)
+  { program :: Program U V O
+  , array   :: Array (V I)
   }
 
 
-vertices :: [Starfield.V I]
+vertices :: [V I]
 vertices = coerce @[V2 Float]
   [ V2 (-1) (-1)
   , V2   1  (-1)
