68a0e6ff6d07f14c88c566727e7ac9bb0152932b
Rob Rix <rob.rix@me.com>
2020-01-01 15:00:00 -0500

    Move controls into its own module.

--- src/Starlight/Controls.hs
+++ src/Starlight/Controls.hs
@@ -1,2 +1,97 @@
+{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE TypeApplications #-}
 module Starlight.Controls
-() where
+( controls
+, Player(..)
+, _actor
+, _throttle
+, _firing
+) where
+
+import           Control.Applicative (liftA2)
+import           Control.Effect.Lens
+import           Control.Effect.Lift
+import           Control.Effect.Reader
+import           Control.Effect.State
+import           Control.Monad (guard, when)
+import           Data.Coerce (coerce)
+import           Data.Functor.Const
+import           Data.Ix
+import           Data.List (elemIndex)
+import qualified Data.Map as Map
+import           Lens.Micro
+import           Linear.Exts
+import           Linear.Quaternion
+import           Linear.V3
+import           Linear.Vector
+import qualified SDL
+import           Starlight.Actor
+import           Starlight.Body
+import           Starlight.Input
+import           Starlight.System
+import           Unit.Angle
+import           Unit.Time
+
+controls
+  :: ( Has (Lift IO) sig m
+     , Has (Reader (System StateVectors Float)) sig m
+     , Has (State Input) sig m
+     , Has (State Player) sig m
+     )
+  => Delta Seconds Float
+  -> m ()
+controls (Delta (Seconds dt)) = do
+  input <- get
+  when (input ^. (_pressed SDL.KeycodePlus `or` _pressed SDL.KeycodeEquals)) $
+    _throttle += dt * 10
+  when (input ^. _pressed SDL.KeycodeMinus) $
+    _throttle -= dt * 10
+
+  thrust <- uses (_throttle) (dt *)
+
+  let angular = dt *^ Radians 5
+
+  when (input ^. _pressed SDL.KeycodeUp) $ do
+    rotation <- use (_actor . _rotation)
+    _actor . _velocity += rotate rotation (unit _x ^* thrust) ^. _xy
+  when (input ^. _pressed SDL.KeycodeDown) $ do
+    rotation <- use (_actor . _rotation)
+    velocity <- use (_actor . _velocity)
+    _actor . _rotation .= face angular (angleOf (negated velocity)) rotation
+
+  when (input ^. _pressed SDL.KeycodeLeft) $
+    _actor . _rotation *= axisAngle (unit _z) (getRadians angular)
+  when (input ^. _pressed SDL.KeycodeRight) $
+    _actor . _rotation *= axisAngle (unit _z) (getRadians (-angular))
+
+  _firing .= input ^. _pressed SDL.KeycodeSpace
+
+  System{ bodies } <- ask @(System StateVectors Float)
+  let identifiers = Map.keys bodies
+      switchTarget shift target = case target >>= (`elemIndex` identifiers) of
+        Just i  -> identifiers !! i' <$ guard (inRange (0, pred (length bodies)) i') where
+          i' | shift     = i - 1
+             | otherwise = i + 1
+        Nothing -> Just $ if shift then last identifiers else head identifiers
+  when (input ^. _pressed SDL.KeycodeTab) $ do
+    _actor . _target %= switchTarget (input ^. (_pressed SDL.KeycodeLShift `or` _pressed SDL.KeycodeRShift))
+    _pressed SDL.KeycodeTab .= False
+  where
+  or = liftA2 (liftA2 (coerce (||)))
+
+
+data Player = Player
+  { actor    :: !Actor
+  , throttle :: !Float
+  , firing   :: !Bool
+  }
+  deriving (Show)
+
+_actor :: Lens' Player Actor
+_actor = lens actor (\ s a -> s { actor = a })
+
+_throttle :: Lens' Player Float
+_throttle = lens throttle (\ s v -> s { throttle = v })
+
+_firing :: Lens' Player Bool
+_firing = lens firing (\ s p -> s { firing = p })
--- src/Starlight/Game.hs
+++ src/Starlight/Game.hs
@@ -12,14 +12,13 @@ module Starlight.Game
 ( main
 ) where
 
-import           Control.Applicative (liftA2)
 import           Control.Carrier.Empty.Maybe
 import           Control.Carrier.Finally
 import qualified Control.Carrier.Profile.Identity as NoProfile
 import qualified Control.Carrier.Profile.Time as Profile
 import           Control.Carrier.Reader
 import           Control.Carrier.State.Strict
-import           Control.Effect.Lens ((%=), (*=), (+=), (-=), (.=))
+import           Control.Effect.Lens ((%=), (.=))
 import qualified Control.Effect.Lens as Lens
 import           Control.Effect.Lift
 import           Control.Effect.Profile
@@ -29,13 +28,9 @@ import           Control.Monad.IO.Class.Lift (MonadIO, runLiftIO)
 import           Data.Coerce
 import           Data.Foldable (find, for_)
 import           Data.Function (fix)
-import           Data.Functor.Const
 import           Data.Functor.Identity
 import           Data.Functor.Interval
-import           Data.Ix (inRange)
-import           Data.List (elemIndex)
 import           Data.List.NonEmpty (NonEmpty(..))
-import qualified Data.Map as Map
 import           Data.Maybe (isJust)
 import           Data.Time.Clock (NominalDiffTime, UTCTime, diffUTCTime, getCurrentTime)
 import           GL.Framebuffer
@@ -50,11 +45,11 @@ import           Linear.V2 as Linear
 import           Linear.V3 as Linear
 import           Linear.Vector as Linear
 import           Numeric
-import qualified SDL
 import           Starlight.Actor as Actor
 import           Starlight.AI
 import           Starlight.Body as Body
 import           Starlight.CLI
+import           Starlight.Controls
 import           Starlight.Identifier
 import           Starlight.Input
 import           Starlight.Physics
@@ -72,7 +67,6 @@ import           UI.Colour
 import           UI.Label as Label
 import           UI.Typeface (Font(Font), cacheCharactersForDrawing, readTypeface)
 import qualified UI.Window as Window
-import           Unit.Angle
 import           Unit.Length
 import           Unit.Time
 
@@ -168,53 +162,6 @@ runGame = do
           continue <$ measure "swap" Window.swap
         when continue loop
 
-controls
-  :: ( Has (Lift IO) sig m
-     , Has (Reader (System StateVectors Float)) sig m
-     , Has (State Input) sig m
-     , Has (State Player) sig m
-     )
-  => Delta Seconds Float
-  -> m ()
-controls (Delta (Seconds dt)) = do
-  input <- get
-  when (input ^. (_pressed SDL.KeycodePlus `or` _pressed SDL.KeycodeEquals)) $
-    _throttle += dt * 10
-  when (input ^. _pressed SDL.KeycodeMinus) $
-    _throttle -= dt * 10
-
-  thrust <- Lens.uses (_throttle) (dt *)
-
-  let angular = dt *^ Radians 5
-
-  when (input ^. _pressed SDL.KeycodeUp) $ do
-    rotation <- Lens.use (_actor . _rotation)
-    _actor . _velocity += rotate rotation (unit _x ^* thrust) ^. _xy
-  when (input ^. _pressed SDL.KeycodeDown) $ do
-    rotation <- Lens.use (_actor . _rotation)
-    velocity <- Lens.use (_actor . _velocity)
-    _actor . _rotation .= face angular (angleOf (negated velocity)) rotation
-
-  when (input ^. _pressed SDL.KeycodeLeft) $
-    _actor . _rotation *= axisAngle (unit _z) (getRadians angular)
-  when (input ^. _pressed SDL.KeycodeRight) $
-    _actor . _rotation *= axisAngle (unit _z) (getRadians (-angular))
-
-  _firing .= input ^. _pressed SDL.KeycodeSpace
-
-  System{ bodies } <- ask @(System StateVectors Float)
-  let identifiers = Map.keys bodies
-      switchTarget shift target = case target >>= (`elemIndex` identifiers) of
-        Just i  -> identifiers !! i' <$ guard (inRange (0, pred (length bodies)) i') where
-          i' | shift     = i - 1
-             | otherwise = i + 1
-        Nothing -> Just $ if shift then last identifiers else head identifiers
-  when (input ^. _pressed SDL.KeycodeTab) $ do
-    _actor . _target %= switchTarget (input ^. (_pressed SDL.KeycodeLShift `or` _pressed SDL.KeycodeRShift))
-    _pressed SDL.KeycodeTab .= False
-  where
-  or = liftA2 (liftA2 (coerce (||)))
-
 (%%=) :: Has (State s) sig m => Lens' s a -> StateC a m () -> m ()
 lens %%= action = Lens.use lens >>= (`execState` action) >>= (lens .=)
 
@@ -301,22 +248,6 @@ withView game m = do
       zoom = zoomForSpeed size (norm velocity)
       focus = game ^. _player . _actor . _position
   runReader View{ scale, size, zoom, focus } m
-
-data Player = Player
-  { actor :: !Actor
-  , throttle :: !Float
-  , firing   :: !Bool
-  }
-  deriving (Show)
-
-_actor :: Lens' Player Actor
-_actor = lens actor (\ s a -> s { actor = a })
-
-_throttle :: Lens' Player Float
-_throttle = lens throttle (\ s v -> s { throttle = v })
-
-_firing :: Lens' Player Bool
-_firing = lens firing (\ s p -> s { firing = p })
 
 data GameState = GameState
   { player   :: !Player
