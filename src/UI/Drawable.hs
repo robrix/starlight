c6a84ffa1f0a290715b0479987d0a2f146db73c6
Rob Rix <rob.rix@me.com>
2020-01-01 00:16:45 -0500

    Define a helper for using the program & array in a Drawable.

--- src/UI/Drawable.hs
+++ src/UI/Drawable.hs
@@ -1,7 +1,11 @@
+{-# LANGUAGE NamedFieldPuns #-}
 module UI.Drawable
 ( Drawable(..)
+, using
 ) where
 
+import Control.Effect.Lift
+import Control.Effect.Reader
 import Data.Functor.I
 import GL.Array
 import GL.Program
@@ -10,3 +14,14 @@ data Drawable u v o = Drawable
   { program :: Program u v o
   , array   :: Array (v I)
   }
+
+using
+  :: ( Has (Lift IO) sig m
+     , Has (Reader a) sig m
+     )
+  => (a -> Drawable u v o)
+  -> ArrayT v (ProgramT u v o m) a
+  -> m a
+using getDrawable m = do
+  Drawable { program, array } <- asks getDrawable
+  use program $ bindArray array m
