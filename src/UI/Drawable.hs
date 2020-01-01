1365c34ae76ecec138cd377235545614c0222928
Rob Rix <rob.rix@me.com>
2020-01-01 00:08:53 -0500

    Define a Drawable datatype.

--- src/UI/Drawable.hs
+++ src/UI/Drawable.hs
@@ -1,2 +1,12 @@
 module UI.Drawable
-() where
+( Drawable(..)
+) where
+
+import Data.Functor.I
+import GL.Array
+import GL.Program
+
+data Drawable u v o = Drawable
+  { program :: Program u v o
+  , array   :: Array (v I)
+  }
