a368840bc0630ae3ee3c1f015d5da699524eadce
Rob Rix <rob.rix@me.com>
2020-01-02 12:39:28 -0500

    Simplify Predicate using ReaderC.

--- src/Control/Carrier/Reader/Predicate.hs
+++ src/Control/Carrier/Reader/Predicate.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 module Control.Carrier.Reader.Predicate
 ( -- * Predicates
@@ -7,30 +7,14 @@ module Control.Carrier.Reader.Predicate
 ) where
 
 import Control.Algebra
-import Control.Applicative (Alternative(..), liftA2)
-import Control.Effect.Reader
-import Control.Monad (ap, liftM)
+import Control.Applicative (Alternative)
+import Control.Carrier.Reader
 
 runPredicate :: i -> Predicate i a -> Maybe a
-runPredicate i (Predicate m) = m i
+runPredicate i (Predicate m) = runReader i m
 
-newtype Predicate i a = Predicate (i -> Maybe a)
-
-instance Functor (Predicate i) where
-  fmap = liftM
-
-instance Applicative (Predicate i) where
-  pure a = Predicate (\ _ -> Just a)
-  (<*>) = ap
-
-instance Alternative (Predicate i) where
-  empty = Predicate (\ _ -> Nothing)
-  Predicate l <|> Predicate r = Predicate (liftA2 (<|>) l r)
-
-instance Monad (Predicate i) where
-  Predicate m >>= f = Predicate (\ i -> m i >>= runPredicate i . f)
+newtype Predicate i a = Predicate (ReaderC i Maybe a)
+  deriving (Alternative, Applicative, Functor, Monad)
 
 instance Algebra (Reader i) (Predicate i) where
-  alg = \case
-    Ask       k -> Predicate Just                     >>= k
-    Local f m k -> Predicate ((`runPredicate` m) . f) >>= k
+  alg = Predicate . send . handleCoercible
