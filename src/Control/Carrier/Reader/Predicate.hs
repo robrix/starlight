602096417ce1404f0f8c5514677be5b0ce903eac
Rob Rix <rob.rix@me.com>
2020-01-02 12:42:40 -0500

    Define an expect operator lifting a getter to a predicate.

--- src/Control/Carrier/Reader/Predicate.hs
+++ src/Control/Carrier/Reader/Predicate.hs
@@ -4,11 +4,15 @@ module Control.Carrier.Reader.Predicate
 ( -- * Predicates
   runPredicate
 , Predicate(..)
+, expect
 ) where
 
 import Control.Algebra
 import Control.Applicative (Alternative)
 import Control.Carrier.Reader
+import Control.Effect.Lens (view)
+import Control.Monad (guard, (<=<))
+import Lens.Micro (Getting)
 
 runPredicate :: i -> Predicate i a -> Maybe a
 runPredicate i (Predicate m) = runReader i m
@@ -18,3 +22,7 @@ newtype Predicate i a = Predicate (ReaderC i Maybe a)
 
 instance Algebra (Reader i) (Predicate i) where
   alg = Predicate . send . handleCoercible
+
+
+expect :: (Alternative m, Has (Reader r) sig m) => Getting Bool r Bool -> m ()
+expect = guard <=< view
