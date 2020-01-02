0afeba993fd12b1dbca139700b731c63e5eccddb
Rob Rix <rob.rix@me.com>
2020-01-02 12:32:28 -0500

    Define a handler for Predicate.

--- src/Control/Carrier/Reader/Predicate.hs
+++ src/Control/Carrier/Reader/Predicate.hs
@@ -3,4 +3,7 @@ module Control.Carrier.Reader.Predicate
   Predicate(..)
 ) where
 
+runPredicate :: i -> Predicate i a -> Maybe a
+runPredicate i (Predicate m) = m i
+
 newtype Predicate i a = Predicate (i -> Maybe a)
