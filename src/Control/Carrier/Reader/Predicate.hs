7911a360c770f2a356ec2500edf81dc27f6c32cc
Rob Rix <rob.rix@me.com>
2020-01-02 12:33:01 -0500

    Define Functor, Applicative, & Monad instances for Predicate.

--- src/Control/Carrier/Reader/Predicate.hs
+++ src/Control/Carrier/Reader/Predicate.hs
@@ -3,7 +3,19 @@ module Control.Carrier.Reader.Predicate
   Predicate(..)
 ) where
 
+import Control.Monad (ap, liftM)
+
 runPredicate :: i -> Predicate i a -> Maybe a
 runPredicate i (Predicate m) = m i
 
 newtype Predicate i a = Predicate (i -> Maybe a)
+
+instance Functor (Predicate i) where
+  fmap = liftM
+
+instance Applicative (Predicate i) where
+  pure a = Predicate (\ _ -> Just a)
+  (<*>) = ap
+
+instance Monad (Predicate i) where
+  Predicate m >>= f = Predicate (\ i -> m i >>= runPredicate i . f)
