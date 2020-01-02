430d49505473ca41a1b7c09daa0f3a1635880a0d
Rob Rix <rob.rix@me.com>
2020-01-02 17:31:50 -0500

    Define a polymorphic lens for System’s bodies.

--- src/Starlight/System.hs
+++ src/Starlight/System.hs
@@ -3,11 +3,12 @@ module Starlight.System
 ( System(..)
 , systemTrans
 , scale_
+, bodies_
 , lookup
 ) where
 
 import qualified Data.Map as Map
-import           Lens.Micro (Lens', lens)
+import           Lens.Micro (Lens, Lens', lens)
 import           Linear.Matrix
 import           Linear.V4
 import           Linear.Vector
@@ -25,6 +26,9 @@ systemTrans System{ scale } = scaled (V4 scale scale scale 1)
 
 scale_ :: Lens' (System f a) a
 scale_ = lens scale (\ s scale -> s { scale })
+
+bodies_ :: Lens (System f a) (System g a) (Map.Map Identifier (f a)) (Map.Map Identifier (g a))
+bodies_ = lens bodies (\ s bodies -> s { bodies })
 
 lookup :: Identifier -> System f a -> Maybe (f a)
 lookup i = Map.lookup i . bodies
