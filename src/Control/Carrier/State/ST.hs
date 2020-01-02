a0044f62ce381f8d8e5acb9ea74617ae6b1ceae8
Rob Rix <rob.rix@me.com>
2020-01-02 00:31:11 -0500

    Stub in a State carrier.

--- src/Control/Carrier/State/ST.hs
+++ src/Control/Carrier/State/ST.hs
@@ -1,6 +1,14 @@
+{-# LANGUAGE RankNTypes #-}
 module Control.Carrier.State.ST
-( -- * State effect
-  module Control.Effect.State
+( -- * State carrier
+  StateC(..)
+  -- * State effect
+, module Control.Effect.State
 ) where
 
+import Control.Carrier.Reader
 import Control.Effect.State
+import Control.Monad.ST
+import Data.STRef
+
+newtype StateC s a = StateC (forall t . ReaderC (STRef t s) (ST t) a)
