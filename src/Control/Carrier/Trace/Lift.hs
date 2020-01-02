47b19aacbb11e91e5ce4bd7f61947418e20fe8a3
Rob Rix <rob.rix@me.com>
2020-01-02 16:49:07 -0500

    Define an Algebra instance for TraceC.

--- src/Control/Carrier/Trace/Lift.hs
+++ src/Control/Carrier/Trace/Lift.hs
@@ -1,4 +1,9 @@
+{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE UndecidableInstances #-}
 module Control.Carrier.Trace.Lift
 ( -- * Trace carrier
   runTrace
@@ -7,8 +12,16 @@ module Control.Carrier.Trace.Lift
 , module Control.Effect.Trace
 ) where
 
+import Control.Algebra
+import Control.Effect.Lift
 import Control.Effect.Trace
 import Control.Monad.IO.Class
+import System.IO
 
 newtype TraceC m a = TraceC { runTrace :: m a }
   deriving (Applicative, Functor, Monad, MonadIO)
+
+instance Has (Lift IO) sig m => Algebra (Trace :+: sig) (TraceC m) where
+  alg = \case
+    L (Trace s k) -> sendM (hPutStrLn stderr s) *> k
+    R other       -> TraceC (send (handleCoercible other))
