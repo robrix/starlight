495b88c8736a7c42daf75efa90aaf45fdf469d52
Rob Rix <rob.rix@me.com>
2020-01-02 00:42:51 -0500

    Define an Algebra instance for StateC.

--- src/Control/Carrier/State/ST.hs
+++ src/Control/Carrier/State/ST.hs
@@ -1,4 +1,6 @@
 {-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE RankNTypes #-}
 module Control.Carrier.State.ST
 ( -- * State carrier
@@ -7,6 +9,7 @@ module Control.Carrier.State.ST
 , module Control.Effect.State
 ) where
 
+import Control.Algebra
 import Control.Carrier.Reader
 import Control.Effect.State
 import Control.Monad (ap)
@@ -22,3 +25,10 @@ instance Applicative (StateC s) where
 
 instance Monad (StateC s) where
   StateC m >>= f = StateC (m >>= (\ (StateC m) -> m) . f)
+
+instance Algebra (State s) (StateC s) where
+  alg = \case
+    Get   k -> do
+      s <- StateC (ReaderC (\ ref -> readSTRef ref))
+      k s
+    Put s k -> StateC (ReaderC (\ ref -> writeSTRef ref s)) >> k
