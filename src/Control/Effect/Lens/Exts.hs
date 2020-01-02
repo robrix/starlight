3803c5227c48dad7ba6865e57585489eb2d2f7a3
Rob Rix <rob.rix@me.com>
2020-01-02 01:05:40 -0500

    Define a &~ combinator.

--- src/Control/Effect/Lens/Exts.hs
+++ src/Control/Effect/Lens/Exts.hs
@@ -1,5 +1,12 @@
 module Control.Effect.Lens.Exts
-( module Control.Effect.Lens
+( (&~)
+, module Control.Effect.Lens
 ) where
 
+import Control.Carrier.State.ST.Strict
 import Control.Effect.Lens
+
+(&~) :: s -> StateC s a -> s
+(&~) = execState
+
+infixl 1 &~
