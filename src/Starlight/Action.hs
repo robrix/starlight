16c82349ee6737baaa9a97ce730ed263405a2c2d
Rob Rix <rob.rix@me.com>
2020-01-02 11:20:27 -0500

    Define some datatypes describing actions.

--- src/Starlight/Action.hs
+++ src/Starlight/Action.hs
@@ -1,2 +1,22 @@
 module Starlight.Action
-() where
+( Action(..)
+, Direction(..)
+, Change(..)
+) where
+
+data Action
+  = Thrust
+  | TurnL
+  | TurnR
+  | Face Direction
+  | FireMain
+  | ChangeTarget (Maybe Change)
+
+data Direction
+  = Backwards
+  | Forwards
+  | Target
+
+data Change
+  = Prev
+  | Next
