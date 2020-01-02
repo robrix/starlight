34a2af8f797c2bb300fca20345a29968a1413431
Rob Rix <rob.rix@me.com>
2020-01-02 16:44:33 -0500

    Re-export Control.Effect.Trace.

--- src/Control/Carrier/Trace/Lift.hs
+++ src/Control/Carrier/Trace/Lift.hs
@@ -1,6 +1,10 @@
 module Control.Carrier.Trace.Lift
 ( -- * Trace carrier
   TraceC(..)
+  -- * Trace effect
+, module Control.Effect.Trace
 ) where
+
+import Control.Effect.Trace
 
 newtype TraceC m a = TraceC (m a)
