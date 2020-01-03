5c285d573ba12782242b98ae21b1774419ac228e
Rob Rix <rob.rix@me.com>
2020-01-03 11:44:57 -0500

    Define lenses for target & actions.

--- src/Starlight/Character.hs
+++ src/Starlight/Character.hs
@@ -1,13 +1,16 @@
+{-# LANGUAGE NamedFieldPuns #-}
 -- | Characters are player or non-player characters.
 module Starlight.Character
 ( Character(..)
 , actor_
+, target_
+, actions_
 ) where
 
 import Data.Set (Set)
 import Lens.Micro (Lens', lens)
 import Starlight.Action
-import Starlight.Actor
+import Starlight.Actor (Actor)
 import Starlight.Identifier
 
 data Character = Character
@@ -18,3 +21,9 @@ data Character = Character
 
 actor_ :: Lens' Character Actor
 actor_ = lens actor (\ s a -> s { actor = a })
+
+target_ :: Lens' Character (Maybe Identifier)
+target_ = lens target (\ s target -> s { target })
+
+actions_ :: Lens' Character (Set Action)
+actions_ = lens actions (\ s actions -> s { actions })
