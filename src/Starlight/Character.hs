eb1d4e79a13962e9671a889ba4cc8d4edde7ac31
Rob Rix <rob.rix@me.com>
2020-01-03 11:40:26 -0500

    Define a lens for the actor.

--- src/Starlight/Character.hs
+++ src/Starlight/Character.hs
@@ -1,10 +1,15 @@
 -- | Characters are player or non-player characters.
 module Starlight.Character
 ( Character(..)
+, actor_
 ) where
 
+import Lens.Micro (Lens', lens)
 import Starlight.Actor
 
 data Character = Character
   { actor :: !Actor
   }
+
+actor_ :: Lens' Character Actor
+actor_ = lens actor (\ s a -> s { actor = a })
