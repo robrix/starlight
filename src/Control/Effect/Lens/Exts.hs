5137023c7eaf0efe78cdff75065e34f34d894bcf
Rob Rix <rob.rix@me.com>
2020-01-03 23:21:03 -0500

    Define a ~> combinator.

--- src/Control/Effect/Lens/Exts.hs
+++ src/Control/Effect/Lens/Exts.hs
@@ -4,13 +4,14 @@ module Control.Effect.Lens.Exts
 , zoom
 , zoomEach
 , (<~)
+, (~>)
 , module Control.Effect.Lens
 ) where
 
 import Control.Carrier.State.ST.Strict as ST
 import Control.Carrier.State.Strict as Strict
 import Control.Effect.Lens
-import Control.Lens (ASetter, Lens')
+import Control.Lens (ASetter, Getting, Lens')
 
 (&~) :: s -> ST.StateC s a -> s
 (&~) = ST.execState
@@ -19,13 +20,13 @@ infixl 1 &~
 
 
 zoom :: Has (State s) sig m => Lens' s a -> Strict.StateC a m () -> m ()
-zoom lens action = lens <~ (use lens >>= (`Strict.execState` action))
+zoom lens action = lens <~ (lens ~> (`Strict.execState` action))
 
 infixr 2 `zoom`
 
 
 zoomEach :: (Has (State s) sig m, Traversable t) => Lens' s (t a) -> Strict.StateC a m () -> m ()
-zoomEach lens action = lens <~ (use lens >>= traverse (`Strict.execState` action))
+zoomEach lens action = lens <~ (lens ~> traverse (`Strict.execState` action))
 
 infixr 2 `zoomEach`
 
@@ -34,3 +35,8 @@ infixr 2 `zoomEach`
 lens <~ act = act >>= assign lens
 
 infixr 2 <~
+
+(~>) :: (Has (State s) sig m) => Getting a s a -> (a -> m b) -> m b
+lens ~> act = use lens >>= act
+
+infixr 2 ~>
