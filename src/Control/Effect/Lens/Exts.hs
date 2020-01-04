4ef0be85d4c03d45ad2470a956feb9761c57906e
Rob Rix <rob.rix@me.com>
2020-01-04 14:20:20 -0500

    :fire: &~, zoom, & zoomEach.

--- src/Control/Effect/Lens/Exts.hs
+++ src/Control/Effect/Lens/Exts.hs
@@ -1,9 +1,6 @@
 {-# LANGUAGE RankNTypes #-}
 module Control.Effect.Lens.Exts
-( (&~)
-, zoom
-, zoomEach
-, (~>)
+( (~>)
 , (<~>)
 , (<--)
 , (-->)
@@ -11,27 +8,9 @@ module Control.Effect.Lens.Exts
 , module Control.Effect.Lens
 ) where
 
-import Control.Carrier.State.ST.Strict as ST
-import Control.Carrier.State.Strict as Strict
+import Control.Effect.State
 import Control.Effect.Lens
 import Control.Lens (ASetter, Getting, Lens', set, (^.))
-
-(&~) :: s -> ST.StateC s a -> s
-(&~) = ST.execState
-
-infixl 1 &~
-
-
-zoom :: Has (State s) sig m => Lens' s a -> Strict.StateC a m () -> m ()
-zoom lens action = lens <~> (`Strict.execState` action)
-
-infixr 2 `zoom`
-
-
-zoomEach :: (Has (State s) sig m, Traversable t) => Lens' s (t a) -> Strict.StateC a m () -> m ()
-zoomEach lens action = lens <~> traverse (`Strict.execState` action)
-
-infixr 2 `zoomEach`
 
 
 -- | Compose a getter onto the input of a Kleisli arrow and run it on the 'State'.
