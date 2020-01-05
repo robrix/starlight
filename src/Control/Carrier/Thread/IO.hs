05563a59d9534d20ae1a6959233547eff98339e9
Rob Rix <rob.rix@me.com>
2020-01-05 14:37:13 -0500

    Define an Algebra instance for ThreadC.

--- src/Control/Carrier/Thread/IO.hs
+++ src/Control/Carrier/Thread/IO.hs
@@ -1,4 +1,9 @@
+{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE UndecidableInstances #-}
 module Control.Carrier.Thread.IO
 ( runThread
 , ThreadC(ThreadC)
@@ -6,8 +11,19 @@ module Control.Carrier.Thread.IO
 , module Control.Effect.Thread
 ) where
 
-import Control.Effect.Thread
-import Control.Monad.IO.Class
+import           Control.Algebra
+import qualified Control.Concurrent as CC
+import           Control.Effect.Lift
+import           Control.Effect.Thread
+import           Control.Monad (void)
+import           Control.Monad.IO.Class
 
 newtype ThreadC m a = ThreadC { runThread :: m a }
   deriving (Applicative, Functor, Monad, MonadIO)
+
+instance Has (Lift IO) sig m => Algebra (Thread :+: sig) (ThreadC m) where
+  alg = \case
+    -- NB: this discards state changes in the other thread
+    L (Fork m k) -> liftWith (\ ctx run -> (<$ ctx) <$> CC.forkIO (void (run (m <$ ctx)))) >> k
+    L (Yield  k) -> sendM CC.yield >> k
+    R other      -> ThreadC (send (handleCoercible other))
