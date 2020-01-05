73af1cfb964882ead0745c41f28f36bcf9da0ab9
Rob Rix <rob.rix@me.com>
2020-01-05 01:57:03 -0500

    Re-export the Check effect.

--- src/GL/Carrier/Check/Identity.hs
+++ src/GL/Carrier/Check/Identity.hs
@@ -3,9 +3,12 @@ module GL.Carrier.Check.Identity
 ( -- * Check carrier
   runCheck
 , CheckC(CheckC)
+  -- * Check effect
+, module GL.Effect.Check
 ) where
 
 import Control.Monad.IO.Class
+import GL.Effect.Check
 
 newtype CheckC m a = CheckC { runCheck :: m a }
   deriving (Applicative, Functor, Monad, MonadIO)
