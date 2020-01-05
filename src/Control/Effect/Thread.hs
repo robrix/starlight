3ec42329a202d517be77aa5d15c2c9e93dd1dc92
Rob Rix <rob.rix@me.com>
2020-01-05 14:22:20 -0500

    Define a yield operation.

--- src/Control/Effect/Thread.hs
+++ src/Control/Effect/Thread.hs
@@ -1,9 +1,11 @@
 {-# LANGUAGE DeriveFunctor #-}
 {-# LANGUAGE ExistentialQuantification #-}
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE StandaloneDeriving #-}
 module Control.Effect.Thread
 ( -- * Thread effect
   fork
+, yield
 , Thread(..)
 ) where
 
@@ -12,13 +14,21 @@ import Control.Algebra
 fork :: Has Thread sig m => m () -> m ()
 fork m = send (Fork m (pure ()))
 
+yield :: Has Thread sig m => m ()
+yield = send (Yield (pure ()))
+
 data Thread m k
   = forall a . Fork (m a) (m k)
+  | Yield (m k)
 
 deriving instance Functor m => Functor (Thread m)
 
 instance HFunctor Thread where
-  hmap f (Fork m k) = Fork (f m) (f k)
+  hmap f = \case
+    Fork m k -> Fork (f m) (f k)
+    Yield  k -> Yield      (f k)
 
 instance Effect Thread where
-  thread ctx hdl (Fork m k) = Fork (hdl (m <$ ctx)) (hdl (k <$ ctx))
+  thread ctx hdl = \case
+    Fork m k -> Fork (hdl (m <$ ctx)) (hdl (k <$ ctx))
+    Yield  k -> Yield                 (hdl (k <$ ctx))
