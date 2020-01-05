e7c4a3b387fb3e3da1a0f3bcf43b968fdf259c7e
Rob Rix <rob.rix@me.com>
2020-01-05 01:30:43 -0500

    Derive some instances for CheckC.

--- src/GL/Carrier/Check/IO.hs
+++ src/GL/Carrier/Check/IO.hs
@@ -1,7 +1,11 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module GL.Carrier.Check.IO
 ( -- * Check carrier
   runCheck
 , CheckC(CheckC)
 ) where
 
+import Control.Monad.IO.Class
+
 newtype CheckC m a = CheckC { runCheck :: m a }
+  deriving (Applicative, Functor, Monad, MonadIO)
