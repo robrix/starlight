fe1ecbe1058a2eb68e62c392c88050c65ff97ad1
Rob Rix <rob.rix@me.com>
2020-01-05 14:29:50 -0500

    Derive some instances for ThreadC.

--- src/Control/Carrier/Thread/IO.hs
+++ src/Control/Carrier/Thread/IO.hs
@@ -1,5 +1,9 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module Control.Carrier.Thread.IO
 ( ThreadC(..)
 ) where
 
+import Control.Monad.IO.Class
+
 newtype ThreadC m a = ThreadC (m a)
+  deriving (Applicative, Functor, Monad, MonadIO)
