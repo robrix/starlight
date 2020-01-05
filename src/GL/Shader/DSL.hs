8f652446afa6a2158211410b69723512be169df0
Rob Rix <rob.rix@me.com>
2020-01-05 02:25:16 -0500

    Mark all the field functions as inlinable.

--- src/GL/Shader/DSL.hs
+++ src/GL/Shader/DSL.hs
@@ -709,25 +709,32 @@ class Vars t where
   makeVars :: (forall a . GLSLType a => Field Maybe a -> v a) -> t v
   default makeVars :: (Generic (t v), GMakeVars t v (Rep (t v))) => (forall a . GLSLType a => Field Maybe a -> v a) -> t v
   makeVars f = to (run (evalFresh 0 (S.evalState (Offset 0) (gmakeVars @t f))))
+  {-# INLINABLE makeVars #-}
 
   traverseVars :: Applicative m => (forall a . GLSLType a => Field v a -> m (v' a)) -> t v -> m (t v')
   default traverseVars :: forall v v' m . (Generic (t v), Generic (t v'), GTraverseVars t v v' (Rep (t v)) (Rep (t v')), Applicative m) => (forall a . GLSLType a => Field v a -> m (v' a)) -> t v -> m (t v')
   traverseVars f = fmap to . run . evalFresh 0 . S.evalState (Offset 0) . gtraverseVars @t f . from
+  {-# INLINABLE traverseVars #-}
 
 foldVars :: (Vars t, Monoid b) => (forall a . GLSLType a => Field v a -> b) -> t v -> b
 foldVars f t = getConst $ traverseVars (Const . f) t
+{-# INLINABLE foldVars #-}
 
 foldVarsM :: (Vars t, Monoid b, Applicative m) => (forall a . GLSLType a => Field v a -> m b) -> t v -> m b
 foldVarsM f t = getAp $ foldVars (Ap . f) t
+{-# INLINABLE foldVarsM #-}
 
 mapVars :: Vars t => (forall a . GLSLType a => Field v a -> v' a) -> t v -> t v'
 mapVars f t = runIdentity $ traverseVars (Identity . f) t
+{-# INLINABLE mapVars #-}
 
 forVars :: (Vars t, Applicative m) => t v -> (forall a . GLSLType a => Field v a -> m (v' a)) -> m (t v')
 forVars t f = traverseVars f t
+{-# INLINABLE forVars #-}
 
 defaultVars :: Vars t => t Maybe
 defaultVars = makeVars value
+{-# INLINABLE defaultVars #-}
 
 
 class GMakeVars t v f where
@@ -735,15 +742,19 @@ class GMakeVars t v f where
 
 instance GMakeVars t v f => GMakeVars t v (M1 D d f) where
   gmakeVars f = M1 <$> gmakeVars f
+  {-# INLINABLE gmakeVars #-}
 
 instance GMakeVars t v f => GMakeVars t v (M1 C c f) where
   gmakeVars f = M1 <$> gmakeVars f
+  {-# INLINABLE gmakeVars #-}
 
 instance GMakeVars t v U1 where
   gmakeVars _ = pure U1
+  {-# INLINABLE gmakeVars #-}
 
 instance (GMakeVars t v fl, GMakeVars t v fr) => GMakeVars t v (fl :*: fr) where
   gmakeVars f = (:*:) <$> gmakeVars f <*> gmakeVars f
+  {-# INLINABLE gmakeVars #-}
 
 instance (Storable a, GMakeVar t a v f, Selector s) => GMakeVars t v (M1 S s f) where
   gmakeVars f = do
@@ -751,12 +762,14 @@ instance (Storable a, GMakeVar t a v f, Selector s) => GMakeVars t v (M1 S s f) 
     o <- S.get
     S.put (o <> Offset (sizeOf @a undefined))
     pure (fix $ \ x -> M1 (gmakeVar f (Field (selName x) i o Nothing)))
+  {-# INLINABLE gmakeVars #-}
 
 class GMakeVar t a v f | f -> a v where
   gmakeVar :: (forall a . GLSLType a => Field Maybe a -> v a) -> (forall a . Field Maybe a) -> f (t v)
 
 instance GLSLType a => GMakeVar t a v (K1 R (v a)) where
   gmakeVar f s = K1 (f s)
+  {-# INLINABLE gmakeVar #-}
 
 
 class GTraverseVars t v1 v2 f1 f2 where
@@ -764,15 +777,19 @@ class GTraverseVars t v1 v2 f1 f2 where
 
 instance GTraverseVars t v1 v2 f1 f2 => GTraverseVars t v1 v2 (M1 D d f1) (M1 D d f2) where
   gtraverseVars f a = fmap M1 <$> gtraverseVars @t @v1 @v2 @f1 @f2 f (unM1 a)
+  {-# INLINABLE gtraverseVars #-}
 
 instance GTraverseVars t v1 v2 f1 f2 => GTraverseVars t v1 v2 (M1 C c f1) (M1 C c f2) where
   gtraverseVars f a = fmap M1 <$> gtraverseVars @t @v1 @v2 @f1 @f2 f (unM1 a)
+  {-# INLINABLE gtraverseVars #-}
 
 instance GTraverseVars t v1 v2 U1 U1 where
   gtraverseVars _ _ = pure (pure U1)
+  {-# INLINABLE gtraverseVars #-}
 
 instance (GTraverseVars t v1 v2 f1l f2l, GTraverseVars t v1 v2 f1r f2r) => GTraverseVars t v1 v2 (f1l :*: f1r) (f2l :*: f2r) where
   gtraverseVars f (l :*: r) = liftA2 (:*:) <$> gtraverseVars @t @v1 @v2 @f1l @f2l f l <*> gtraverseVars @t @v1 @v2 @f1r @f2r f r
+  {-# INLINABLE gtraverseVars #-}
 
 instance (Storable a, GTraverseVar t a v1 v2 f1 f2, Selector s) => GTraverseVars t v1 v2 (M1 S s f1) (M1 S s f2) where
   gtraverseVars f m = do
@@ -780,9 +797,11 @@ instance (Storable a, GTraverseVar t a v1 v2 f1 f2, Selector s) => GTraverseVars
     o <- S.get
     S.put (o <> Offset (sizeOf @a undefined))
     pure (M1 <$> gtraverseVar f (Field (selName m) i o) (unM1 m))
+  {-# INLINABLE gtraverseVars #-}
 
 class GTraverseVar t a v1 v2 f1 f2 | f1 -> a v1, f2 -> a v2 where
   gtraverseVar :: Applicative f => (forall a . GLSLType a => Field v1 a -> f (v2 a)) -> (forall a . v1 a -> Field v1 a) -> f1 (t v1) -> f (f2 (t v2))
 
 instance GLSLType a => GTraverseVar t a v1 v2 (K1 R (v1 a)) (K1 R (v2 a)) where
   gtraverseVar f s = fmap K1 . f . s . unK1
+  {-# INLINABLE gtraverseVar #-}
