82bd647344976efc2592ae77473683fe227fd6a4
Rob Rix <rob.rix@me.com>
2020-01-05 13:07:52 -0500

    Express armour as an interval.

--- src/Starlight/Ship.hs
+++ src/Starlight/Ship.hs
@@ -8,18 +8,20 @@ module Starlight.Ship
 ) where
 
 import Control.Lens (Lens')
+import Data.Functor.Identity
+import Data.Functor.Interval
 import Data.Generics.Product.Fields
 import GHC.Generics (Generic)
 import UI.Colour
 
 data Ship = Ship
   { colour :: Colour Float
-  , armour :: Float
+  , armour :: Interval Identity Float
   }
   deriving (Generic, Show)
 
 colour_ :: Lens' Ship (Colour Float)
 colour_ = field @"colour"
 
-armour_ :: Lens' Ship Float
+armour_ :: Lens' Ship (Interval Identity Float)
 armour_ = field @"armour"
