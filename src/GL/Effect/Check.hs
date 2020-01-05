a85e1cfb9fdde35f13ceb5cf3896843c06087d0d
Rob Rix <rob.rix@me.com>
2020-01-05 01:25:35 -0500

    Derive a Generic1 instance for Check.

--- src/GL/Effect/Check.hs
+++ src/GL/Effect/Check.hs
@@ -1,11 +1,12 @@
-{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE DeriveFunctor, DeriveGeneric #-}
 module GL.Effect.Check
 ( -- * Check effect
   Check(..)
 ) where
 
+import GHC.Generics (Generic1)
 import GHC.Stack
 
 data Check m k
   = Check (Maybe (String, SrcLoc)) (m k)
-  deriving (Functor)
+  deriving (Functor, Generic1)
