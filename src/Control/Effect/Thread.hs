845edbd407a6cf217d22fc70de487e3bc4d491ec
Rob Rix <rob.rix@me.com>
2020-01-05 14:18:48 -0500

    Define an HFunctor instance for Thread.

--- src/Control/Effect/Thread.hs
+++ src/Control/Effect/Thread.hs
@@ -6,7 +6,12 @@ module Control.Effect.Thread
   Thread(..)
 ) where
 
+import Control.Algebra
+
 data Thread m k
   = forall a . Fork (m a) (a -> m k)
 
 deriving instance Functor m => Functor (Thread m)
+
+instance HFunctor Thread where
+  hmap f (Fork m k) = Fork (f m) (f . k)
