79f4bb917f3a09670b157d798437c8a59b0daa17
Rob Rix <rob.rix@me.com>
2020-01-05 14:30:16 -0500

    Re-export the Thread effect.

--- src/Control/Carrier/Thread/IO.hs
+++ src/Control/Carrier/Thread/IO.hs
@@ -1,9 +1,12 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module Control.Carrier.Thread.IO
 ( ThreadC(..)
+  -- * Thread effect
+, module Control.Effect.Thread
 ) where
 
 import Control.Monad.IO.Class
+import Control.Effect.Thread
 
 newtype ThreadC m a = ThreadC (m a)
   deriving (Applicative, Functor, Monad, MonadIO)
