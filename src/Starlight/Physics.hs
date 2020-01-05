8397676112073215241436d068580b6acadf64b2
Rob Rix <rob.rix@me.com>
2020-01-05 16:16:13 -0500

    Don’t trace on hits.

--- src/Starlight/Physics.hs
+++ src/Starlight/Physics.hs
@@ -15,7 +15,6 @@ import Control.Applicative ((<|>))
 import Control.Carrier.State.Strict
 import Control.Effect.Lens
 import Control.Effect.Reader
-import Control.Effect.Trace
 import Control.Lens hiding (modifying, use, view, views, (%=), (*=), (+=), (.=))
 import Control.Monad (foldM, guard)
 import Data.Foldable (foldl', for_, traverse_)
@@ -51,14 +50,13 @@ gravity (Delta (Seconds dt)) a = do
 
 
 -- FIXME: do something smarter than ray-sphere intersection.
-hit :: (Has (Reader (System StateVectors)) sig m, Has Trace sig m) => Delta Seconds Float -> Character -> m Character
+hit :: Has (Reader (System StateVectors)) sig m => Delta Seconds Float -> Character -> m Character
 hit (Delta (Seconds dt)) c = do
   scale <- views (System.scale_ @StateVectors) (1/)
   beams <- view (beams_ @StateVectors)
   foldM (go scale) c beams where
   go scale char@Character{ actor = Actor{ position = c }, ship = Ship{ scale = r } } Beam{ angle = theta, position = o }
     | intersects (P (c^._xy)) (r * scale) (P (o^._xy)) (cartesian2 theta 1) = do
-      trace "hit"
       pure $ char & ship_.armour_.min_.coerced -~ damage
     | otherwise                                                             = pure char
   damage = 100 * dt
