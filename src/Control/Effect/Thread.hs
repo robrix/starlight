a617da02277ed79bf00f1744e7cfa080c90e96ea
Rob Rix <rob.rix@me.com>
2020-01-05 14:17:55 -0500

    Derive a Functor instance for Thread.

--- src/Control/Effect/Thread.hs
+++ src/Control/Effect/Thread.hs
@@ -1,4 +1,6 @@
+{-# LANGUAGE DeriveFunctor #-}
 {-# LANGUAGE ExistentialQuantification #-}
+{-# LANGUAGE StandaloneDeriving #-}
 module Control.Effect.Thread
 ( -- * Thread effect
   Thread(..)
@@ -6,3 +8,5 @@ module Control.Effect.Thread
 
 data Thread m k
   = forall a . Fork (m a) (a -> m k)
+
+deriving instance Functor m => Functor (Thread m)
