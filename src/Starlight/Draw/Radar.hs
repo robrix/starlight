1408cc0b30954c3e61437625fe9601c61686e483
Rob Rix <rob.rix@me.com>
2020-01-06 14:03:18 -0500

    Compute radar blips in the shader.

--- src/Starlight/Draw/Radar.hs
+++ src/Starlight/Draw/Radar.hs
@@ -45,7 +45,6 @@ import qualified Starlight.Ship as S
 import           Starlight.System
 import           Starlight.View
 import qualified UI.Drawable as UI
-import           Unit.Angle (Radians(..))
 import           Unit.Length
 
 drawRadar
@@ -59,12 +58,13 @@ drawRadar
   => m ()
 drawRadar = measure "radar" . UI.using getDrawable $ do
   system@System{ scale, bodies } <- ask @(System B.StateVectors)
-  Character{ actor = Actor{ position = here }, target } <- view (player_ @B.StateVectors)
+  target <- view (player_ @B.StateVectors .target_)
+  here   <- view (player_ @B.StateVectors .actor_.position_)
   npcs <- view (npcs_ @B.StateVectors)
   vs <- ask
 
-  let shipVertices = verticesForShips here npcs
-      bodyVertices = verticesForBodies here scale bodies
+  let shipVertices = verticesForShips npcs
+      bodyVertices = verticesForBodies scale bodies
       vertices = shipVertices <> bodyVertices
 
   measure "realloc/copy" $ do
@@ -75,6 +75,7 @@ drawRadar = measure "radar" . UI.using getDrawable $ do
   let radius = 100
       matrix = scaleToView vs
   matrix_ ?= matrix !*! scaled (V3 radius radius 1)
+  here_   ?= here^._xy
 
   -- FIXME: skip blips for extremely distant objects
   -- FIXME: blips should shadow more distant blips
@@ -103,37 +104,25 @@ runRadar = UI.loadingDrawable Drawable shader []
 newtype Drawable = Drawable { getDrawable :: UI.Drawable U V O }
 
 
-makeVertex :: Point V3 Float -> Float -> Colour Float -> V Identity
-makeVertex (P there) r colour = V{ angle = Identity angle, sweep = Identity sweep, colour = Identity colour } where
-  there' = there ^. _xy
-  angle = angleOf there'
-  d = norm there
-  direction = normalize there'
-  edge = perp direction ^* r + direction ^* d
-  sweep = max minSweep (abs (wrap (Interval (-pi) pi) (angleOf edge - angle)))
-  minSweep = 0.0133 -- at radius'=150, makes approx. 4px blips
-
-
-verticesForBodies :: Foldable t => Point V3 Float -> Float -> t B.StateVectors -> [V Identity]
-verticesForBodies here scale vs =
+verticesForBodies :: Foldable t => Float -> t B.StateVectors -> [V Identity]
+verticesForBodies scale vs =
   [ vertex
   | B.StateVectors{ body = B.Body{ radius = Metres r, colour }, actor = Actor{ position = there } } <- toList vs
-  , let vertex = makeVertex (there ^-^ here) (r * scale) colour
+  , let vertex = V{ there = Identity (there^._xy), r = Identity (r * scale), colour = Identity colour }
   ]
 
-verticesForShips :: Foldable t => Point V3 Float -> t Character -> [V Identity]
-verticesForShips here cs =
+verticesForShips :: Foldable t => t Character -> [V Identity]
+verticesForShips cs =
   [ vertex
   | Character{ actor = Actor{ position = there }, ship = S.Ship { colour, scale } } <- toList cs
-  , let vertex = makeVertex (there ^-^ here) scale colour
+  , let vertex = V{ there = Identity (there^._xy), r = Identity scale, colour = Identity colour }
   ]
 
 
 shader :: Shader U V O
 shader = program $ \ u
-  ->  vertex (\ V{ angle, sweep, colour } IF{ colour2 = out } -> do
-    angle <- let' "angle" (D.coerce angle + float gl_InstanceID * D.coerce sweep)
-    pos   <- let' "pos"   (vec2 (cos angle) (sin angle))
+  ->  vertex (\ V{ there, colour } IF{ colour2 = out } -> do
+    pos   <- let' "pos"   ((there - here u) D.^* (1/D.norm (there - here u)))
     gl_PointSize .= 3
     out .= colour
     gl_Position .= ext4 (ext3 ((matrix u !* ext3 pos 1) D.^. D._xy) 0) 1)
@@ -141,13 +130,19 @@ shader = program $ \ u
   >>> fragment (\ IF{ colour2 } O{ fragColour } -> fragColour .= colour2)
 
 
-newtype U v = U { matrix :: v (M33 Float) }
+data U v = U
+  { matrix :: v (M33 Float)
+  , here   :: v (V2 Float)
+  }
   deriving (Generic)
 
 instance Vars U
 
 matrix_ :: Lens' (U v) (v (M33 Float))
 matrix_ = field @"matrix"
+
+here_ :: Lens' (U v) (v (V2 Float))
+here_ = field @"here"
 
 
 newtype IF v = IF { colour2 :: v (Colour Float) }
@@ -157,8 +152,8 @@ instance Vars IF
 
 
 data V v = V
-  { angle  :: v (Radians Float)
-  , sweep  :: v (Radians Float)
+  { there  :: v (V2 Float) -- location of object
+  , r      :: v Float      -- radius of object
   , colour :: v (Colour Float)
   }
   deriving (Generic)
