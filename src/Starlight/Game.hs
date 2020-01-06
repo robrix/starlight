94beb73c60c635ce3e5501ba8dafc82866cca2af
Rob Rix <rob.rix@me.com>
2020-01-05 19:16:09 -0500

    Beef up the bad ship a bit.

--- src/Starlight/Game.hs
+++ src/Starlight/Game.hs
@@ -93,7 +93,7 @@ game = do
               }
             , target  = Just (C Player)
             , actions = mempty
-            , ship    = Ship{ colour = red, armour = Interval 50 100, scale = 30 }
+            , ship    = Ship{ colour = red, armour = Interval 500 500, scale = 30 }
             }
           , Character
             { actor   = Actor
