9992d9984f34cead9074bda42ec451f8b59e7afa
Rob Rix <rob.rix@me.com>
2020-01-06 17:24:11 -0500

    Move Stmt into its own module.

--- src/GL/Shader/Stmt.hs
+++ src/GL/Shader/Stmt.hs
@@ -1,2 +1,159 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
 module GL.Shader.Stmt
-() where
+( Stmt
+, let'
+, var
+, discard
+, iff
+, switch
+, break
+, while
+, (.=)
+, (+=)
+, (*=)
+, (*!=)
+, renderStmt
+, renderTypeOf
+) where
+
+import           Control.Monad (ap, liftM, (<=<))
+import           Data.Functor.Const
+import           Data.Text.Prettyprint.Doc hiding (dot)
+import           GL.Shader (Type(..))
+import           GL.Shader.Expr
+import qualified GL.Uniform as GL
+import           Prelude hiding (break)
+
+data Stmt (k :: Type) a where
+  Pure :: a -> Stmt k a
+  Let :: GL.Uniform b => String -> Expr k b -> (Const String b -> Stmt k a) -> Stmt k a
+  Discard :: Stmt 'Fragment a -> Stmt 'Fragment a
+  If :: Expr k Bool -> Stmt k () -> Stmt k () -> Stmt k a -> Stmt k a
+  Switch :: Expr k Int -> [(Maybe Int, Stmt k ())] -> Stmt k a -> Stmt k a
+  Break :: Stmt k a -> Stmt k a
+  While :: Expr k Bool -> Stmt k () -> Stmt k a -> Stmt k a
+  (:.=) :: Ref k b -> Expr k b -> Stmt k a -> Stmt k a
+  (:+=) :: Ref k b -> Expr k b -> Stmt k a -> Stmt k a
+  (:*=) :: Ref k b -> Expr k b -> Stmt k a -> Stmt k a
+  (:*!=) :: Ref k (v b) -> Expr k (v (v b)) -> Stmt k a -> Stmt k a
+  Stmt :: Pretty b => b -> (b -> Stmt k a) -> Stmt k a
+
+infixr 4 :.=
+infixr 4 :+=
+infixr 4 :*=
+infixr 4 :*!=
+
+instance Functor (Stmt k) where
+  fmap = liftM
+
+instance Applicative (Stmt k) where
+  pure = Pure
+  (<*>) = ap
+
+instance Monad (Stmt k) where
+  Pure a       >>= f = f a
+  Let n v    k >>= f = Let n v (f <=< k)
+  Discard    k >>= f = Discard (k >>= f)
+  If c t e   k >>= f = If c t e (k >>= f)
+  Switch s c k >>= f = Switch s c (k >>= f)
+  Break      k >>= f = Break (k >>= f)
+  While c t  k >>= f = While c t (k >>= f)
+  (:.=) r v  k >>= f = (r :.= v) (k >>= f)
+  (:+=) r v  k >>= f = (r :+= v) (k >>= f)
+  (:*=) r v  k >>= f = (r :*= v) (k >>= f)
+  (:*!=) r v k >>= f = (r :*!= v) (k >>= f)
+  Stmt a     k >>= f = Stmt a (f <=< k)
+
+
+let' :: GL.Uniform a => String -> Expr k a -> Stmt k (Expr k a)
+let' n v = Let n v (pure . Var . getConst)
+
+var :: GL.Uniform a => String -> Expr k a -> Stmt k (Ref k a)
+var n v = Let n v (pure . Ref . getConst)
+
+
+
+discard :: Stmt 'Fragment ()
+discard = Discard (pure ())
+
+
+iff :: Expr k Bool -> Stmt k () -> Stmt k () -> Stmt k ()
+iff c t e = If c t e (pure ())
+
+switch :: Expr k Int -> [(Maybe Int, Stmt k ())] -> Stmt k ()
+switch s cs = Switch s cs (pure ())
+
+break :: Stmt k ()
+break = Break (pure ())
+
+while :: Expr k Bool -> Stmt k () -> Stmt k ()
+while c t = While c t (pure ())
+
+
+(.=) :: Ref k a -> Expr k a -> Stmt k ()
+r .= v = (r :.= v) (pure ())
+
+infixr 4 .=
+
+(+=) :: Ref k a -> Expr k a -> Stmt k ()
+r += v = (r :+= v) (pure ())
+
+infixr 4 +=
+
+(*=) :: Ref k a -> Expr k a -> Stmt k ()
+r *= v = (r :*= v) (pure ())
+
+infixr 4 *=
+
+(*!=) :: Ref k (v a) -> Expr k (v (v a)) -> Stmt k ()
+r *!= v = (r :*!= v) (pure ())
+
+infixr 4 *!=
+
+
+renderStmt :: Stmt k a -> Doc ()
+renderStmt = \case
+  Pure _ -> mempty
+  Let n v k
+    -> renderTypeOf v <+> pretty n <+> pretty '=' <+> renderExpr v <> pretty ';' <> hardline
+    <> renderStmt (k (Const n))
+  Discard k
+    -> pretty "discard" <> pretty ';' <> hardline
+    <> renderStmt k
+  If c t e k
+    -> pretty "if" <+> parens (renderExpr c) <+> braces (nest 2 (line <> renderStmt t <> line)) <+> pretty "else" <+> braces (nest 2 (line <> renderStmt e <> line)) <> hardline
+    <> renderStmt k
+  Switch s cs k
+    -> pretty "switch" <+> parens (renderExpr s) <+> braces (nest 2 (line <> vsep (map renderCase cs) <> line)) <> hardline
+    <> renderStmt k
+  Break k
+    -> pretty "break" <+> pretty ';' <> hardline
+    <> renderStmt k
+  While c t k
+    -> pretty "while" <+> parens (renderExpr c) <+> braces (nest 2 (line <> renderStmt t <> line)) <> hardline
+    <> renderStmt k
+  (:.=) r v k
+    -> renderRef r <+> pretty '=' <+> renderExpr v <> pretty ';' <> hardline
+    <> renderStmt k
+  (:+=) r v k
+    -> renderRef r <+> pretty "+=" <+> renderExpr v <> pretty ';' <> hardline
+    <> renderStmt k
+  (:*=) r v k
+    -> renderRef r <+> pretty "*=" <+> renderExpr v <> pretty ';' <> hardline
+    <> renderStmt k
+  (:*!=) r v k
+    -> renderRef r <+> pretty "*=" <+> renderExpr v <> pretty ';' <> hardline
+    <> renderStmt k
+  Stmt b k
+    -> pretty b <> pretty ';' <> hardline
+    <> renderStmt (k b)
+  where
+  renderCase (i, s) = maybe (pretty "default:" <> hardline) (\ i -> pretty "case" <+> pretty i <> pretty ':') i  <> hardline <> renderStmt s
+
+renderTypeOf :: forall a expr . GL.Uniform a => expr a -> Doc ()
+renderTypeOf _ = pretty (GL.glslType @a)
