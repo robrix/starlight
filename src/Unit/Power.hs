8d8bd1f5b3e5ea244962659bf81a9cfbfefe3db5
Rob Rix <rob.rix@me.com>
2020-01-08 00:04:02 -0500

    Define a GL.Type instance for Watts.

--- src/Unit/Power.hs
+++ src/Unit/Power.hs
@@ -1,16 +1,25 @@
 {-# LANGUAGE DeriveTraversable #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
 module Unit.Power
 ( Watts(..)
 , getWatts
 , module Unit
 ) where
 
+import Data.Proxy
 import Foreign.Storable
+import GL.Type as GL
 import Unit
 
 newtype Watts a = Watts a
   deriving (Eq, Foldable, Floating, Fractional, Functor, Num, Ord, Read, Real, RealFloat, RealFrac, Show, Storable, Traversable)
+
+instance GL.Type a => GL.Type (Watts a) where
+  glType _ = glType (Proxy @a)
+
+  glDims _ = glDims (Proxy @a)
 
 getWatts :: Watts a -> a
 getWatts (Watts a) = a
