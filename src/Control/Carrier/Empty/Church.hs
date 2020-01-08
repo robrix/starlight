c205e7a7c83d6cb842fc57f0ccff939ad4344791
Rob Rix <rob.rix@me.com>
2020-01-07 20:37:21 -0500

    Derive a Functor instance for EmptyC.

--- src/Control/Carrier/Empty/Church.hs
+++ src/Control/Carrier/Empty/Church.hs
@@ -1,6 +1,8 @@
+{-# LANGUAGE DeriveFunctor #-}
 {-# LANGUAGE RankNTypes #-}
 module Control.Carrier.Empty.Church
 ( EmptyC(EmptyC)
 ) where
 
 newtype EmptyC m a = EmptyC (forall r . (a -> m r) -> m r -> m r)
+  deriving (Functor)
