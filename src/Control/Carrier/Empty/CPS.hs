32ca3a1062fbbc9fc433c91234e090b92f8fe069
Rob Rix <rob.rix@me.com>
2020-01-07 20:14:58 -0500

    Stub in a CPS’d Maybe carrier.

--- src/Control/Carrier/Empty/CPS.hs
+++ src/Control/Carrier/Empty/CPS.hs
@@ -1,6 +1,11 @@
+{-# LANGUAGE RankNTypes #-}
 module Control.Carrier.Empty.CPS
-( -- * Empty effect
-  module Control.Effect.Empty
+( -- * Empty carrier
+  EmptyC(EmptyC)
+  -- * Empty effect
+, module Control.Effect.Empty
 ) where
 
 import Control.Effect.Empty
+
+newtype EmptyC m a = EmptyC (forall r . (a -> m (Maybe r)) -> m (Maybe r))
