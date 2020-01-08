94fa847361823692d4d86fd04a980aa5fe241e35
Rob Rix <rob.rix@me.com>
2020-01-07 20:16:52 -0500

    Define a handler for EmptyC.

--- src/Control/Carrier/Empty/CPS.hs
+++ src/Control/Carrier/Empty/CPS.hs
@@ -1,11 +1,15 @@
 {-# LANGUAGE RankNTypes #-}
 module Control.Carrier.Empty.CPS
 ( -- * Empty carrier
-  EmptyC(EmptyC)
+  runEmpty
+, EmptyC(EmptyC)
   -- * Empty effect
 , module Control.Effect.Empty
 ) where
 
 import Control.Effect.Empty
+
+runEmpty :: Applicative m => EmptyC m a -> m (Maybe a)
+runEmpty (EmptyC run) = run (pure . Just)
 
 newtype EmptyC m a = EmptyC (forall r . (a -> m (Maybe r)) -> m (Maybe r))
