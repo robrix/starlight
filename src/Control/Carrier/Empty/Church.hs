a666039574a7609ab45816057f960a00d7f4e6a2
Rob Rix <rob.rix@me.com>
2020-01-07 20:40:07 -0500

    Define Applicative & Monad instances for EmptyC.

--- src/Control/Carrier/Empty/Church.hs
+++ src/Control/Carrier/Empty/Church.hs
@@ -4,5 +4,14 @@ module Control.Carrier.Empty.Church
 ( EmptyC(EmptyC)
 ) where
 
-newtype EmptyC m a = EmptyC (forall r . (a -> m r) -> m r -> m r)
+import Control.Monad (ap)
+
+newtype EmptyC m a = EmptyC { runEmptyC :: forall r . (a -> m r) -> m r -> m r }
   deriving (Functor)
+
+instance Applicative (EmptyC m) where
+  pure a = EmptyC (\ just _ -> just a)
+  (<*>) = ap
+
+instance Monad (EmptyC m) where
+  EmptyC m >>= f = EmptyC (\ just nothing -> m (\ a -> runEmptyC (f a) just nothing) nothing)
