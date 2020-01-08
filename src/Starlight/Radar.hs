23608040311439a134ae7148599a5b089b0f7d56
Rob Rix <rob.rix@me.com>
2020-01-08 00:28:22 -0500

    Define a lens for power.

--- src/Starlight/Radar.hs
+++ src/Starlight/Radar.hs
@@ -1,7 +1,14 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE TypeApplications #-}
 module Starlight.Radar
 ( Radar(..)
+, power_
 ) where
 
+import Control.Lens
+import Data.Generics.Product.Fields
+import GHC.Generics (Generic)
 import Unit.Power
 
 -- FIXME: transmitter gain
@@ -9,4 +16,7 @@ import Unit.Power
 data Radar = Radar
   { power :: Watts Float
   }
-  deriving (Show)
+  deriving (Generic, Show)
+
+power_ :: Lens' Radar (Watts Float)
+power_ = field @"power"
