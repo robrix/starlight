c83b16075980742bfc6386b3e4d373404040b6d0
Rob Rix <rob.rix@me.com>
2020-01-07 20:24:09 -0500

    Mark everything inlinable.

--- src/Control/Carrier/Empty/CPS.hs
+++ src/Control/Carrier/Empty/CPS.hs
@@ -15,22 +15,29 @@ import Control.Monad.Trans.Class
 
 runEmpty :: Applicative m => EmptyC m a -> m (Maybe a)
 runEmpty (EmptyC run) = run (pure . Just)
+{-# INLINABLE runEmpty #-}
 
 newtype EmptyC m a = EmptyC { runEmptyC :: forall r . (a -> m (Maybe r)) -> m (Maybe r) }
   deriving (Functor)
 
 instance Applicative (EmptyC m) where
   pure a = EmptyC ($ a)
+  {-# INLINABLE pure #-}
   (<*>) = ap
+  {-# INLINABLE (<*>) #-}
 
 instance Monad (EmptyC m) where
   EmptyC m >>= f = EmptyC (\ k -> m (($ k) . runEmptyC . f))
+  {-# INLINABLE (>>=) #-}
 
 instance MonadFail m => MonadFail (EmptyC m) where
   fail = lift . fail
+  {-# INLINABLE fail #-}
 
 instance MonadIO m => MonadIO (EmptyC m) where
   liftIO = lift . liftIO
+  {-# INLINABLE liftIO #-}
 
 instance MonadTrans EmptyC where
   lift m = EmptyC (m >>=)
+  {-# INLINABLE lift #-}
