d14207f67ec369d493cb5482bfece32268a91c64
Rob Rix <rob.rix@me.com>
2020-01-08 00:03:35 -0500

    Derive a number of instances for Watts.

--- src/Unit/Power.hs
+++ src/Unit/Power.hs
@@ -1,12 +1,16 @@
+{-# LANGUAGE DeriveTraversable #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module Unit.Power
 ( Watts(..)
 , getWatts
 , module Unit
 ) where
 
+import Foreign.Storable
 import Unit
 
 newtype Watts a = Watts a
+  deriving (Eq, Foldable, Floating, Fractional, Functor, Num, Ord, Read, Real, RealFloat, RealFrac, Show, Storable, Traversable)
 
 getWatts :: Watts a -> a
 getWatts (Watts a) = a
