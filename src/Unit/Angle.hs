2e2525070ac78ae3462347e18c4a2a09a3d3f902
Rob Rix <rob.rix@me.com>
2020-01-08 21:23:24 -0500

    Define a Unit instance for Radians.

--- src/Unit/Angle.hs
+++ src/Unit/Angle.hs
@@ -15,6 +15,8 @@ import Unit
 newtype Radians a = Radians { getRadians :: a }
   deriving (Eq, Foldable, Floating, Fractional, Functor, Num, Ord, Read, Real, RealFloat, RealFrac, Show, Storable, Traversable, GL.Type, Uniform)
 
+instance Unit Radians
+
 fromDegrees :: Floating a => Degrees a -> Radians a
 fromDegrees (Degrees d) = Radians (d * pi / 180)
 
