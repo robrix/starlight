ed6436fc2a4a17ab78bb868cb27825277238f28e
Rob Rix <rob.rix@me.com>
2020-01-09 21:12:24 -0500

    Re-export the Unit module.

--- src/Unit/Force.hs
+++ src/Unit/Force.hs
@@ -2,10 +2,12 @@
 {-# LANGUAGE TypeOperators #-}
 module Unit.Force
 ( Newton
+, module Unit
 ) where
 
 import Unit.Length
 import Unit.Mass
 import Unit.Time
+import Unit
 
 type Newton = Kilo Grams :*: Metres :/: Seconds :^: 2
