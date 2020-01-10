9584f96255614f52b5266575d0ef17b67c06a580
Rob Rix <rob.rix@me.com>
2020-01-09 22:15:50 -0500

    :fire: the HasActor instance for Beam.

--- src/Starlight/Weapon/Laser.hs
+++ src/Starlight/Weapon/Laser.hs
@@ -4,9 +4,7 @@ module Starlight.Weapon.Laser
 ( Beam(..)
 ) where
 
-import Control.Lens (lens)
 import Linear.Exts
-import Starlight.Actor
 import Starlight.Identifier
 import UI.Colour
 import Unit.Angle
@@ -18,8 +16,3 @@ data Beam = Beam
   , firedBy  :: CharacterIdentifier
   }
   deriving (Show)
-
-instance HasActor Beam where
-  actor_ = lens get set where
-    get Beam{ position, angle } = Actor{ position, rotation = axisAngle (unit _z) (getRadians angle), velocity = 0 }
-    set Beam{ colour, firedBy } Actor{ position, rotation } = Beam{ position, angle = snd (toAxisAngle rotation), colour, firedBy }
