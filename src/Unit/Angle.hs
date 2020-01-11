062150f99ce381bfa97529d00d8f2be1ef0255eb
Rob Rix <rob.rix@me.com>
2020-01-11 14:29:14 -0500

    Derive Epsilon & Conjugate instances for the units.

--- src/Unit/Angle.hs
+++ src/Unit/Angle.hs
@@ -13,12 +13,14 @@ import Data.Functor.Identity
 import Foreign.Storable
 import GL.Type as GL
 import GL.Uniform
+import Linear.Conjugate
+import Linear.Epsilon
 import Linear.Metric
 import Linear.Vector
 import Unit
 
 newtype Radians a = Radians { getRadians :: a }
-  deriving (Eq, Foldable, Floating, Fractional, Functor, Num, Ord, Real, RealFloat, RealFrac, Show, Storable, Traversable, GL.Type, Uniform)
+  deriving (Conjugate, Epsilon, Eq, Foldable, Floating, Fractional, Functor, Num, Ord, Real, RealFloat, RealFrac, Show, Storable, Traversable, GL.Type, Uniform)
   deriving (Additive, Applicative, Metric, Monad) via Identity
 
 instance Unit Radians where suffix = Const ("rad"++)
@@ -28,7 +30,7 @@ fromDegrees (Degrees d) = Radians (d * pi / 180)
 
 
 newtype Degrees a = Degrees { getDegrees :: a }
-  deriving (Eq, Foldable, Floating, Fractional, Functor, Num, Ord, Real, RealFloat, RealFrac, Show, Storable, Traversable, GL.Type, Uniform)
+  deriving (Conjugate, Epsilon, Eq, Foldable, Floating, Fractional, Functor, Num, Ord, Real, RealFloat, RealFrac, Show, Storable, Traversable, GL.Type, Uniform)
   deriving (Additive, Applicative, Metric, Monad) via Identity
 
 instance Unit Degrees where suffix = Const ('°':)
--- src/Unit/Length.hs
+++ src/Unit/Length.hs
@@ -12,12 +12,14 @@ import Data.Functor.Identity
 import Foreign.Storable
 import GL.Type as GL
 import GL.Uniform
+import Linear.Conjugate
+import Linear.Epsilon
 import Linear.Metric
 import Linear.Vector
 import Unit
 
 newtype Metres a = Metres { getMetres :: a }
-  deriving (Eq, Foldable, Floating, Fractional, Functor, Num, Ord, Real, RealFloat, RealFrac, Show, Storable, Traversable, GL.Type, Uniform)
+  deriving (Conjugate, Epsilon, Eq, Foldable, Floating, Fractional, Functor, Num, Ord, Real, RealFloat, RealFrac, Show, Storable, Traversable, GL.Type, Uniform)
   deriving (Additive, Applicative, Metric, Monad) via Identity
 
 instance Unit Metres where suffix = Const ('m':)
