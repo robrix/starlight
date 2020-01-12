6714971b9fa577723ff865a5e18b9f62e10b3031
Rob Rix <rob.rix@me.com>
2020-01-12 16:54:36 -0500

    Define a runStateVar handler for StateC.

--- src/Control/Carrier/State/STM/TVar.hs
+++ src/Control/Carrier/State/STM/TVar.hs
@@ -1,12 +1,16 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module Control.Carrier.State.STM.TVar
-( StateC(..)
+( runStateVar
+, StateC(..)
 ) where
 
 import Control.Carrier.Reader
 import Control.Concurrent.STM.TVar
 import Control.Monad.IO.Class
 import Control.Monad.Trans.Class
+
+runStateVar :: TVar s -> StateC s m a -> m a
+runStateVar var (StateC m) = runReader var m
 
 newtype StateC s m a = StateC (ReaderC (TVar s) m a)
   deriving (Applicative, Functor, Monad, MonadFail, MonadIO, MonadTrans)
