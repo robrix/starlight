9e60b955bdaa62f107deb949653d5d02a42c0387
Rob Rix <rob.rix@me.com>
2020-01-12 16:51:54 -0500

    Stub in a StateC carrier.

--- src/Control/Carrier/State/STM/TVar.hs
+++ src/Control/Carrier/State/STM/TVar.hs
@@ -1,2 +1,8 @@
 module Control.Carrier.State.STM.TVar
-() where
+( StateC(..)
+) where
+
+import Control.Carrier.Reader
+import Control.Concurrent.STM.TVar
+
+newtype StateC s m a = StateC (ReaderC (TVar s) m a)
