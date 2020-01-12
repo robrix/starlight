f5354458c092ab294aa251c17cfc5a476410935f
Rob Rix <rob.rix@me.com>
2020-01-12 16:52:25 -0500

    Derive a bunch of instances for StateC.

--- src/Control/Carrier/State/STM/TVar.hs
+++ src/Control/Carrier/State/STM/TVar.hs
@@ -1,8 +1,12 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module Control.Carrier.State.STM.TVar
 ( StateC(..)
 ) where
 
 import Control.Carrier.Reader
 import Control.Concurrent.STM.TVar
+import Control.Monad.IO.Class
+import Control.Monad.Trans.Class
 
 newtype StateC s m a = StateC (ReaderC (TVar s) m a)
+  deriving (Applicative, Functor, Monad, MonadFail, MonadIO, MonadTrans)
