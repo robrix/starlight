82b3722e31979dceaeaa20d407e797d1539a0b6d
Rob Rix <rob.rix@me.com>
2020-01-12 12:00:35 -0500

    Don’t accelerate.

--- src/Starlight/Physics.hs
+++ src/Starlight/Physics.hs
@@ -123,10 +123,7 @@ runActions i c = do
     Jump -> case target of
       Just target
         | isFacing (pi/128) rotation targetAngle -> pure c
-        | otherwise                              -> foldM (go dt system) c (concat
-          [ [ Face Target ] -- FIXME: face *near* the target
-          , [ Thrust | isFacing pi rotation targetAngle ]
-          ])
+        | otherwise                              -> foldM (go dt system) c [ Face Target ] -- FIXME: face *near* the target
         where
         targetAngle = prj <$> angleTo (projected (c^.actor_)^._xy) (projected target^._xy)
       _ -> pure c
