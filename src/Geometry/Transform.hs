33c9149073d0c7f3e0d094fb8e7f059416a44001
Rob Rix <rob.rix@me.com>
2020-01-14 07:43:20 -0500

    Define an operation to construct translations.

--- src/Geometry/Transform.hs
+++ src/Geometry/Transform.hs
@@ -1,10 +1,13 @@
 module Geometry.Transform
 ( Transform(..)
+, mkTranslation
 , (>>>)
 ) where
 
 import Control.Category
+import Control.Lens ((&), (.~))
 import Linear.Matrix
+import Linear.V3
 
 newtype Transform a b = Transform { getTransform :: M44 Float }
   deriving (Show)
@@ -12,3 +15,6 @@ newtype Transform a b = Transform { getTransform :: M44 Float }
 instance Category Transform where
   id = Transform identity
   Transform a . Transform b = Transform (b !*! a)
+
+mkTranslation :: V3 Float -> Transform a b
+mkTranslation v = Transform (identity & translation .~ v)
