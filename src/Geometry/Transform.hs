6ed0c5f682e9b2f0bb5b92c33735943db899e10a
Rob Rix <rob.rix@me.com>
2020-01-14 07:52:08 -0500

    Define an operation to apply transforms to 4-vectors.

--- src/Geometry/Transform.hs
+++ src/Geometry/Transform.hs
@@ -2,14 +2,13 @@ module Geometry.Transform
 ( Transform(..)
 , mkTranslation
 , mkRotation
+, apply
 , (>>>)
 ) where
 
 import Control.Category
 import Control.Lens ((&), (.~))
-import Linear.Matrix
-import Linear.Quaternion
-import Linear.V3
+import Linear.Exts
 
 newtype Transform a b = Transform { getTransform :: M44 Float }
   deriving (Show)
@@ -23,3 +22,6 @@ mkTranslation v = Transform (identity & translation .~ v)
 
 mkRotation :: Quaternion Float -> Transform a b
 mkRotation q = Transform (identity !*! mkTransformation q 0)
+
+apply :: Transform a b -> V4 Float -> V4 Float
+apply (Transform m) v = m !* v
