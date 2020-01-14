915122ceb4ae5d9e697a22d304d2a21f7d65837f
Rob Rix <rob.rix@me.com>
2020-01-14 16:22:34 -0500

    :fire: redundant qualifiers.

--- src/GL/Uniform.hs
+++ src/GL/Uniform.hs
@@ -44,68 +44,68 @@ instance Uniform Double where
   glslType = "double"
   uniform prog loc = runLiftIO . glProgramUniform1d prog loc
 
-instance Uniform (Linear.V2 Float) where
+instance Uniform (V2 Float) where
   glslType = "vec2"
-  uniform prog loc (Linear.V2 x y) = runLiftIO $ glProgramUniform2f prog loc x y
+  uniform prog loc (V2 x y) = runLiftIO $ glProgramUniform2f prog loc x y
 
-instance Uniform (Linear.V2 Double) where
+instance Uniform (V2 Double) where
   glslType = "dvec2"
-  uniform prog loc (Linear.V2 x y) = runLiftIO $ glProgramUniform2d prog loc x y
+  uniform prog loc (V2 x y) = runLiftIO $ glProgramUniform2d prog loc x y
 
-instance Uniform (Linear.V3 Float) where
+instance Uniform (V3 Float) where
   glslType = "vec3"
-  uniform prog loc (Linear.V3 x y z) = runLiftIO $ glProgramUniform3f prog loc x y z
+  uniform prog loc (V3 x y z) = runLiftIO $ glProgramUniform3f prog loc x y z
 
-instance Uniform (Linear.V3 Double) where
+instance Uniform (V3 Double) where
   glslType = "dvec3"
-  uniform prog loc (Linear.V3 x y z) = runLiftIO $ glProgramUniform3d prog loc x y z
+  uniform prog loc (V3 x y z) = runLiftIO $ glProgramUniform3d prog loc x y z
 
-instance Uniform (Linear.V4 Float) where
+instance Uniform (V4 Float) where
   glslType = "vec4"
-  uniform prog loc (Linear.V4 x y z w) = runLiftIO $ glProgramUniform4f prog loc x y z w
+  uniform prog loc (V4 x y z w) = runLiftIO $ glProgramUniform4f prog loc x y z w
 
-instance Uniform (Linear.V4 Double) where
+instance Uniform (V4 Double) where
   glslType = "dvec4"
-  uniform prog loc (Linear.V4 x y z w) = runLiftIO $ glProgramUniform4d prog loc x y z w
+  uniform prog loc (V4 x y z w) = runLiftIO $ glProgramUniform4d prog loc x y z w
 
-instance Uniform (Linear.M22 Float) where
+instance Uniform (M22 Float) where
   glslType = "mat2"
-  uniform prog loc matrix = A.with (Linear.transpose matrix) (runLiftIO . glProgramUniformMatrix2fv prog loc 1 GL_FALSE . castPtr)
+  uniform prog loc matrix = A.with (transpose matrix) (runLiftIO . glProgramUniformMatrix2fv prog loc 1 GL_FALSE . castPtr)
 
-instance Uniform (Linear.M22 Double) where
+instance Uniform (M22 Double) where
   glslType = "dmat2"
-  uniform prog loc matrix = A.with (Linear.transpose matrix) (runLiftIO . glProgramUniformMatrix2dv prog loc 1 GL_FALSE . castPtr)
+  uniform prog loc matrix = A.with (transpose matrix) (runLiftIO . glProgramUniformMatrix2dv prog loc 1 GL_FALSE . castPtr)
 
-instance Uniform (Linear.M23 Float) where
+instance Uniform (M23 Float) where
   glslType = "mat2x3"
-  uniform prog loc matrix = A.with (Linear.transpose matrix) (runLiftIO . glProgramUniformMatrix2x3fv prog loc 1 GL_FALSE . castPtr)
+  uniform prog loc matrix = A.with (transpose matrix) (runLiftIO . glProgramUniformMatrix2x3fv prog loc 1 GL_FALSE . castPtr)
 
-instance Uniform (Linear.M23 Double) where
+instance Uniform (M23 Double) where
   glslType = "dmat2x3"
-  uniform prog loc matrix = A.with (Linear.transpose matrix) (runLiftIO . glProgramUniformMatrix2x3dv prog loc 1 GL_FALSE . castPtr)
+  uniform prog loc matrix = A.with (transpose matrix) (runLiftIO . glProgramUniformMatrix2x3dv prog loc 1 GL_FALSE . castPtr)
 
-instance Uniform (Linear.M33 Float) where
+instance Uniform (M33 Float) where
   glslType = "mat3"
-  uniform prog loc matrix = A.with (Linear.transpose matrix) (runLiftIO . glProgramUniformMatrix3fv prog loc 1 GL_FALSE . castPtr)
+  uniform prog loc matrix = A.with (transpose matrix) (runLiftIO . glProgramUniformMatrix3fv prog loc 1 GL_FALSE . castPtr)
 
-instance Uniform (Linear.M33 Double) where
+instance Uniform (M33 Double) where
   glslType = "dmat3"
-  uniform prog loc matrix = A.with (Linear.transpose matrix) (runLiftIO . glProgramUniformMatrix3dv prog loc 1 GL_FALSE . castPtr)
+  uniform prog loc matrix = A.with (transpose matrix) (runLiftIO . glProgramUniformMatrix3dv prog loc 1 GL_FALSE . castPtr)
 
-instance Uniform (Linear.M34 Float) where
+instance Uniform (M34 Float) where
   glslType = "mat3x4"
-  uniform prog loc matrix = A.with (Linear.transpose matrix) (runLiftIO . glProgramUniformMatrix3x4fv prog loc 1 GL_FALSE . castPtr)
+  uniform prog loc matrix = A.with (transpose matrix) (runLiftIO . glProgramUniformMatrix3x4fv prog loc 1 GL_FALSE . castPtr)
 
-instance Uniform (Linear.M34 Double) where
+instance Uniform (M34 Double) where
   glslType = "dmat3x4"
-  uniform prog loc matrix = A.with (Linear.transpose matrix) (runLiftIO . glProgramUniformMatrix3x4dv prog loc 1 GL_FALSE . castPtr)
+  uniform prog loc matrix = A.with (transpose matrix) (runLiftIO . glProgramUniformMatrix3x4dv prog loc 1 GL_FALSE . castPtr)
 
-instance Uniform (Linear.M44 Float) where
+instance Uniform (M44 Float) where
   glslType = "mat4"
-  uniform prog loc matrix = A.with (Linear.transpose matrix) (runLiftIO . glProgramUniformMatrix4fv prog loc 1 GL_FALSE . castPtr)
+  uniform prog loc matrix = A.with (transpose matrix) (runLiftIO . glProgramUniformMatrix4fv prog loc 1 GL_FALSE . castPtr)
 
-instance Uniform (Linear.M44 Double) where
+instance Uniform (M44 Double) where
   glslType = "dmat4"
-  uniform prog loc matrix = A.with (Linear.transpose matrix) (runLiftIO . glProgramUniformMatrix4dv prog loc 1 GL_FALSE . castPtr)
+  uniform prog loc matrix = A.with (transpose matrix) (runLiftIO . glProgramUniformMatrix4dv prog loc 1 GL_FALSE . castPtr)
 
-deriving instance Uniform (f a) => Uniform (Linear.Point f a)
+deriving instance Uniform (f a) => Uniform (Point f a)
