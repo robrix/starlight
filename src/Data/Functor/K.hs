69459fcadbfc9a9f0382a13db23209279dc59857
Rob Rix <rob.rix@me.com>
2020-01-16 23:16:49 -0500

    Derive a lot of instances for K.

--- src/Data/Functor/K.hs
+++ src/Data/Functor/K.hs
@@ -1,6 +1,17 @@
+{-# LANGUAGE DeriveTraversable #-}
+{-# LANGUAGE DerivingVia #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 -- | I got sick of writing 'Const' out in full.
 module Data.Functor.K
 ( K(..)
 ) where
 
+import Data.Functor.Const
+import Foreign.Storable
+import GL.Type as GL
+import GL.Uniform
+import Linear
+
 newtype K a b = K { getK :: a }
+  deriving (Conjugate, Epsilon, Enum, Eq, Foldable, Floating, Fractional, Functor, Integral, Num, Ord, Real, RealFloat, RealFrac, Scalar, Show, Storable, Traversable, GL.Type, Uniform)
+  deriving (Applicative) via Const a
