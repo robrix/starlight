e299f7593e27cce4a7ecf7b2cc05ca97b81c0c2b
Rob Rix <rob.rix@me.com>
2020-01-16 22:13:14 -0500

    Derive a lot of instances for I.

--- src/Data/Functor/I.hs
+++ src/Data/Functor/I.hs
@@ -1,5 +1,16 @@
+{-# LANGUAGE DeriveTraversable #-}
+{-# LANGUAGE DerivingVia #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module Data.Functor.I
 ( I(..)
 ) where
 
+import Data.Functor.Identity
+import Linear
+import Foreign.Storable
+import GL.Type as GL
+import GL.Uniform
+
 newtype I a = I { getI :: a }
+ deriving (Conjugate, Epsilon, Enum, Eq, Foldable, Floating, Fractional, Functor, Integral, Num, Ord, Real, RealFloat, RealFrac, Scalar, Show, Storable, Traversable, GL.Type, Uniform)
+  deriving (Additive, Applicative, Metric, Monad) via Identity
