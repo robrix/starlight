0146df112f42c84603847eb026f2fdb9327acb09
Rob Rix <rob.rix@me.com>
2020-01-16 21:25:04 -0500

    Try to encode everything via a single Simplify type family.

--- src/Unit/Algebra.hs
+++ src/Unit/Algebra.hs
@@ -4,8 +4,8 @@
 {-# LANGUAGE DerivingVia #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE FunctionalDependencies #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE PatternSynonyms #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeApplications #-}
@@ -14,13 +14,12 @@
 {-# LANGUAGE UndecidableInstances #-}
 module Unit.Algebra
 (  -- * Algebra
-  Alg(..)
+  (.*.)
 , (./.)
   -- * Combinators
 , (:*:)(..)
--- , (:/:)(..)
-, (:/:)
-, Inv(..)
+, (:/:)(..)
+, Inv
 , I
 , pattern I
 , getI
@@ -40,67 +39,30 @@ import Unit
 
 -- * Algebra
 
-class (Unit dimu u, Unit dimv v) => Alg dimu u dimv v | u -> dimu, v -> dimv where
-  type Res u v (c :: (* -> *) -> (* -> *) -> (* -> *)) :: * -> *
-  (.*.) :: Fractional a => u a -> v a -> Res u v (:*:) a
+(.*.) :: (Unit du u, Unit dv v, Unit dw (Simplify (u :*: v)), Fractional a) => u a -> v a -> Simplify (u :*: v) a
+u .*. v = pure (prj v * prj u)
 
 infixl 7 .*.
 
-(./.) :: forall dimu u dimv v a . (Alg dimu u dimv (Inv v), Unit dimv v, Fractional a) => u a -> v a -> Res u (Inv v) (:*:) a
-u ./. v = u .*. Inv @v (1/prj v)
+(./.) :: (Unit du u, Unit dv v, Unit dw (Simplify (u :/: v)), Fractional a) => u a -> v a -> Simplify (u :/: v) a
+u ./. v = pure (prj u / prj v)
 
 infixl 7 ./.
 
-instance (AlgBy step dimu u dimv v, Step u v ~ step, Unit dimu u, Unit dimv v) => Alg dimu u dimv v where
-  type Res u v c = ResBy (Step u v) u v c
-  (.*.) = mulBy @step
-
-
-class (Unit dimu u, Unit dimv v) => AlgBy (step :: Act (* -> *)) dimu u dimv v | u -> dimu, v -> dimv where
-  type ResBy step u v (c :: (* -> *) -> (* -> *) -> (* -> *)) :: * -> *
-  mulBy :: Fractional a => u a -> v a -> ResBy step u v (:*:) a
-
--- | Prepend at the head of the chain.
-instance {-# OVERLAPPABLE #-} (Unit dimu u, Unit dimv v) => AlgBy 'Prepend dimu u dimv v where
-  type ResBy 'Prepend u v (:*:) = u :*: v
-  u `mulBy` v = Prd (prj u * prj v)
-
--- | Elimination by reciprocals at left.
-instance {-# OVERLAPPABLE #-} (Unit dimu u, Unit dimv v, Unit dimu' u', u' ~ InvOf u) => AlgBy ('Cancel v) (dimu :*: dimv) (u :*: v) dimu' u' where
-  type ResBy ('Cancel v) (u :*: v) u' (:*:) = v
-  u `mulBy` v = pure (prj u * prj v)
-
--- | Elimination by reciprocals at right.
-instance {-# OVERLAPPABLE #-} (Unit dimu u, Unit dimv v, Unit dimv' v', v' ~ InvOf v) => AlgBy ('Cancel u) (dimu :*: dimv) (u :*: v) dimv' v' where
-  type ResBy ('Cancel u) (u :*: v) v' (:*:) = u
-  u `mulBy` v = pure (prj u * prj v)
-
--- | Decompose products on the right.
-instance {-# OVERLAPPABLE #-} (Alg dimu u dimv' v', Alg dimuv' (Res u v' (:*:)) dimv v, Unit dimv' v', Unit dimuv'v (Res (Res u v' (:*:)) v (:*:))) => AlgBy 'Decompose dimu u (dimv :*: dimv') (v :*: v') where
-  type ResBy 'Decompose u (v :*: v') (:*:) = Res (Res u v' (:*:)) v (:*:)
-  u `mulBy` Prd v = pure (prj u * v)
-
--- | Walk the chain.
-instance {-# OVERLAPPABLE #-} (Alg dimu u dimv v, Unit dimu' u') => AlgBy 'Walk (dimu :*: dimu') (u :*: u') dimv v where
-  type ResBy 'Walk (u :*: u') v (:*:) = Res u v (:*:) :*: u'
-  Prd u `mulBy` v = Prd (u * prj v)
-
-
-data Act a = Prepend | Cancel a | Decompose | Walk
-
-type family Step u v where
-  Step (v     :*: u) (Inv v)   = 'Cancel u
-  Step (Inv v :*: u) v         = 'Cancel u
-  Step (u     :*: v) (Inv v)   = 'Cancel u
-  Step (u     :/: v) v         = 'Cancel u
-  Step _             (_ :*: _) = 'Decompose
-  Step (_     :*: _) _         = 'Walk
-  Step _             _         = 'Prepend
-
-type family InvOf u where
-  InvOf I       = I
-  InvOf (Inv u) = u
-  InvOf u       = Inv u
+type family Simplify u where
+  Simplify (I       :*: u) = u
+  Simplify (u       :*: I) = u
+  Simplify (u       :/: I) = u
+  Simplify (u :/: v :*: v) = u
+  Simplify (u :*: v :/: v) = u
+  Simplify (v :*: u :/: v) = u
+  Simplify (u :*: v :*: w) = Simplify (u :*: w) :*: v
+  Simplify (u :*: v :/: w) = Simplify (u :/: w) :*: v
+  Simplify (u :/: v :*: w) = Simplify (u :*: w) :/: v
+  Simplify (u :/: v :/: w) = Simplify (u :/: w) :/: v
+  Simplify (u :*: (v :*: w)) = Simplify (u :*: v :*: w)
+  Simplify (u :/: (v :*: w)) = Simplify (u :/: v :/: w)
+  Simplify u               = u
 
 
 -- * Combinators
@@ -116,27 +78,18 @@ instance (Unit dimu u, Unit dimv v) => Unit (dimu :*: dimv) (u :*: v) where
   suffix = Const (getConst (suffix @_ @u) . ('·' :) . getConst (suffix @_ @v))
 
 
-newtype Inv (u :: * -> *) a = Inv { getInv :: a }
+newtype ((u :: * -> *) :/: (v :: * -> *)) a = Per { getPer :: a }
   deriving (Conjugate, Epsilon, Enum, Eq, Foldable, Floating, Fractional, Functor, Integral, Num, Ord, Real, RealFloat, RealFrac, Scalar, Show, Storable, Traversable, GL.Type, Uniform)
   deriving (Additive, Applicative, Metric, Monad) via Identity
 
-instance Unit dimu u => Unit dimu (Inv u) where
-  prj = getInv
-  factor = Const (1/getConst (factor @_ @u))
-  suffix = Const (getConst (suffix @_ @u) . ('⁻' :) . ('¹' :))
-
-
--- newtype ((u :: * -> *) :/: (v :: * -> *)) a = Per { getPer :: a }
---   deriving (Conjugate, Epsilon, Enum, Eq, Foldable, Floating, Fractional, Functor, Integral, Num, Ord, Real, RealFloat, RealFrac, Scalar, Show, Storable, Traversable, GL.Type, Uniform)
---   deriving (Additive, Applicative, Metric, Monad) via Identity
-
-type u :/: v = u :*: Inv v
-
 infixl 7 :/:
 
--- instance (Unit dimu u, Unit dimv v) => Unit (dimu :/: dimv) (u :/: v) where
---   factor = Const (getConst (factor @_ @u) / getConst (factor @_ @v))
---   suffix = Const (getConst (suffix @_ @u) . ('/' :) . getConst (suffix @_ @v))
+instance (Unit dimu u, Unit dimv v) => Unit (dimu :/: dimv) (u :/: v) where
+  factor = Const (getConst (factor @_ @u) / getConst (factor @_ @v))
+  suffix = Const (getConst (suffix @_ @u) . ('/' :) . getConst (suffix @_ @v))
+
+
+type Inv u = I :/: u
 
 
 type I = Identity
