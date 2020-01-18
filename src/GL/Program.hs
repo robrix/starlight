f3def7cdf00bf1cd8762dcae07be69dc3940f5c6
Rob Rix <rob.rix@me.com>
2020-01-18 09:05:03 -0500

    Correct the Program/Buffer instances to not require the buffer be related.

--- src/GL/Program.hs
+++ src/GL/Program.hs
@@ -106,5 +106,5 @@ instance (Has Check sig m, Has (Lift IO) sig m, Vars u) => Algebra (State (u May
 instance Algebra sig m => HasProgram u v o (ProgramC u v o m) where
   askProgram = ProgramC ask
 
-deriving instance B.HasBuffer 'B.Array (v I)   m => B.HasBuffer 'B.Array (v I)   (ProgramC u    v    o m)
-deriving instance HasProgram u          v    o m => HasProgram u          v    o (B.BufferC ty (v I)   m)
+deriving instance B.HasBuffer ty x m => B.HasBuffer ty x (ProgramC u v o m)
+deriving instance HasProgram u v o m => HasProgram u v o (B.BufferC ty x m)
