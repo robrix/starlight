bee665e91775f434844481514cb3c5e8ebde49ec
Rob Rix <rob.rix@me.com>
2020-01-17 20:03:37 -0500

    Generalize the Actor lenses over HasActor.

--- src/Starlight/Actor.hs
+++ src/Starlight/Actor.hs
@@ -6,11 +6,6 @@
 -- | An 'Actor' has 'position', 'velocity', and 'rotation', and can be acted on by the physics simulation.
 module Starlight.Actor
 ( Actor(..)
-, position_
-, velocity_
-, rotation_
-, mass_
-, magnitude_
 , transformToActor
 , applyForce
 , HasActor(..)
@@ -38,22 +33,6 @@ data Actor = Actor
   }
   deriving (Generic, Show)
 
-position_ :: HasCallStack => Lens' Actor (V3 (Mega Metres Double))
-position_ = field @"position".asserting (none isNaN)
-
-velocity_ :: HasCallStack => Lens' Actor (V3 ((Mega Metres :/: Seconds) Double))
-velocity_ = field @"velocity".asserting (none isNaN)
-
-rotation_ :: HasCallStack => Lens' Actor (Quaternion Double)
-rotation_ = field @"rotation".asserting (none isNaN)
-
-mass_ :: HasCallStack => Lens' Actor (Kilo Grams Double)
-mass_ = field @"mass".asserting (not.isNaN)
-
-magnitude_ :: HasCallStack => Lens' Actor (Mega Metres Double)
-magnitude_ = field @"magnitude".asserting (not.isNaN)
-
-
 transformToActor :: Actor -> Transform Double (Mega Metres) (Mega Metres)
 transformToActor Actor{ position, rotation } = mkTranslation (prj <$> position) >>> mkRotation rotation
 
@@ -63,6 +42,22 @@ applyForce force dt a = a & velocity_ +~ ((.*. dt) . (./. a^.mass_) <$> force)
 
 class HasActor t where
   actor_ :: Lens' t Actor
+
+  position_ :: HasCallStack => Lens' t (V3 (Mega Metres Double))
+  position_ = actor_.field @"position".asserting (none isNaN)
+
+  velocity_ :: HasCallStack => Lens' t (V3 ((Mega Metres :/: Seconds) Double))
+  velocity_ = actor_.field @"velocity".asserting (none isNaN)
+
+  rotation_ :: HasCallStack => Lens' t (Quaternion Double)
+  rotation_ = actor_.field @"rotation".asserting (none isNaN)
+
+  mass_ :: HasCallStack => Lens' t (Kilo Grams Double)
+  mass_ = actor_.field @"mass".asserting (not.isNaN)
+
+  magnitude_ :: HasCallStack => Lens' t (Mega Metres Double)
+  magnitude_ = actor_.field @"magnitude".asserting (not.isNaN)
+
 
 instance HasActor Actor where
   actor_ = id
