a21834c086b865dbd57ce1c60a32ae0ed36430fb
Rob Rix <rob.rix@me.com>
2020-01-19 12:23:26 -0500

    Define a Unit instance for Context.Pixels.

--- src/UI/Context.hs
+++ src/UI/Context.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE DeriveTraversable #-}
 {-# LANGUAGE DerivingVia #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE TypeFamilies #-}
 module UI.Context
 ( Context
 , Pixels(..)
@@ -12,17 +13,23 @@ import           Control.Effect.Lift
 import qualified Control.Exception.Lift as E
 import           Control.Monad.IO.Class.Lift
 import           Data.Functor.I
+import           Data.Functor.K
 import           Foreign.Storable
 import           GL.Type as GL
 import           GL.Uniform
 import           Graphics.GL.Core41
 import           SDL
+import           Unit.Length
 
 type Context = GLContext
 
 newtype Pixels a = Pixels { getPixels :: a }
   deriving (Column, Conjugate, Enum, Epsilon, Eq, Foldable, Floating, Fractional, Functor, Integral, Num, Ord, Real, RealFloat, RealFrac, Row, Show, Storable, Traversable, GL.Type, Uniform)
   deriving (Additive, Applicative, Metric, Monad) via I
+
+instance Unit Pixels where
+  type Dim Pixels = Length
+  suffix = K ("px"++)
 
 runContext :: (Has (Lift IO) sig m, Has (Reader Window) sig m) => ReaderC Context m a -> m a
 runContext = E.bracket
