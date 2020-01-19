4505f753cae142fd6286819d742e7f22bc0d6fed
Rob Rix <rob.rix@me.com>
2020-01-19 14:36:46 -0500

    Apply forces by lifting the unital operations into the force vector.

--- src/Starlight/Actor.hs
+++ src/Starlight/Actor.hs
@@ -38,7 +38,7 @@ transformToActor :: Actor -> Transform Double (Mega Metres) (Mega Metres)
 transformToActor Actor{ position, rotation } = mkTranslation position >>> mkRotation rotation
 
 applyForce :: HasCallStack => V3 (Newtons Double) -> Seconds Double -> Actor -> Actor
-applyForce force dt a = a & velocity_.mapping converting +~ ((.*. dt) . (./. a^.mass_) <$> force)
+applyForce force dt a = a & velocity_.mapping converting +~ force ^/. a^.mass_ ^*. dt
 
 
 class HasActor t where
