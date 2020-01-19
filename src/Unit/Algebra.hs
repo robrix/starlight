3041cbfe24d0c559f2c3fbda5e1c194d7e0ad6b0
Rob Rix <rob.rix@me.com>
2020-01-19 06:12:39 -0500

    Division constructs exponents.

--- src/Unit/Algebra.hs
+++ src/Unit/Algebra.hs
@@ -73,6 +73,7 @@ type family Div u v where
   Div  u              (v :*: w) = Div (Div u w) v               -- u / (v * w) = (u / w) / v
   Div  u              (v :^: n) = Div (Div u v) (Exp v (n - 1)) -- u / (v * w) = (u / w) / v
   Div (u :*: v)        w        = Mul (Div u w) v               -- (u * v) / w = (u / w) * v
+  Div (u :/: v)        v        = u :/: v :^: 2                 -- (u / v) / v = u / v²
   Div (u :/: v)        w        = Div u w :/: v                 -- (u / v) / w = (u / w) / v
   Div  u               v        = u :/: v                       -- u / v       = u / v
 
