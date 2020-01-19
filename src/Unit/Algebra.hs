b3c9193c6d52e1f7d84a97f6b8735dc467a0aa84
Rob Rix <rob.rix@me.com>
2020-01-19 14:29:14 -0500

    Give fixity & precedence for .-..

--- src/Unit/Algebra.hs
+++ src/Unit/Algebra.hs
@@ -56,6 +56,8 @@ infixl 6 .+.
 (.-.) :: (Unit u, Num a) => u a -> u a -> u a
 u .-. v = pure (prj u - prj v)
 
+infixl 6 .-.
+
 (.*.) :: (Unit u, Unit v, Unit (Mul u v), Num a) => u a -> v a -> Mul u v a
 u .*. v = pure (prj v * prj u)
 
