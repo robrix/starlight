b3792b3baff08c5cbdb9ef3b20a6c1b1f4e0c4e5
Rob Rix <rob.rix@me.com>
2020-01-19 14:18:12 -0500

    Derive a bunch of units for :#.

--- src/Unit/Vector.hs
+++ src/Unit/Vector.hs
@@ -1,8 +1,20 @@
+{-# LANGUAGE DeriveTraversable #-}
+{-# LANGUAGE DerivingVia #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE TypeOperators #-}
 module Unit.Vector
 ( (:#)(..)
 ) where
 
+import Foreign.Storable
+import GHC.Generics ((:.:)(..))
+import GL.Type as GL
+import GL.Uniform
+import Linear.Conjugate
+import Linear.Exts
+
 newtype (v :# u) a = V { getV :: v (u a) }
+  deriving (Column, Conjugate, Epsilon, Enum, Eq, Foldable, Floating, Fractional, Functor, Integral, Num, Ord, Real, RealFloat, RealFrac, Row, Show, Storable, Traversable, GL.Type, Uniform)
+  deriving Applicative via v :.: u
 
 infixr 3 :#
