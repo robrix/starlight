8fe9e2ceb77ac20b91da4c58f7f1c9edd59bf920
Rob Rix <rob.rix@me.com>
2020-01-19 00:01:23 -0500

    mapping converting
    Mollifies Drake

--- src/Starlight/Physics.hs
+++ src/Starlight/Physics.hs
@@ -58,7 +58,7 @@ gravity a = do
     force = (a^.mass_ .*. b^.mass_ ./. r) .*. gravC
     -- FIXME: gravity seems extremely weak
     r :: (Metres :*: Metres) Double
-    r = (b^.position_.to (fmap convert)) `qdU` (a^.position_.to (fmap convert)) -- “quadrance” (square of distance between actor & body)
+    r = (b^.position_.mapping converting) `qdU` (a^.position_.mapping converting) -- “quadrance” (square of distance between actor & body)
   gravC :: (Metres :*: Metres :*: Metres :/: Kilo Grams :/: Seconds :/: Seconds) Double
   gravC = 6.67430e-11
 
