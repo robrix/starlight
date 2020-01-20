9ed452019283ebb9a19295baa075647489e453c9
Rob Rix <rob.rix@me.com>
2020-01-19 19:54:40 -0500

    Fix facing backwards.

--- src/Starlight/Physics.hs
+++ src/Starlight/Physics.hs
@@ -99,7 +99,7 @@ runActions i c = do
       where
       desiredAngle Actor{ velocity, position } t = case dir of
         Forwards  -> Just (angleOf (velocity^._xy))
-        Backwards -> t^?_Just.velocity_.to (subtract velocity).to (angleOf.(^._xy)) <|> Just (-angleOf (velocity^._xy))
+        Backwards -> t^?_Just.velocity_.to (subtract velocity).to (angleOf.(^._xy)) <|> Just (angleOf (-velocity^._xy))
         Target    -> t^?_Just.to projected.to (`L.direction` position).to (angleOf.(^._xy))
 
     Turn t -> pure $! c & rotation_ *~ axisAngle (unit _z) ((case t of
