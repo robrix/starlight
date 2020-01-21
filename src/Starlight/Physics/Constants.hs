f30b6835c8ada2451147bb70409de03fc7a53c82
Rob Rix <rob.rix@me.com>
2020-01-20 22:11:16 -0500

    Set the system units to Giga Metres.

--- src/Starlight/Physics/Constants.hs
+++ src/Starlight/Physics/Constants.hs
@@ -10,7 +10,7 @@ import Unit.Length
 import Unit.Mass
 import Unit.Time
 
-type Distance = Mega Metres
+type Distance = Giga Metres
 
 gravC :: (Metres :^: 3 :/: Kilo Grams :/: Seconds :^: 2) Double
 gravC = 6.67430e-11
