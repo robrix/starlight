63189eb514a9d84175c56acc2b7de322fef65722
Rob Rix <rob.rix@me.com>
2020-01-20 21:55:14 -0500

    Define Distance as a synonym for Mega Metres.

--- src/Starlight/Physics/Constants.hs
+++ src/Starlight/Physics/Constants.hs
@@ -1,13 +1,16 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE TypeOperators #-}
 module Starlight.Physics.Constants
-( gravC
+( Distance
+, gravC
 ) where
 
 import Unit.Algebra
 import Unit.Length
 import Unit.Mass
 import Unit.Time
+
+type Distance = Mega Metres
 
 gravC :: (Metres :^: 3 :/: Kilo Grams :/: Seconds :^: 2) Double
 gravC = 6.67430e-11
