7a5c73f8103c5a8109bf11050a288b883cb37cda
Rob Rix <rob.rix@me.com>
2020-01-21 20:05:46 -0500

    Define gravitation in Starlight.Physics.Constants.

--- src/Starlight/Physics/Constants.hs
+++ src/Starlight/Physics/Constants.hs
@@ -3,9 +3,12 @@
 module Starlight.Physics.Constants
 ( Distance
 , gravC
+, gravitation
 ) where
 
+import Linear.Metric
 import Unit.Algebra
+import Unit.Force
 import Unit.Length
 import Unit.Mass
 import Unit.Time
@@ -14,3 +17,7 @@ type Distance = Giga Metres
 
 gravC :: (Metres :^: 3 :/: Kilo Grams :/: Seconds :^: 2) Double
 gravC = 6.67430e-11
+
+gravitation :: Metric v => Kilo Grams Double -> Kilo Grams Double -> v (Metres Double) -> v (Metres Double) -> Newtons Double
+gravitation m1 m2 p1 p2 = (m1 .*. m2 ./. (p1 `qdU` p2)) .*. gravC
+-- FIXME: gravity seems extremely weak, measures as a factor of approximately 46
