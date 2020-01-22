543f07cb627f41f4600948e2fb52dd096364f47b
Rob Rix <rob.rix@me.com>
2020-01-21 20:54:23 -0500

    Use the extracted definition of the radar range equation.

--- src/Starlight/System.hs
+++ src/Starlight/System.hs
@@ -93,19 +93,13 @@ neighbourhoodOf c sys@System{ bodies, players, npcs } = sys
   -- FIXME: dimensional analysis
   visible i a = case i of
     B (Star _) -> True
-    _          -> received > threshold
+    _          -> received .>. threshold
     where
-    received :: Pico Watts Double
-    received = (c^.ship_.radar_.power_.converting @_ @(Pico Watts) .*. gain .*. convert @_ @(Distance :^: 2) aperture .*. crossSection .*. patternPropagationFactor ** 4) ./. (I ((4 * pi) ** 2) .*. r .*. r)
-    crossSection :: (Distance :^: 2) Double
+    received = radarRange (c^.ship_.radar_.power_.converting) gain (convert aperture) (convert crossSection) patternPropagationFactor (convert <$> a^.position_) (convert <$> c^.position_)
     crossSection = a^.magnitude_ .*. a^.magnitude_
-    r :: (Distance :^: 2) Double
-    r = (a^.position_) `qdU` (c^.position_)
   aperture :: (Mega Metres :^: 2) Double
   aperture = 10
-  gain :: I Double
   gain = 1
-  patternPropagationFactor :: I Double
   patternPropagationFactor = 1
   threshold :: Pico Watts Double
   threshold = 1
