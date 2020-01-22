2ec564b13aa075b3434c3de85187855bd803d281
Rob Rix <rob.rix@me.com>
2020-01-21 20:50:48 -0500

    Extract the radar range equation.

--- src/Starlight/Physics/Constants.hs
+++ src/Starlight/Physics/Constants.hs
@@ -4,6 +4,7 @@ module Starlight.Physics.Constants
 ( Distance
 , gravC
 , gravitation
+, radarRange
 ) where
 
 import Linear.Metric
@@ -11,6 +12,7 @@ import Unit.Algebra
 import Unit.Force
 import Unit.Length
 import Unit.Mass
+import Unit.Power
 import Unit.Time
 
 type Distance = Giga Metres
@@ -21,3 +23,10 @@ gravC = 6.67430e-11
 gravitation :: (Metric v, Fractional a) => Kilo Grams a -> Kilo Grams a -> v (Metres a) -> v (Metres a) -> Newtons a
 gravitation m1 m2 p1 p2 = (m1 .*. m2 ./. (p1 `qdU` p2)) .*. gravC
 -- FIXME: gravity seems extremely weak, measures as a factor of approximately 46
+
+radarRange :: Metric v => Watts Double -> I Double -> (Metres :^: 2) Double -> (Metres :^: 2) Double -> I Double -> v (Metres Double) -> v (Metres Double) -> Watts Double
+radarRange pt gain aperture crossSection patternPropagationFactor p1 p2
+  =   (pt .*. gain .*. aperture .*. crossSection .*. patternPropagationFactor ** 4)
+  ./. (I ((4 * pi) ** 2) .*. r .*. r)
+  where
+  r = p1 `qdU` p2
