eb386cda50f3f7ff84bb74e824f73ffe29b853b3
Rob Rix <rob.rix@me.com>
2020-01-22 20:38:22 -0500

    Sort imports.

--- src/Unit/Force.hs
+++ src/Unit/Force.hs
@@ -6,11 +6,11 @@ module Unit.Force
 , module Unit.Multiple
 ) where
 
-import Unit.Length
-import Unit.Mass
-import Unit.Time
 import Unit
 import Unit.Algebra
+import Unit.Length
+import Unit.Mass
 import Unit.Multiple
+import Unit.Time
 
 type Newtons = Kilo Grams :*: Metres :/: Seconds :^: 2
