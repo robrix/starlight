372d8eea8b185b973db57afa91625a50d238f49f
Rob Rix <rob.rix@me.com>
2020-01-22 20:43:47 -0500

    Define a unit of areal number density.

--- src/Unit/Density/Areal.hs
+++ src/Unit/Density/Areal.hs
@@ -1,2 +1,11 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeOperators #-}
 module Unit.Density.Areal
-() where
+( Density
+) where
+
+import Unit.Algebra
+import Unit.Count
+import Unit.Length
+
+type Density sym = Count sym :/: Metres :^: 2
