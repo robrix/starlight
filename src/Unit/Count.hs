a666c92f35e7ee20697a4ee078f11742ddb6e066
Rob Rix <rob.rix@me.com>
2020-01-22 20:03:08 -0500

    Derive many instances for Count.

--- src/Unit/Count.hs
+++ src/Unit/Count.hs
@@ -1,5 +1,16 @@
+{-# LANGUAGE DeriveTraversable #-}
+{-# LANGUAGE DerivingVia #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module Unit.Count
 ( Count(..)
 ) where
 
+import Data.Functor.I
+import Foreign.Storable
+import GL.Type as GL
+import GL.Uniform
+import Linear
+
 newtype Count a = Count { getCount :: a }
+  deriving (Column, Conjugate, Epsilon, Enum, Eq, Foldable, Floating, Fractional, Functor, Integral, Num, Ord, Real, RealFloat, RealFrac, Row, Show, Storable, Traversable, GL.Type, Uniform)
+  deriving (Additive, Applicative, Metric, Monad) via I
