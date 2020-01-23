f5c8b752d456012fe76969997526e37489786f83
Rob Rix <rob.rix@me.com>
2020-01-22 20:03:50 -0500

    Define a Unit instance for Count.

--- src/Unit/Count.hs
+++ src/Unit/Count.hs
@@ -1,16 +1,23 @@
 {-# LANGUAGE DeriveTraversable #-}
 {-# LANGUAGE DerivingVia #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE TypeFamilies #-}
 module Unit.Count
 ( Count(..)
 ) where
 
 import Data.Functor.I
+import Data.Functor.K
 import Foreign.Storable
 import GL.Type as GL
 import GL.Uniform
 import Linear
+import Unit
 
 newtype Count a = Count { getCount :: a }
   deriving (Column, Conjugate, Epsilon, Enum, Eq, Foldable, Floating, Fractional, Functor, Integral, Num, Ord, Real, RealFloat, RealFrac, Row, Show, Storable, Traversable, GL.Type, Uniform)
   deriving (Additive, Applicative, Metric, Monad) via I
+
+instance Unit Count where
+  type Dim Count = Count
+  suffix = K (""++)
