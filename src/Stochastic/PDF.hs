7435501eee042adfc9bc3b814d2fc991b92e0864
Rob Rix <rob.rix@me.com>
2020-01-23 22:30:29 -0500

    Derive a Semigroup instance for PDF.

--- src/Stochastic/PDF.hs
+++ src/Stochastic/PDF.hs
@@ -1,7 +1,11 @@
+{-# LANGUAGE DerivingVia #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module Stochastic.PDF
 ( PDF(..)
 ) where
 
+import Data.Semigroup (Sum(..))
+
 newtype PDF a b = PDF { runPDF :: a -> b }
   deriving (Functor)
+  deriving Semigroup via (a -> Sum b)
