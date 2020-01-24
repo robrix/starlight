e96300b5af328e483072ddda56a3d13e1913e638
Rob Rix <rob.rix@me.com>
2020-01-23 22:34:10 -0500

    Define a Num instance for PDF.

--- src/Stochastic/PDF.hs
+++ src/Stochastic/PDF.hs
@@ -4,8 +4,18 @@ module Stochastic.PDF
 ( PDF(..)
 ) where
 
+import Control.Applicative (liftA2)
 import Data.Semigroup (Sum(..))
 
 newtype PDF a b = PDF { runPDF :: a -> b }
   deriving (Applicative, Functor, Monad)
   deriving (Monoid, Semigroup) via (a -> Sum b)
+
+instance Num b => Num (PDF a b) where
+  (+) = liftA2 (+)
+  (-) = liftA2 (-)
+  (*) = liftA2 (*)
+  negate = fmap negate
+  abs    = fmap abs
+  signum = fmap signum
+  fromInteger = pure . fromInteger
