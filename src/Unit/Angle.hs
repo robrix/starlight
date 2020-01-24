2c2ad1735784559bb7d09d164615a7d3b953c6fc
Rob Rix <rob.rix@me.com>
2020-01-24 17:27:51 -0500

    Spacing.

--- src/Unit/Angle.hs
+++ src/Unit/Angle.hs
@@ -28,6 +28,7 @@ import Unit.Multiple
 
 type Angle = I
 
+
 newtype Radians a = Radians { getRadians :: a }
   deriving (Column, Conjugate, Epsilon, Enum, Eq, Foldable, Floating, Fractional, Functor, Integral, Num, Ord, Random, Real, RealFloat, RealFrac, Row, Show, Storable, Traversable, GL.Type, Uniform)
   deriving (Additive, Applicative, Metric, Monad) via I
