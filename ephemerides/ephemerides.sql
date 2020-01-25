53d71cc3b334799bb82729985d60652a5230f627
Rob Rix <rob.rix@me.com>
2020-01-25 11:58:08 -0500

    Add orbital elements.

--- ephemerides/ephemerides.sql
+++ ephemerides/ephemerides.sql
@@ -6,4 +6,11 @@ create table bodies
   , tilt real not null -- degrees
   , rotationalPeriod real not null -- days
   , colour integer not null -- 32-bit packed
+  , eccentricity real not null
+  , semimajor real not null -- km
+  , longitudeOfAscendingNode real not null -- degrees
+  , inclination real not null -- degrees
+  , argumentOfPerifocus real not null -- degrees
+  , orbitalPeriod real not null -- seconds
+  , timeOfPeriapsis real not null -- seconds
   );
