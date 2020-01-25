985bfd7232c182c76721c8a9be15e9fe5664f0dd
Rob Rix <rob.rix@me.com>
2020-01-25 11:51:30 -0500

    Add a sql file creating a table for ephemerides.

---
+++ ephemerides/ephemerides.sql
@@ -0,0 +1,9 @@
+create table bodies
+  ( id integer primary key
+  , name text not null
+  , radius real not null -- km
+  , mass real not null -- kg
+  , tilt real not null -- degrees
+  , period real not null -- days
+  , colour integer not null -- 32-bit packed
+  );
