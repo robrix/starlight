cc859a883616a1bafa6d17c50b4b1f469153dfc0
Rob Rix <rob.rix@me.com>
2020-01-25 16:14:56 -0500

    Define an Iso for packing & unpacing colours to Word32.

--- src/UI/Colour.hs
+++ src/UI/Colour.hs
@@ -13,11 +13,14 @@ module UI.Colour
 , _a
 , opaque
 , setClearColour
+, packed
 , HasColour(..)
 ) where
 
 import Control.Lens
 import Control.Monad.IO.Class.Lift
+import Data.Bits
+import Data.Word
 import Graphics.GL.Core41
 import Linear.V4
 
@@ -63,6 +66,13 @@ opaque = set _a 1
 setClearColour :: (Real a, Has (Lift IO) sig m) => Colour a -> m ()
 setClearColour v = runLiftIO $ glClearColor r g b a where
   V4 r g b a = realToFrac <$> v
+
+
+packed :: (RealFrac a, Fractional b) => Iso (Colour a) (Colour b) Word32 Word32
+packed = iso pack unpack
+  where
+  pack c = let V4 r g b a = round . (* 255) <$> c in shiftL r 24 .|. shiftL g 16 .|. shiftL b 8 .|. a :: Word32
+  unpack i = (/ 255) . fromIntegral <$> V4 (0xff .&. shiftR i 24) (0xff .&. shiftR i 16) (0xff .&. shiftR i 8) (0xff .&. i)
 
 
 class HasColour t where
