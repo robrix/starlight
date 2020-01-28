0e65506b2bf8433bbb79a5a8cf5bde9da7e72522
Rob Rix <rob.rix@me.com>
2020-01-28 02:01:50 -0500

    Define pure using point.

--- src/Data/Functor/Interval.hs
+++ src/Data/Functor/Interval.hs
@@ -43,7 +43,7 @@ data Interval f a = Interval
   deriving (Eq, Foldable, Functor, Generic, Ord, Show, Traversable)
 
 instance Applicative f => Applicative (Interval f) where
-  pure a = Interval (pure a) (pure a)
+  pure = point . pure
   Interval f1 f2 <*> Interval a1 a2 = Interval (f1 <*> a1) (f2 <*> a2)
 
 instance Monad f => Monad (Interval f) where
