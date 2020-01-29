9dd47664067c54b9f1e33d7e0f8880e13abefa98
Rob Rix <rob.rix@me.com>
2020-01-28 23:29:36 -0500

    Sample under a functor.

--- src/Stochastic/Histogram.hs
+++ src/Stochastic/Histogram.hs
@@ -13,15 +13,15 @@ import Data.Functor.I
 import Data.Functor.Interval
 import System.Console.Terminal.Size as Size
 
-histogram :: RealFrac a => Interval I a -> Int -> [a] -> [Int]
+histogram :: RealFrac a => Interval I a -> Int -> [I a] -> [I Int]
 histogram interval n samples
   | [] <- samples = []
   | otherwise     = foldl' bucket (replicate n 0) samples
   where
-  which sample = floor (getI (toUnit interval (I sample)) * fromIntegral n)
+  which sample = floor (toUnit interval sample * fromIntegral n)
   bucket accum sample = accum & ix (which sample) +~ 1
 
-sparkify :: [Int] -> String
+sparkify :: [I Int] -> String
 sparkify bins
   | null bins = ""
   | otherwise = spark <$> bins
@@ -34,5 +34,5 @@ sparkify bins
 printHistogram :: (RealFrac a, Has (Lift IO) sig m) => Interval I a -> Int -> m a -> m ()
 printHistogram interval n m = do
   s <- maybe 80 Size.width <$> sendM Size.size
-  samples <- replicateM n m
+  samples <- replicateM n (fmap I m)
   sendM (putStrLn (sparkify (histogram interval s samples)))
