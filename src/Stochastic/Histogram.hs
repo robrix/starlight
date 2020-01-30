4b404926ddfc51197c9b0737bdf349dbb8e093f6
Rob Rix <rob.rix@me.com>
2020-01-29 23:15:10 -0500

    Define two-dimensional histograms.

--- src/Stochastic/Histogram.hs
+++ src/Stochastic/Histogram.hs
@@ -1,16 +1,19 @@
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE TypeFamilies #-}
 module Stochastic.Histogram
 ( histogram
+, histogram2
 , sparkify
 , printHistogram
 ) where
 
 import Control.Effect.Lift
-import Control.Lens (ix, (&), (+~))
+import Control.Lens (ix, (&), (+~), (^.))
 import Control.Monad (replicateM)
 import Data.Foldable (foldl')
 import Data.Functor.I
 import Data.Functor.Interval
+import Linear.V2
 import System.Console.Terminal.Size as Size
 
 histogram :: RealFrac a => Interval I a -> I Int -> [I a] -> [Int]
@@ -18,6 +21,13 @@ histogram interval n = foldl' bucket (replicate (getI n) 0)
   where
   which sample = floor (toUnit interval sample * fromIntegral n)
   bucket accum sample = accum & ix (which sample) +~ 1
+
+histogram2 :: RealFrac a => Interval V2 a -> V2 Int -> [V2 a] -> [[Int]]
+histogram2 interval n = foldl' bucket (replicate (n ^. _y) (replicate (n ^. _x) 0))
+  where
+  which sample = fmap floor . (*) <$> toUnit interval sample <*> (fromIntegral <$> n)
+  bucket accum sample = accum & ix x.ix y +~ 1 where
+    V2 x y = which sample
 
 sparkify :: [Int] -> String
 sparkify bins
