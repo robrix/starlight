a77f30de597b72c556d10176f4d728bbf335ee72
Rob Rix <rob.rix@me.com>
2020-01-29 23:18:03 -0500

    Generate two-dimensional histograms as vectors.

--- src/Stochastic/Histogram.hs
+++ src/Stochastic/Histogram.hs
@@ -13,18 +13,19 @@ import           Control.Monad (replicateM)
 import           Data.Foldable (foldl')
 import           Data.Functor.I
 import           Data.Functor.Interval
-import qualified Data.Vector.Unboxed as V
+import qualified Data.Vector.Unboxed as U
+import qualified Data.Vector as V
 import           Linear.V2
 import           System.Console.Terminal.Size as Size
 
-histogram :: RealFrac a => Interval I a -> I Int -> [I a] -> V.Vector Int
-histogram interval n = foldl' bucket (V.replicate (getI n) 0)
+histogram :: RealFrac a => Interval I a -> I Int -> [I a] -> U.Vector Int
+histogram interval n = foldl' bucket (U.replicate (getI n) 0)
   where
   which sample = floor (toUnit interval sample * fromIntegral n)
   bucket accum sample = accum & ix (which sample) +~ 1
 
-histogram2 :: RealFrac a => Interval V2 a -> V2 Int -> [V2 a] -> [[Int]]
-histogram2 interval n = foldl' bucket (replicate (n ^. _y) (replicate (n ^. _x) 0))
+histogram2 :: RealFrac a => Interval V2 a -> V2 Int -> [V2 a] -> V.Vector (U.Vector Int)
+histogram2 interval n = foldl' bucket (V.replicate (n ^. _y) (U.replicate (n ^. _x) 0))
   where
   which sample = fmap floor . (*) <$> toUnit interval sample <*> (fromIntegral <$> n)
   bucket accum sample = accum & ix x.ix y +~ 1 where
@@ -44,4 +45,4 @@ printHistogram :: (RealFrac a, Has (Lift IO) sig m) => Interval I a -> Int -> m 
 printHistogram interval n m = do
   s <- maybe 80 Size.width <$> sendM Size.size
   samples <- replicateM n (fmap I m)
-  sendM (putStrLn (sparkify (V.toList (histogram interval s samples))))
+  sendM (putStrLn (sparkify (U.toList (histogram interval s samples))))
