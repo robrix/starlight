01fd51d23c4bc15762f9f835ca752b05c4300ece
Rob Rix <rob.rix@me.com>
2020-01-31 10:29:05 -0500

    Rename m to bounds.

--- src/Stochastic/Sample/Slice.hs
+++ src/Stochastic/Sample/Slice.hs
@@ -32,19 +32,19 @@ sample
   -> Interval f a
   -> PDF (f a) (g b)
   -> m (f a)
-sample w m (PDF pdf) = runReader w $ do
+sample w bounds (PDF pdf) = runReader w $ do
   x <- get
   y <- uniformI (Interval 0 (pdf x))
   u <- uniformI =<< ask
   let l = x - u
-  step y l (local (intersection m) (shrink x y))
+  step y l (local (intersection bounds) (shrink x y))
   where
   step y l m' = do
     size' <- asks size
     local (const (Interval l (l + size'))) $ fix (\ go -> ask >>= \ i -> if
-      | or ((>) <$> min' i <*> min' m), y < pdf (min' i) -> local (min_ -~ size') go
-      | or ((<) <$> max' i <*> max' m), y < pdf (max' i) -> local (max_ +~ size') go
-      | otherwise                                        -> m')
+      | or ((>) <$> min' i <*> min' bounds), y < pdf (min' i) -> local (min_ -~ size') go
+      | or ((<) <$> max' i <*> max' bounds), y < pdf (max' i) -> local (max_ +~ size') go
+      | otherwise                                             -> m')
   shrink x y = fix (\ go -> ask >>= uniformI >>= \case
     x' | y < pdf x' -> x' <$ put x'
        | otherwise  -> local (interval mn mx <*> point x <*> point x' <*>) go)
