2892bb641d385ead8ae46a005ae2ba14fef8474a
Rob Rix <rob.rix@me.com>
2020-01-31 13:19:04 -0500

    Eliminate the Reader for passing intervals around.

--- src/Stochastic/Sample/Slice.hs
+++ src/Stochastic/Sample/Slice.hs
@@ -7,7 +7,6 @@ module Stochastic.Sample.Slice
 
 import           Control.Applicative (liftA2)
 import           Control.Carrier.Random.Gen
-import           Control.Carrier.Reader
 import           Control.Carrier.State.Strict
 import           Control.Lens (over, (&))
 import           Data.Functor.Interval
@@ -32,24 +31,23 @@ sample
   -> Interval f a
   -> PDF (f a) (g b)
   -> m (f a)
-sample w bounds (PDF pdf) = runReader w $ do
+sample w bounds (PDF pdf) = do
   x <- get
   y <- uniformI (Interval (pure 0) (pdf x))
-  u <- uniformI =<< ask
-  size' <- asks size
+  u <- uniformI w
   let step i
         -- if any coordinate of the interval’s min is in-bounds…
         | or (min' i ^>^ min' bounds)
         -- … & it still lies under the curve, step the min outwards
-        , y < pdf (min' i) = step (i & over min_ (^-^ size'))
+        , y < pdf (min' i) = step (i & over min_ (^-^ size w))
         -- if any coordinate of the interval’s max is in-bounds…
         | or (max' i ^<^ max' bounds)
         -- … & it still lies under the curve, step the max outwards
-        , y < pdf (max' i) = step (i & over max_ (^+^ size'))
+        , y < pdf (max' i) = step (i & over max_ (^+^ size w))
         | otherwise        = i
-      shrink = ask >>= uniformI >>= \case
+      shrink i = uniformI i >>= \case
         x' | y < pdf x' -> x' <$ put x'
-           | otherwise  -> local (interval mn mx <*> point x <*> point x' <*>) shrink
+           | otherwise  -> shrink (interval mn mx <*> point x <*> point x' <*> i)
       mn x x' i = if x' < x then x' else i
       mx x x' i = if x' < x then i  else x'
       (^-^) = liftA2 (-)
@@ -57,4 +55,4 @@ sample w bounds (PDF pdf) = runReader w $ do
       (^<^) = liftA2 (<)
       (^>^) = liftA2 (>)
 
-  local (intersection bounds . step . (point (x ^-^ u) +)) shrink
+  shrink (intersection bounds (step (point (x ^-^ u) + w)))
