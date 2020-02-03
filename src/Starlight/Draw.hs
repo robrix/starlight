b33a9db4896c79a6161dcb193942a039f9d10eed
Rob Rix <rob.rix@me.com>
2020-02-03 14:01:41 -0500

    Define onScreen without pattern matching.

--- src/Starlight/Draw.hs
+++ src/Starlight/Draw.hs
@@ -116,7 +116,7 @@ draw = measure "draw" . runLiftIO $ do
   measure "laser" $ for_ beams Laser.draw
 
   let hypotenuse = norm (fromIntegral <$> size) * 0.5
-      onScreen StateVectors{ body = Body{ radius }, actor = Actor{ position = pos } } = lengthToWindowPixels v .*. (distance pos position - convert radius) < hypotenuse
+      onScreen a = lengthToWindowPixels v .*. (distance (a^.position_) position - convert (a^.magnitude_ * 0.5)) < hypotenuse
 
   measure "body" $ for_ (system^?bodies_.traversed.filtered onScreen) Body.draw
 
