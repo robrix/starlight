98de61231baea26dccb822fa0791f01ba546b7d1
Rob Rix <rob.rix@me.com>
2020-02-08 18:02:18 -0500

    Define a MonadTrans instance for ErrorC.

--- src/Control/Carrier/Error/IO.hs
+++ src/Control/Carrier/Error/IO.hs
@@ -11,9 +11,13 @@ import Control.Effect.Error
 import Control.Effect.Lift
 import Control.Exception.Lift
 import Control.Monad.IO.Class
+import Control.Monad.Trans.Class
 
 runError :: (Exception e, Has (Lift IO) sig m) => ErrorC e m a -> m (Either e a)
 runError (ErrorC m) = try m
 
 newtype ErrorC e m a = ErrorC (m a)
   deriving (Applicative, Functor, Monad, MonadFail, MonadIO)
+
+instance MonadTrans (ErrorC e) where
+  lift = ErrorC
