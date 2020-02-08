0ec435f5bea29bf068a96c3f1ad70de611186ff5
Rob Rix <rob.rix@me.com>
2020-02-08 18:04:41 -0500

    Define an Algebra instance for ErrorC.

--- src/Control/Carrier/Error/IO.hs
+++ src/Control/Carrier/Error/IO.hs
@@ -1,4 +1,9 @@
+{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE UndecidableInstances #-}
 module Control.Carrier.Error.IO
 ( -- * Error carrier
   runError
@@ -7,6 +12,7 @@ module Control.Carrier.Error.IO
 , module Control.Effect.Error
 ) where
 
+import Control.Algebra
 import Control.Effect.Error
 import Control.Effect.Lift
 import Control.Exception.Lift
@@ -21,3 +27,9 @@ newtype ErrorC e m a = ErrorC (m a)
 
 instance MonadTrans (ErrorC e) where
   lift = ErrorC
+
+instance (Exception e, Has (Lift IO) sig m) => Algebra (Error e :+: sig) (ErrorC e m) where
+  alg = \case
+    L (L (Throw e))     -> throwIO e
+    L (R (Catch m h k)) -> (m `catch` h) >>= k
+    R other             -> ErrorC (send (handleCoercible other))
