f2917dfa47ce10428e2059baf7ba4b9977b0c438
Rob Rix <rob.rix@me.com>
2020-02-08 18:00:10 -0500

    Define a handler for ErrorC.

--- src/Control/Carrier/Error/IO.hs
+++ src/Control/Carrier/Error/IO.hs
@@ -1,10 +1,16 @@
 module Control.Carrier.Error.IO
 ( -- * Error carrier
-  ErrorC(..)
+  runError
+, ErrorC(..)
   -- * Error effect
 , module Control.Effect.Error
 ) where
 
 import Control.Effect.Error
+import Control.Effect.Lift
+import Control.Exception.Lift
+
+runError :: (Exception e, Has (Lift IO) sig m) => ErrorC e m a -> m (Either e a)
+runError (ErrorC m) = try m
 
 newtype ErrorC e m a = ErrorC (m a)
