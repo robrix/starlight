79ce82c4d80b27de9250f0de0ab3b4ef46e15551
Rob Rix <rob.rix@me.com>
2020-02-10 12:36:57 -0500

    Define a HasColour instance for Faction.

--- src/Starlight/Faction.hs
+++ src/Starlight/Faction.hs
@@ -1,9 +1,18 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE TypeApplications #-}
 module Starlight.Faction
 ( Faction(..)
 ) where
 
+import Data.Generics.Product.Fields
+import GHC.Generics (Generic)
 import UI.Colour
 
 newtype Faction = Faction
   { colour :: Colour Float
   }
+  deriving (Generic)
+
+instance HasColour Faction where
+  colour_ = field @"colour"
