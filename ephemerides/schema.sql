eb2de041dca581cf5586c40b2195375c91c2059d
Rob Rix <rob.rix@me.com>
2020-02-12 14:34:14 -0500

    Define a schema for relationships.

--- ephemerides/schema.sql
+++ ephemerides/schema.sql
@@ -23,3 +23,9 @@ create table factions
   ( name text not null
   , colour integer not null
   );
+
+create table relationships
+  ( factionId1 integer not null
+  , factionId2 integer not null
+  , relationship real not null
+  );
