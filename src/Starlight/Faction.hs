e2291f4aa72a2ec11b9d770a54cdb29c07d3ebad
Rob Rix <rob.rix@me.com>
2020-02-12 14:08:16 -0500

    Give factions a table of relationships to other factions.

--- src/Starlight/Faction.hs
+++ src/Starlight/Faction.hs
@@ -1,17 +1,23 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DeriveGeneric #-}
 module Starlight.Faction
-( Faction(..)
+( Factions(..)
+, Faction(..)
 ) where
 
+import Bound.Scope
 import Data.Text (Text)
+import Data.Void (Void)
 import GHC.Generics (Generic)
 import UI.Colour
 
-data Faction = Faction
-  { name   :: Text
-  , colour :: Colour Float
+newtype Factions = Factions { getFactions :: Scope Int Faction Void }
+
+data Faction a = Faction
+  { name          :: Text
+  , colour        :: Colour Float
+  , relationships :: [(a, Float)]
   }
   deriving (Eq, Generic, Ord, Show)
 
-instance HasColour Faction
+instance HasColour (Faction a)
