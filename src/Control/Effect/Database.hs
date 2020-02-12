eff72daac871d0b22017aba2e30108a5777295f4
Rob Rix <rob.rix@me.com>
2020-02-12 15:50:15 -0500

    Define a Step operation.

--- src/Control/Effect/Database.hs
+++ src/Control/Effect/Database.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE DeriveFunctor #-}
 {-# LANGUAGE ExistentialQuantification #-}
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE StandaloneDeriving #-}
 module Control.Effect.Database
 ( -- * Database effect
@@ -17,11 +18,16 @@ import Data.Text (Text)
 
 data Database row m k
   = Execute Text (row -> m k)
+  | forall a . Step (row -> m a) (a -> m k)
 
 deriving instance Functor m => Functor (Database row m)
 
 instance HFunctor (Database row) where
-  hmap f (Execute cmd k) = Execute cmd (f . k)
+  hmap f = \case
+    Execute cmd k -> Execute cmd    (f . k)
+    Step    row k -> Step (f . row) (f . k)
 
 instance Effect (Database row) where
-  thread ctx hdl (Execute cmd k) = Execute cmd (hdl . (<$ ctx) . k)
+  thread ctx hdl = \case
+    Execute cmd k -> Execute cmd (hdl . (<$ ctx) . k)
+    Step    row k -> Step (hdl . (<$ ctx) . row) (hdl . fmap k)
