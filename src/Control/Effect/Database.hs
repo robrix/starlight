b04cd28691b0ac6a4fbcba9b3bcd9aa71f34f31a
Rob Rix <rob.rix@me.com>
2020-02-12 15:23:23 -0500

    Define an Execute operation.

--- src/Control/Effect/Database.hs
+++ src/Control/Effect/Database.hs
@@ -1,2 +1,14 @@
+{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE ExistentialQuantification #-}
+{-# LANGUAGE StandaloneDeriving #-}
 module Control.Effect.Database
-() where
+( Database(..)
+) where
+
+import Data.Text (Text)
+import Database.SQLite3 (SQLData)
+
+data Database m k
+  = forall a . Execute Text ([SQLData] -> m a) (a -> m k)
+
+deriving instance Functor m => Functor (Database m)
