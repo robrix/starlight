47224a9317b1dab09eec2716015bbd61a2f4b0f2
Rob Rix <rob.rix@me.com>
2020-02-12 15:08:41 -0500

    Define a function to run an effect vending a database.

--- src/Data/SQL.hs
+++ src/Data/SQL.hs
@@ -1,2 +1,12 @@
 module Data.SQL
-() where
+( runDatabase
+) where
+
+import Control.Carrier.Reader
+import Control.Effect.Lift
+import Control.Exception.Lift
+import Data.Text (pack)
+import Database.SQLite3
+
+runDatabase :: Has (Lift IO) sig m => String -> ReaderC Database m a -> m a
+runDatabase file m = bracket (sendM (open (pack file))) (sendM . close) (`runReader` m)
