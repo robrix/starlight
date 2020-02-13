5c2f40becc4f0040fb0f2fc9339af354bcfa6346
Rob Rix <rob.rix@me.com>
2020-02-13 00:53:32 -0500

    Define an Algebra instance for DatabaseC.

--- src/Control/Carrier/Database/SQLite.hs
+++ src/Control/Carrier/Database/SQLite.hs
@@ -1,4 +1,9 @@
+{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE UndecidableInstances #-}
 module Control.Carrier.Database.SQLite
 ( -- * Database carrier
   runDatabase
@@ -9,6 +14,7 @@ module Control.Carrier.Database.SQLite
 
 import           Control.Carrier.Reader
 import           Control.Effect.Database
+import           Control.Effect.Labelled
 import           Control.Effect.Lift
 import           Control.Exception.Lift
 import           Control.Monad.Fix
@@ -22,3 +28,18 @@ runDatabase file (DatabaseC m) = bracket (sendM (SQLite.open (pack file))) (send
 
 newtype DatabaseC m a = DatabaseC (ReaderC SQLite.Database m a)
   deriving (Applicative, Functor, Monad, MonadFail, MonadFix, MonadIO, MonadTrans)
+
+instance Has (Lift IO) sig m => Algebra (Labelled Database (Database SQLite.Statement) :+: sig) (DatabaseC m) where
+  alg = \case
+    L (Labelled (Execute cmd m k)) -> do
+      db <- DatabaseC ask
+      stmt <- sendM (SQLite.prepare db cmd)
+      a <- m stmt
+      sendM (SQLite.finalize stmt)
+      k a
+    L (Labelled (Step stmt k)) -> do
+      res <- sendM (SQLite.step stmt)
+      case res of
+        SQLite.Done -> k Nothing
+        SQLite.Row  -> sendM (SQLite.columns stmt) >>= k . Just
+    R other -> DatabaseC (send (handleCoercible other))
