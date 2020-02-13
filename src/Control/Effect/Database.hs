4d0aaaeaa12ca79249454ee7536546aff1bd0a2a
Rob Rix <rob.rix@me.com>
2020-02-13 01:02:21 -0500

    Rename the row vars to stmt.

--- src/Control/Effect/Database.hs
+++ src/Control/Effect/Database.hs
@@ -27,18 +27,18 @@ execute cmd m = sendLabelled @Database (Labelled (Execute cmd m pure))
 step :: HasLabelled Database (Database stmt) sig m => stmt -> m (Maybe [SQLData])
 step stmt = sendLabelled @Database (Labelled (Step stmt pure))
 
-data Database row m k
-  = forall a . Execute Text (row -> m a) (a -> m k)
-  | Step row (Maybe [SQLData] -> m k)
+data Database stmt m k
+  = forall a . Execute Text (stmt -> m a) (a -> m k)
+  | Step stmt (Maybe [SQLData] -> m k)
 
-deriving instance Functor m => Functor (Database row m)
+deriving instance Functor m => Functor (Database stmt m)
 
-instance HFunctor (Database row) where
+instance HFunctor (Database stmt) where
   hmap f = \case
     Execute cmd m k -> Execute cmd (f . m) (f . k)
-    Step row k -> Step row (f . k)
+    Step stmt k -> Step stmt (f . k)
 
-instance Effect (Database row) where
+instance Effect (Database stmt) where
   thread ctx hdl = \case
     Execute cmd m k -> Execute cmd (hdl . (<$ ctx) . m) (hdl . fmap k)
-    Step row k -> Step row (hdl . (<$ ctx) . k)
+    Step stmt k -> Step stmt (hdl . (<$ ctx) . k)
