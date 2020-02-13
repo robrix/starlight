e2c57b96fe6c6ef6215856f11dd7f0b5feaeaef5
Rob Rix <rob.rix@me.com>
2020-02-13 00:46:23 -0500

    Define a handler for DatabaseC.

--- src/Control/Carrier/Database/SQLite.hs
+++ src/Control/Carrier/Database/SQLite.hs
@@ -1,17 +1,24 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module Control.Carrier.Database.SQLite
 ( -- * Database carrier
-  DatabaseC(..)
+  runDatabase
+, DatabaseC(..)
   -- * Database effect
 , module Control.Effect.Database
 ) where
 
 import           Control.Carrier.Reader
 import           Control.Effect.Database
+import           Control.Effect.Lift
+import           Control.Exception.Lift
 import           Control.Monad.Fix
 import           Control.Monad.IO.Class
 import           Control.Monad.Trans.Class
+import           Data.Text (pack)
 import qualified Database.SQLite3 as SQLite
+
+runDatabase :: Has (Lift IO) sig m => FilePath -> DatabaseC m a -> m a
+runDatabase file (DatabaseC m) = bracket (sendM (SQLite.open (pack file))) (sendM . SQLite.close) (`runReader` m)
 
 newtype DatabaseC m a = DatabaseC (ReaderC SQLite.Database m a)
   deriving (Applicative, Functor, Monad, MonadFail, MonadFix, MonadIO, MonadTrans)
