5acfa2a40ea415b48f86ec1c9a99d0823128e071
Rob Rix <rob.rix@me.com>
2020-02-13 00:43:51 -0500

    Derive a bunch of instances.

--- src/Control/Carrier/Database/SQLite.hs
+++ src/Control/Carrier/Database/SQLite.hs
@@ -1,9 +1,14 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module Control.Carrier.Database.SQLite
 ( -- * Database carrier
   DatabaseC(..)
 ) where
 
 import           Control.Carrier.Reader
+import           Control.Monad.Fix
+import           Control.Monad.IO.Class
+import           Control.Monad.Trans.Class
 import qualified Database.SQLite3 as SQLite
 
 newtype DatabaseC m a = DatabaseC (ReaderC SQLite.Database m a)
+  deriving (Applicative, Functor, Monad, MonadFail, MonadFix, MonadIO, MonadTrans)
