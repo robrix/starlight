d14a446c9fbcabcfd88be22a6c5c69d2e1ea2c7e
Rob Rix <rob.rix@me.com>
2020-02-13 00:25:16 -0500

    Step takes a row and returns fields.

--- src/Control/Effect/Database.hs
+++ src/Control/Effect/Database.hs
@@ -15,19 +15,20 @@ module Control.Effect.Database
 import Control.Algebra
 import Control.Effect.Labelled
 import Data.Text (Text)
+import Database.SQLite3 (SQLData)
 
 data Database row m k
   = forall a . Execute Text (row -> m a) (a -> m k)
-  | forall a . Step (row -> m a) (a -> m k)
+  | Step row (Maybe [SQLData] -> m k)
 
 deriving instance Functor m => Functor (Database row m)
 
 instance HFunctor (Database row) where
   hmap f = \case
-    Execute cmd row k -> Execute cmd (f . row) (f . k)
-    Step        row k -> Step        (f . row) (f . k)
+    Execute cmd m k -> Execute cmd (f . m) (f . k)
+    Step row k -> Step row (f . k)
 
 instance Effect (Database row) where
   thread ctx hdl = \case
-    Execute cmd row k -> Execute cmd (hdl . (<$ ctx) . row) (hdl . fmap k)
-    Step        row k -> Step        (hdl . (<$ ctx) . row) (hdl . fmap k)
+    Execute cmd m k -> Execute cmd (hdl . (<$ ctx) . m) (hdl . fmap k)
+    Step row k -> Step row (hdl . (<$ ctx) . k)
