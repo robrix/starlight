22f70de3832a48d9bcb55789330890124a84e257
Rob Rix <rob.rix@me.com>
2020-02-13 00:43:12 -0500

    Define a Database carrier.

--- src/Control/Carrier/Database/SQLite.hs
+++ src/Control/Carrier/Database/SQLite.hs
@@ -1,2 +1,9 @@
 module Control.Carrier.Database.SQLite
-() where
+( -- * Database carrier
+  DatabaseC(..)
+) where
+
+import           Control.Carrier.Reader
+import qualified Database.SQLite3 as SQLite
+
+newtype DatabaseC m a = DatabaseC (ReaderC SQLite.Database m a)
