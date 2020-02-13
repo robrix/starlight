8136c7832a57b962b336116a8538046c2d03aec1
Rob Rix <rob.rix@me.com>
2020-02-13 00:15:05 -0500

    Execute takes an action.

--- src/Control/Effect/Database.hs
+++ src/Control/Effect/Database.hs
@@ -17,17 +17,17 @@ import Control.Effect.Labelled
 import Data.Text (Text)
 
 data Database row m k
-  = Execute Text (row -> m k)
+  = forall a . Execute Text (row -> m a) (a -> m k)
   | forall a . Step (row -> m a) (a -> m k)
 
 deriving instance Functor m => Functor (Database row m)
 
 instance HFunctor (Database row) where
   hmap f = \case
-    Execute cmd k -> Execute cmd    (f . k)
-    Step    row k -> Step (f . row) (f . k)
+    Execute cmd row k -> Execute cmd (f . row) (f . k)
+    Step        row k -> Step        (f . row) (f . k)
 
 instance Effect (Database row) where
   thread ctx hdl = \case
-    Execute cmd k -> Execute cmd (hdl . (<$ ctx) . k)
-    Step    row k -> Step (hdl . (<$ ctx) . row) (hdl . fmap k)
+    Execute cmd row k -> Execute cmd (hdl . (<$ ctx) . row) (hdl . fmap k)
+    Step        row k -> Step        (hdl . (<$ ctx) . row) (hdl . fmap k)
