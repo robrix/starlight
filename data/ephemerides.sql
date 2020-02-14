608a8bb8b22bf0b4ac859641a852cb3c9bfc1acf
Rob Rix <rob.rix@me.com>
2020-02-14 14:25:40 -0500

    Move the bodies schema to data/ephemerides.sql.

--- data/ephemerides.sql
+++ data/ephemerides.sql
@@ -1,3 +1,23 @@
+-- schema for the ephemerides table
+create table bodies
+  ( parentId integer
+  , code integer not null
+  , name text not null
+  , population integer not null
+  , radius real not null -- km
+  , mass real not null -- kg
+  , tilt real not null -- degrees
+  , rotationalPeriod real not null -- days
+  , colour integer not null -- 32-bit packed
+  , eccentricity real not null
+  , semimajor real not null -- km
+  , longitudeOfAscendingNode real not null -- degrees
+  , inclination real not null -- degrees
+  , argumentOfPerifocus real not null -- degrees
+  , orbitalPeriod real not null -- seconds
+  , timeOfPeriapsis real not null -- seconds
+  );
+
 insert into bodies values
   ( null
   , 10
