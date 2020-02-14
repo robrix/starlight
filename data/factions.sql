e9227dfdd150c9632383ba226be1a3bfc6a9b38f
Rob Rix <rob.rix@me.com>
2020-02-14 14:26:36 -0500

    Move the schema for the factions tables into the factions SQL file.

--- data/factions.sql
+++ data/factions.sql
@@ -1,3 +1,15 @@
+-- schema for factions & their relationships
+create table factions
+  ( name text not null
+  , colour integer not null
+  );
+
+create table relationships
+  ( factionId1 integer not null
+  , factionId2 integer not null
+  , relationship real not null
+  );
+
 insert into factions values
   ( "venusian"
   , 0xffff80ff
