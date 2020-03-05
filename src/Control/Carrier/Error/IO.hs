0c67187d8107f37776914fe91223f48604a9e768
Rob Rix <rob.rix@me.com>
2020-03-04 20:51:15 -0500

    Update the IO.ErrorC algebra.

--- src/Control/Carrier/Error/IO.hs
+++ src/Control/Carrier/Error/IO.hs
@@ -7,7 +7,7 @@
 module Control.Carrier.Error.IO
 ( -- * Error carrier
   runError
-, ErrorC(..)
+, ErrorC(ErrorC)
   -- * Error effect
 , module Control.Effect.Error
 ) where
@@ -22,14 +22,14 @@ import Control.Monad.Trans.Class
 runError :: (Exception e, Has (Lift IO) sig m) => ErrorC e m a -> m (Either e a)
 runError (ErrorC m) = try m
 
-newtype ErrorC e m a = ErrorC (m a)
+newtype ErrorC e m a = ErrorC { runErrorC :: m a }
   deriving (Applicative, Functor, Monad, MonadFail, MonadIO)
 
 instance MonadTrans (ErrorC e) where
   lift = ErrorC
 
 instance (Exception e, Has (Lift IO) sig m) => Algebra (Error e :+: sig) (ErrorC e m) where
-  alg = \case
+  alg ctx hdl = \case
     L (L (Throw e))     -> throwIO e
-    L (R (Catch m h k)) -> (m `catch` h) >>= k
-    R other             -> ErrorC (send (handleCoercible other))
+    L (R (Catch m h k)) -> (hdl (m <$ ctx) `catch` (hdl . (<$ ctx) . h)) >>= hdl . fmap k
+    R other             -> ErrorC (alg ctx (runErrorC . hdl) other)
