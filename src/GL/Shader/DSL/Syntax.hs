e33d847ec864d0bfda8d28c3c468941ae136cdcc
Rob Rix <rob.rix@me.com>
2020-03-18 12:29:48 -0400

    :fire: the stage parameter from expr.

--- src/GL/Shader/DSL/Syntax.hs
+++ src/GL/Shader/DSL/Syntax.hs
@@ -1,5 +1,4 @@
 {-# LANGUAGE DataKinds #-}
-{-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE QuantifiedConstraints #-}
 module GL.Shader.DSL.Syntax
 ( Expr(..)
@@ -9,64 +8,62 @@ module GL.Shader.DSL.Syntax
 ) where
 
 import Data.Coerce
-import Data.Kind (Type)
 import Geometry.Transform
-import GL.Shader (Stage(..))
 import Linear.V2
 import Linear.V3
 import Linear.V4
 
-class ( forall a b s . Coercible a b => Coercible (expr s a) (expr s b)
-      , forall a s . Num a => Num (expr s a)
-      , forall a s . Fractional a => Fractional (expr s a)
-      , forall a s . Floating a => Floating (expr s a)
+class ( forall a b . Coercible a b => Coercible (expr a) (expr b)
+      , forall a . Num a => Num (expr a)
+      , forall a . Fractional a => Fractional (expr a)
+      , forall a . Floating a => Floating (expr a)
       )
-   => Expr (expr :: Stage -> Type -> Type) where
+   => Expr expr where
   -- matrix
 
-  (>>>) :: expr s (Transform m a u v) -> expr s (Transform m a v w) -> expr s (Transform m a u w)
-  (>*) :: expr s (Transform m a u v) -> expr s (m (v a)) -> expr s (m (u a))
+  (>>>) :: expr (Transform m a u v) -> expr (Transform m a v w) -> expr (Transform m a u w)
+  (>*) :: expr (Transform m a u v) -> expr (m (v a)) -> expr (m (u a))
 
   infixr 1 >>>
   infixl 7 >*
 
-  (!*!) :: expr s (u (v a)) -> expr s (v (w a)) -> expr s (u (w a))
-  (!*) :: expr s (u (v a)) -> expr s (v a) -> expr s (u a)
+  (!*!) :: expr (u (v a)) -> expr (v (w a)) -> expr (u (w a))
+  (!*) :: expr (u (v a)) -> expr (v a) -> expr (u a)
 
   infixl 7 !*!, !*
 
   -- vector
 
-  v2 :: expr s a -> expr s a -> expr s (V2 a)
-  v3 :: expr s a -> expr s a -> expr s a -> expr s (V3 a)
-  v4 :: expr s a -> expr s a -> expr s a -> expr s a -> expr s (V4 a)
+  v2 :: expr a -> expr a -> expr (V2 a)
+  v3 :: expr a -> expr a -> expr a -> expr (V3 a)
+  v4 :: expr a -> expr a -> expr a -> expr a -> expr (V4 a)
 
-  ext3 :: expr s (V2 a) -> expr s a -> expr s (V3 a)
-  ext4 :: expr s (V3 a) -> expr s a -> expr s (V4 a)
+  ext3 :: expr (V2 a) -> expr a -> expr (V3 a)
+  ext4 :: expr (V3 a) -> expr a -> expr (V4 a)
 
-  norm :: expr s (v a) -> expr s a
+  norm :: expr (v a) -> expr a
 
-  (^*) :: expr s (v a) -> expr s a -> expr s (v a)
-  (*^) :: expr s a -> expr s (v a) -> expr s (v a)
-  (^/) :: expr s (v a) -> expr s a -> expr s (v a)
+  (^*) :: expr (v a) -> expr a -> expr (v a)
+  (*^) :: expr a -> expr (v a) -> expr (v a)
+  (^/) :: expr (v a) -> expr a -> expr (v a)
 
   infixl 7 ^*, *^, ^/
 
   -- scalar
 
-  atan2 :: expr s a -> expr s a -> expr s a
+  atan2 :: expr a -> expr a -> expr a
 
-  float :: expr s a -> expr s Float
+  float :: expr a -> expr Float
 
-  min, max :: expr s a -> expr s a -> expr s a
+  min, max :: expr a -> expr a -> expr a
 
-  (<), (>), (<=), (>=), (==) :: expr s a -> expr s a -> expr s Bool
+  (<), (>), (<=), (>=), (==) :: expr a -> expr a -> expr Bool
 
   -- general syntax
 
-  let' :: expr s a -> (expr s a -> expr s b) -> expr s b
+  let' :: expr a -> (expr a -> expr b) -> expr b
 
-  case' :: expr s a -> [(a, expr s b)] -> expr s b
+  case' :: expr a -> [(a, expr b)] -> expr b
 
 class Boolean expr where
   fromBool :: Bool -> expr Bool
