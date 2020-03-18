64569662072362acb4f7077860c3ad1391d17770
Rob Rix <rob.rix@me.com>
2020-03-17 21:38:25 -0400

    Reformat the Expr context.

--- src/GL/Shader/DSL/Syntax.hs
+++ src/GL/Shader/DSL/Syntax.hs
@@ -8,7 +8,10 @@ import Linear.V2
 import Linear.V3
 import Linear.V4
 
-class (forall a b . Coercible a b => Coercible (expr a) (expr b), forall a . Num a => Num (expr a)) => Expr expr where
+class ( forall a b . Coercible a b => Coercible (expr a) (expr b)
+      , forall a . Num a => Num (expr a)
+      )
+   => Expr expr where
   (!*!) :: expr (u (v a)) -> expr (v (w a)) -> expr (u (w a))
 
   (!*) :: expr (u (v a)) -> expr (v a) -> expr (u a)
