fd24b49100cbfe9649a5f76ba8a6f1d9291d79a0
Rob Rix <rob.rix@me.com>
2020-03-18 12:10:54 -0400

    :fire: some redundant qualifiers.

--- src/Starlight/Draw/Radar.hs
+++ src/Starlight/Draw/Radar.hs
@@ -141,11 +141,11 @@ vertex' U{ here, scale } = vertex (\ V{ there, r, colour } IG{ colour2, sweep } 
   d     <- let' "d"     (D.norm there)
   let angleOf vec = atan2' (vec D.^.D._y) (vec D.^.D._x)
   angle <- let' "angle" (angleOf (vec2 [there]))
-  radius <- let' "radius" (D.min' (scale * float d) radius)
+  radius <- let' "radius" (min' (scale * float d) radius)
   minSweep <- let' "minSweep" (minBlipSize / (2 * pi * D.coerce radius))
   iff (r `gt` d)
     (sweep .= pi/2)
-    (sweep .= (minSweep `D.max'` abs (D.coerce (asin (float (r/d))))))
+    (sweep .= (minSweep `max'` abs (D.coerce (asin (float (r/d))))))
   pos   <- let' "pos"   (vec2 [cos angle, sin angle] D.^* D.coerce radius)
   colour2 .= colour
   gl_Position .= ext4 (ext3 pos 0) 1) where
