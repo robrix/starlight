5158ae93b7fd8689c6b5451a2a8136eeb0b129c7
Rob Rix <rob.rix@me.com>
2020-03-17 21:34:53 -0400

    Require a Num instance for Exprs.

--- src/GL/Shader/DSL/Syntax.hs
+++ src/GL/Shader/DSL/Syntax.hs
@@ -5,7 +5,7 @@ module GL.Shader.DSL.Syntax
 
 import Data.Coerce
 
-class (forall a b . Coercible a b => Coercible (expr a) (expr b)) => Expr expr where
+class (forall a b . Coercible a b => Coercible (expr a) (expr b), forall a . Num a => Num (expr a)) => Expr expr where
   (!*!) :: expr (u (v a)) -> expr (v (w a)) -> expr (u (w a))
 
   (!*) :: expr (u (v a)) -> expr (v a) -> expr (u a)
