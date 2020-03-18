fed1c4aa9aebe4203831e4472d322aeb34af682c
Rob Rix <rob.rix@me.com>
2020-03-18 10:28:56 -0400

    Parameterize expressions by the stage they can occur within.

--- src/GL/Shader/DSL/Syntax.hs
+++ src/GL/Shader/DSL/Syntax.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE QuantifiedConstraints #-}
 module GL.Shader.DSL.Syntax
 ( Expr(..)
@@ -6,49 +8,51 @@ module GL.Shader.DSL.Syntax
 ) where
 
 import Data.Coerce
+import Data.Kind (Type)
+import GL.Shader (Stage(..))
 import Linear.V2
 import Linear.V3
 import Linear.V4
 
-class ( forall a b . Coercible a b => Coercible (expr a) (expr b)
-      , forall a . Num a => Num (expr a)
-      , forall a . Fractional a => Fractional (expr a)
-      , forall a . Floating a => Floating (expr a)
+class ( forall a b s . Coercible a b => Coercible (expr s a) (expr s b)
+      , forall a s . Num a => Num (expr s a)
+      , forall a s . Fractional a => Fractional (expr s a)
+      , forall a s . Floating a => Floating (expr s a)
       )
-   => Expr expr where
+   => Expr (expr :: Stage -> Type -> Type) where
   -- matrix
 
-  (!*!) :: expr (u (v a)) -> expr (v (w a)) -> expr (u (w a))
-  (!*) :: expr (u (v a)) -> expr (v a) -> expr (u a)
+  (!*!) :: expr s (u (v a)) -> expr s (v (w a)) -> expr s (u (w a))
+  (!*) :: expr s (u (v a)) -> expr s (v a) -> expr s (u a)
 
   infixl 7 !*!, !*
 
   -- vector
 
-  ext3 :: expr (V2 a) -> expr a -> expr (V3 a)
-  ext4 :: expr (V3 a) -> expr a -> expr (V4 a)
+  ext3 :: expr s (V2 a) -> expr s a -> expr s (V3 a)
+  ext4 :: expr s (V3 a) -> expr s a -> expr s (V4 a)
 
-  norm :: expr (v a) -> expr a
+  norm :: expr s (v a) -> expr s a
 
-  (^*) :: expr (v a) -> expr a -> expr (v a)
-  (*^) :: expr a -> expr (v a) -> expr (v a)
-  (^/) :: expr (v a) -> expr a -> expr (v a)
+  (^*) :: expr s (v a) -> expr s a -> expr s (v a)
+  (*^) :: expr s a -> expr s (v a) -> expr s (v a)
+  (^/) :: expr s (v a) -> expr s a -> expr s (v a)
 
   infixl 7 ^*, *^, ^/
 
   -- scalar
 
-  atan2 :: expr a -> expr a -> expr a
+  atan2 :: expr s a -> expr s a -> expr s a
 
   -- general syntax
 
-  let' :: expr a -> (expr a -> expr b) -> expr b
+  let' :: expr s a -> (expr s a -> expr s b) -> expr s b
 
   -- booleans
 
-  bool :: Bool -> expr Bool
-  iff :: expr Bool -> expr a -> expr a -> expr a
+  bool :: Bool -> expr s Bool
+  iff :: expr s Bool -> expr s a -> expr s a -> expr s a
 
-false, true :: Expr expr => expr Bool
+false, true :: Expr expr => expr s Bool
 false = bool False
 true  = bool True
