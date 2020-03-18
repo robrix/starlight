adb78f6579e9234be06a2cb71cf3a8f379237b88
Rob Rix <rob.rix@me.com>
2020-03-17 22:07:55 -0400

    Add headings for the methods.

--- src/GL/Shader/DSL/Syntax.hs
+++ src/GL/Shader/DSL/Syntax.hs
@@ -14,15 +14,23 @@ class ( forall a b . Coercible a b => Coercible (expr a) (expr b)
       , forall a . Num a => Num (expr a)
       )
    => Expr expr where
+  -- matrix
+
   (!*!) :: expr (u (v a)) -> expr (v (w a)) -> expr (u (w a))
   (!*) :: expr (u (v a)) -> expr (v a) -> expr (u a)
 
   infixl 7 !*!, !*
 
+  -- vector
+
   ext3 :: expr (V2 a) -> expr a -> expr (V3 a)
   ext4 :: expr (V3 a) -> expr a -> expr (V4 a)
 
+  -- general syntax
+
   let' :: expr a -> (expr a -> expr b) -> expr b
+
+  -- booleans
 
   bool :: Bool -> expr Bool
   iff :: expr Bool -> expr a -> expr a -> expr a
