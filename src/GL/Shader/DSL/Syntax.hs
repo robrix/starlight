98e4e644c8d62e37fac717a7c7a0cedca72b19fc
Rob Rix <rob.rix@me.com>
2020-03-24 17:53:54 -0400

    Define an operation to lift a rotation into a Transform.

--- src/GL/Shader/DSL/Syntax.hs
+++ src/GL/Shader/DSL/Syntax.hs
@@ -27,6 +27,7 @@ class Expr expr where
   m3 :: expr (V3 a) -> expr (V3 a) -> expr (V3 a) -> expr (M33 a)
   m4 :: expr (V4 a) -> expr (V4 a) -> expr (V4 a) -> expr (V4 a) -> expr (M44 a)
 
+  mkRotation :: expr (m (m (I a))) -> expr (Transform m a u v)
   (<<<) :: expr (Transform m a v w) -> expr (Transform m a u v) -> expr (Transform m a u w)
   (>>>) :: expr (Transform m a u v) -> expr (Transform m a v w) -> expr (Transform m a u w)
   (>*) :: expr (Transform m a u v) -> expr (m (v a)) -> expr (m (u a))
