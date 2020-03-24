2c70f9a40df12275b9e817a26f54cf6d6c8e27ff
Rob Rix <rob.rix@me.com>
2020-03-24 14:54:34 -0400

    Derive a bunch of instances for :.:.

--- src/Data/Functor/C.hs
+++ src/Data/Functor/C.hs
@@ -1,6 +1,8 @@
+{-# LANGUAGE DeriveTraversable #-}
 {-# LANGUAGE TypeOperators #-}
 module Data.Functor.C
 ( (:.:)(..)
 ) where
 
 newtype (f :.: g) a = C { getC :: f (g a) }
+  deriving (Eq, Foldable, Functor, Ord, Show, Traversable)
