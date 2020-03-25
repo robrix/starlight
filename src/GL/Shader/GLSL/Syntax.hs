1f39b33fdac114323318b7deb6170972cfab642d
Rob Rix <rob.rix@me.com>
2020-03-24 21:47:46 -0400

    var produces a reference for a final tagless type.

--- src/GL/Shader/GLSL/Syntax.hs
+++ src/GL/Shader/GLSL/Syntax.hs
@@ -1,39 +1,12 @@
+{-# LANGUAGE RankNTypes #-}
 module GL.Shader.GLSL.Syntax
-( Type(..)
-, VType(..)
-, MType(..)
-, N(..)
-, Ref
+( Ref
 , Stmt(..)
 ) where
 
-data Type
-  = Bool
-  | Int
-  | UInt
-  | Float
-  | Double
-  | Vec VType N
-  | Mat MType N
-
-data VType
-  = VB
-  | VI
-  | VU
-  | VF
-  | VD
-
-data MType
-  = MF
-  | MD
-
-data N
-  = N1
-  | N2
-  | N3
-  | N4
+import GL.Shader.GLSL.Type
 
 newtype Ref a = Ref { getRef :: String }
 
 class Stmt stmt where
-  var :: Type -> stmt (Ref a)
+  var :: (forall ty . (Type ty, FType ty, VType ty, MType ty) => ty a) -> stmt (Ref a)
