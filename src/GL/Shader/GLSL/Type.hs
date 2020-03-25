61700a5e0ae2fea718ef859caee2af87b0dda5cb
Rob Rix <rob.rix@me.com>
2020-03-24 21:44:47 -0400

    Define final tagless encodings of the types.

--- src/GL/Shader/GLSL/Type.hs
+++ src/GL/Shader/GLSL/Type.hs
@@ -1,2 +1,29 @@
 module GL.Shader.GLSL.Type
-() where
+( Type(..)
+, VType(..)
+, MType(..)
+) where
+
+import Data.Int
+import Data.Word
+import Linear.Matrix
+import Linear.V2
+import Linear.V3
+import Linear.V4
+
+class Type ty where
+  bool :: ty Bool
+  int :: ty Int32
+  uint :: ty Word32
+  float :: ty Float
+  double :: ty Double
+
+class VType ty where
+  vec2 :: ty a -> ty (V2 a)
+  vec3 :: ty a -> ty (V3 a)
+  vec4 :: ty a -> ty (V4 a)
+
+class MType ty where
+  mat2 :: ty a -> ty (M22 a)
+  mat3 :: ty a -> ty (M33 a)
+  mat4 :: ty a -> ty (M44 a)
