473e44c74baa11962ee6c88c496befaeb2536292
Rob Rix <rob.rix@me.com>
2020-03-24 21:45:38 -0400

    Limit the vector/matrix types.

--- src/GL/Shader/GLSL/Type.hs
+++ src/GL/Shader/GLSL/Type.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE RankNTypes #-}
 module GL.Shader.GLSL.Type
 ( Type(..)
 , VType(..)
@@ -19,11 +20,11 @@ class Type ty where
   double :: ty Double
 
 class VType ty where
-  vec2 :: ty a -> ty (V2 a)
-  vec3 :: ty a -> ty (V3 a)
-  vec4 :: ty a -> ty (V4 a)
+  vec2 :: (forall ty . Type ty => ty a) -> ty (V2 a)
+  vec3 :: (forall ty . Type ty => ty a) -> ty (V3 a)
+  vec4 :: (forall ty . Type ty => ty a) -> ty (V4 a)
 
 class MType ty where
-  mat2 :: ty a -> ty (M22 a)
-  mat3 :: ty a -> ty (M33 a)
-  mat4 :: ty a -> ty (M44 a)
+  mat2 :: (forall ty . Type ty => ty a) -> ty (M22 a)
+  mat3 :: (forall ty . Type ty => ty a) -> ty (M33 a)
+  mat4 :: (forall ty . Type ty => ty a) -> ty (M44 a)
